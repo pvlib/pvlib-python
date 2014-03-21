2bf3be4c77b989e3f1b15e6b57f6d667e33c7bce
Calama Consulting <roba77@gmail.com>
2014-03-21 17:23:17 -0400

    updated for dataframe output

--- pvlib_python/test/test_pvl_ephemeris.py
+++ pvlib_python/test/test_pvl_ephemeris.py
@@ -6,9 +6,9 @@ from .. import pvl_ephemeris
 from .. import pvl_readtmy3
 from .. import pvl_makelocationstruct
 def test_inputs():
-	#TMY3,meta=pvl_readtmy3.pvl_readtmy3(FileName='/Users/robandrews/Dropbox/My_Documents/Documents/Projects/Data/TMY/tmy3/700260TY.csv')
-	#location=pvl_makelocationstruct.pvl_makelocationstruct(latitude=22,longitude=33,altitude=1)
-	#year=pvl_ephemeris.pvl_ephemeris(Time=TMY3.index,Location=location,Timemeta=meta)
+	TMY,meta=pvl_readtmy3.pvl_readtmy3(FileName='/Users/robandrews/Dropbox/My_Documents/Documents/Projects/Data/TMY/tmy3/700260TY.csv')
+
+	DFout=pvl_ephemeris.pvl_ephemeris(Time=TMY.index,Location=meta)
 	assert(1)
 def main():
     unittest.main()

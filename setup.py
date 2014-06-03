91e504fb0704cde390c40c29444e0198115e2da3
Calama Consulting <roba77@gmail.com>
2014-06-03 11:50:46 -0400

    update setup files

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from distutils.core import setup
  
 setup(
     name='pvlpy',
-    version='1.0',
-	author='Clifford Hanson and Rob Andrews',
+    version='0.1',
+	author='Dan Riley, Clifford Hanson and Rob Andrews',
 	author_email='Rob.Andrews@calamaconsulting.ca',
 	packages=['pvlpy','pvlpy.test'],
     license='The BSD 3-Clause License',
+    url = 'https://github.com/Sandia-Labs/PVLIB_Python', #
+  	download_url = 'https://github.com/Sandia-Labs/PVLIB_Python/tarball/0.1', # I'll explain this in a second
     long_description=open('README.txt').read(),
 )

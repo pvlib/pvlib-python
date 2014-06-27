2d733291ed4d792729cf308c68e8282cb4b3674a
Will Holmgren <william.holmgren@gmail.com>
2014-06-26 17:25:17 -0700

    added solarposition.py module. includes PyEphem option

---
+++ pvlib/solarposition.py
@@ -0,0 +1,216 @@
+import logging
+pvl_logger = logging.getLogger('pvlib')
+
+import numpy as np
+import pandas as pd
+
+# optional imports.
+try:
+    import Pysolar 
+except ImportError as e:
+    pvl_logger.warning('Pysolar not found.')
+
+try:    
+    import ephem
+except ImportError as e:
+    pvl_logger.warning('PyEphem not found.')
+
+import pvl_ephemeris
+
+
+
+def get_solarposition(time, location, method='pvlib', pressure=101325, 
+                      temperature=12):
+    """
+    Calculate the solar position using a variety of methods.
+    'pvlib' uses the pvlib ephemeris code. Not recommended as of June 2014.
+    'pyephem' uses the PyEphem package.
+    'pysolar' uses the Pysolar package.
+    
+    The returned DataFrame will be in UTC time if you use 'pyephem' or 'pysolar'.
+    
+    :param time: pandas.DatetimeIndex. 
+    :param location: namedtuple with latitude, longitude, and TZ.
+    :param method: string. 'pvlib', 'pyephem', 'pysolar'
+    :param pressure: int or float. Pascals.
+    :param temperature: int or float. Degrees C.
+    
+    :returns: pandas.DataFrame with columns 'elevation', 'zenith', 'azimuth'.
+              Additional columns including 'solar_time', 'apparent_elevation',
+              and 'apparent_zenith' are available with the 'pvlib'  option.
+    """
+    
+    method = method.lower()
+    
+    if method == 'pvlib':
+        return _pvlib(time, location, pressure, temperature)
+    
+    # convert to UTC times, if necessary.
+    # this crappy code works around the fact that the location
+    # namedtuple specifies the TZ as an offset from UTC.
+    # We should eventually specify the TZ as a pytz/pandas-compatible string
+    # and assume local tz if a tz unaware dataset is provided.
+    try: 
+        # This will work with a timezone aware dataset
+        timeshifted = time.tz_convert('UTC') 
+    except TypeError:
+        # This will work with a timezone unaware dataset
+        # Replaced shift method since it throws away data
+        # and it didn't know the difference between plus and minus offsets
+        ns_offset = location.TZ*3600*1e9
+        timeshifted = pd.DatetimeIndex(time.values.astype(int) - ns_offset).tz_localize('UTC')
+    
+    if method == 'pyephem':
+        return _ephem(timeshifted, location, pressure, temperature)
+    elif method == 'pysolar':
+        return _pysolar(timeshifted, location)
+    else:
+        raise ValueError('Invalid method')
+        
+
+
+def _pvlib(time, location, pressure, temperature):
+    """
+    Calculate the solar position using PVLIB's ephemeris code.
+    """
+    # Will H: I suggest putting the ephemeris code inline here once it's fixed.
+    
+    # We've already corrected the TZ offset above, so don't make 
+    # pvl_ephemeris do the correction too.
+    
+    pvl_logger.debug('using pvlib internal ephemeris code to calculate solar position')
+    
+    return pvl_ephemeris.pvl_ephemeris(time, location, pressure, temperature)
+
+
+
+def _ephem(time, location, pressure, temperature):
+    """
+    Calculate the solar position using the PyEphem package.
+    """
+    
+    pvl_logger.debug('using PyEphem to calculate solar position')
+    
+    sun_coords = pd.DataFrame(index=time)
+    
+    # initialize a PyEphem observer
+    obs = ephem.Observer()
+    obs.lat = str(location.latitude)
+    obs.lon = str(location.longitude)
+    obs.elevation = location.altitude
+    obs.pressure = pressure / 100.
+    obs.temp = temperature
+    
+    # the PyEphem sun
+    sun = ephem.Sun()
+    
+    # make and fill lists of the sun's altitude and azimuth
+    alts = []
+    azis = []
+    for thetime in sun_coords.index:
+        obs.date = ephem.Date(thetime)
+        sun.compute(obs)
+        alts.append(sun.alt)
+        azis.append(sun.az)
+    
+    sun_coords['apparent_elevation'] = alts
+    sun_coords['apparent_azimuth'] = azis
+    
+    # redo it for p=0
+    obs.pressure = 0
+    alts = []
+    azis = []
+    for thetime in sun_coords.index:
+        obs.date = ephem.Date(thetime)
+        sun.compute(obs)
+        alts.append(sun.alt)
+        azis.append(sun.az)
+    
+    sun_coords['elevation'] = alts
+    sun_coords['azimuth'] = azis
+    
+    # convert to degrees. add zenith
+    sun_coords = np.rad2deg(sun_coords)
+    sun_coords['apparent_zenith'] = 90 - sun_coords['apparent_elevation']
+    sun_coords['zenith'] = 90 - sun_coords['elevation']
+    
+    #sun_coords = sun_coords.tz_convert(time.tzinfo)
+    
+    return sun_coords
+    
+    
+
+def _pysolar(time, location):
+    '''
+    Calculate the solar position using the PySolar package
+
+    The PySolar Package is developed by Brandon Stafford, and is
+    found here: https://github.com/pingswept/pysolar/tree/master
+
+    This function will map the standard time and location structures
+    onto the Pysolar function
+
+    Parameters
+    ----------
+
+    Time: Dataframe.index
+
+        A pandas datatime object
+
+    Location: struct
+
+        Standard location structure, containting:
+
+        *Location.latitude* - vector or scalar latitude in decimal degrees (positive is
+                              northern hemisphere)
+        *Location.longitude* - vector or scalar longitude in decimal degrees (positive is 
+                              east of prime meridian)
+        *Location.altitude* - an optional component of the Location struct, not
+                              used in the ephemeris code directly, but it may be used to calculate
+                              standard site pressure (see pvl_alt2pres function)
+
+    Returns
+    -------
+    
+    DataFrame with the following columns:
+    
+    azimuth: 
+        Azimuth of the sun in decimal degrees from North. 0 = North to 270 = West
+  
+    elevation:
+        Actual elevation (not accounting for refraction) of the sun 
+        in decimal degrees, 0 = on horizon. The complement of the True Zenith
+        Angle.
+        
+    zenith: 90 - elevation.
+
+    References
+    ----------
+
+    PySolar Documentation: https://github.com/pingswept/pysolar/tree/master
+    '''
+    
+    #pdb.set_trace()
+
+    pvl_logger.debug('using Pysolar to calculate solar position')
+    
+    sun_az = map(lambda x: Pysolar.GetAzimuth(location.latitude, location.longitude, x), time)
+    sun_el = map(lambda x: Pysolar.GetAltitude(location.latitude, location.longitude, x), time)
+    
+    #sun_el[sun_el < 0] = 0 # Will H: is there a reason this is here?
+    zen = 90 - np.array(sun_el)
+    
+    # Pysolar sets azimuth=0 when facing south. This is inconsistent with
+    # most SPA conventions, including ours. So, we fix it below.
+    sun_az  = (np.array(sun_az) + 360) * -1
+    sun_az[sun_az < -180] = sun_az[sun_az < -180] + 360
+    sun_az += 180
+
+    df_out = pd.DataFrame({'azimuth':sun_az, 'elevation':sun_el, 'zenith':zen}, index=time)
+
+    return df_out
+
+
+
+
+

a5b8ebb5c2e044d11d11baf825b03a17d142de93
Calama Consulting <roba77@gmail.com>
2014-07-17 22:23:06 -0400

    changed local file dependancey

--- pvlib/test/test_pvl_readtmy2.py
+++ pvlib/test/test_pvl_readtmy2.py
@@ -5,11 +5,11 @@ import pandas as pd
 from .. import pvl_readtmy2
 
 def test_proper():
-	fname='/Users/robandrews/Dropbox/My_Documents/Documents/Projects/Data/TMY/tm2/03945.tm2'
+	fname='12839.tm2'
 	TMY2,TMY2meta=pvl_readtmy2(FileName=fname)
 	assert(max(TMY2.RHum<=100))
 
-	fname='/Users/robandrews/Dropbox/My_Documents/Documents/Projects/Data/TMY/tm2/12839.tm2'
+	fname='12839.tm2'
 	TMY2,TMY2meta=pvl_readtmy2(FileName=fname)
 	assert(max(TMY2.RHum<=100))
 	assert(TMY2meta['Latitude']>0)

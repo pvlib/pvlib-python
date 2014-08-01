1bb4559f89731d9ba9586eb0ba23d92ab9f93fd5
Will Holmgren <william.holmgren@gmail.com>
2014-07-31 19:50:37 -0700

    python3 import compatiblity and remove pysolar

--- pvlib/solarposition.py
+++ pvlib/solarposition.py
@@ -11,17 +11,13 @@ import numpy as np
 import pandas as pd
 
 # optional imports.
-try:
-    import Pysolar 
-except ImportError as e:
-    pvl_logger.warning('Pysolar not found.')
 
 try:    
     import ephem
 except ImportError as e:
     pvl_logger.warning('PyEphem not found.')
 
-import pvl_ephemeris
+from . import pvl_ephemeris
 
 
 
@@ -31,13 +27,12 @@ def get_solarposition(time, location, method='pyephem', pressure=101325,
     Calculate the solar position using a variety of methods.
     * 'pvlib' uses the pvlib ephemeris code. Not recommended as of June 2014.
     * 'pyephem' uses the PyEphem package. Default.
-    * 'pysolar' uses the Pysolar package.
     
     The returned DataFrame will be localized to location.tz.
     
     :param time: pandas.DatetimeIndex. 
     :param location: pvlib Location object.
-    :param method: string. 'pvlib', 'pyephem', 'pysolar'
+    :param method: string. 'pvlib', 'pyephem'
     :param pressure: int or float. Pascals.
     :param temperature: int or float. Degrees C.
     
@@ -135,76 +130,3 @@ def _ephem(time, location, pressure, temperature):
     
     return sun_coords
     
-    
-
-def _pysolar(time, location):
-    '''
-    Calculate the solar position using the PySolar package
-
-    The PySolar Package is developed by Brandon Stafford, and is
-    found here: https://github.com/pingswept/pysolar/tree/master
-
-    This function will map the standard time and location structures
-    onto the Pysolar function
-
-    Parameters
-    ----------
-
-    Time: Dataframe.index
-
-        A pandas datatime object
-
-    Location: struct
-
-        Standard location structure, containting:
-
-        *Location.latitude* - vector or scalar latitude in decimal degrees (positive is
-                              northern hemisphere)
-        *Location.longitude* - vector or scalar longitude in decimal degrees (positive is 
-                              east of prime meridian)
-        *Location.altitude* - an optional component of the Location struct, not
-                              used in the ephemeris code directly, but it may be used to calculate
-                              standard site pressure (see pvl_alt2pres function)
-
-    Returns
-    -------
-    
-    DataFrame with the following columns:
-    
-    azimuth: 
-        Azimuth of the sun in decimal degrees from North. 0 = North to 270 = West
-  
-    elevation:
-        Actual elevation (not accounting for refraction) of the sun 
-        in decimal degrees, 0 = on horizon. The complement of the True Zenith
-        Angle.
-        
-    zenith: 90 - elevation.
-
-    References
-    ----------
-
-    PySolar Documentation: https://github.com/pingswept/pysolar/tree/master
-    '''
-    
-    #pdb.set_trace()
-
-    pvl_logger.debug('using Pysolar to calculate solar position')
-    
-    sun_az = map(lambda x: Pysolar.GetAzimuth(location.latitude, location.longitude, x), time)
-    sun_el = map(lambda x: Pysolar.GetAltitude(location.latitude, location.longitude, x), time)
-    
-    #sun_el[sun_el < 0] = 0 # Will H: is there a reason this is here?
-    zen = 90 - np.array(sun_el)
-    
-    # Pysolar sets azimuth=0 when facing south. This is inconsistent with
-    # most SPA conventions, including ours. So, we fix it below.
-    sun_az  = (np.array(sun_az) + 360) * -1
-    sun_az[sun_az < -180] = sun_az[sun_az < -180] + 360
-    sun_az += 180
-
-    df_out = pd.DataFrame({'azimuth':sun_az, 'elevation':sun_el, 'zenith':zen}, index=time)
-
-    return df_out
-
-

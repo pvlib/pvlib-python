fdb5cb7d61a65b67c764d0718920f3454798ba48
Will Holmgren <william.holmgren@gmail.com>
2014-10-31 16:27:14 -0700

    move pvlib/test/data to pvlib/data. supports future tutorial structure

--- pvlib/test/test_tmy.py
+++ pvlib/test/test_tmy.py
@@ -14,8 +14,8 @@ from .. import tmy
 
 
 def test_readtmy3():
-    tmy.readtmy3(os.path.join(test_dir, 'data/703165TY.csv'))
+    tmy.readtmy3(os.path.join(test_dir, '../data/703165TY.csv'))
     
 def test_readtmy2():
-    tmy.readtmy2(os.path.join(test_dir, 'data/12839.tm2'))
+    tmy.readtmy2(os.path.join(test_dir, '../data/12839.tm2'))
     

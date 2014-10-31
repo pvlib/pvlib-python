27ccffe5c971fa0438bda77f225c1dc265832359
Will Holmgren <william.holmgren@gmail.com>
2014-10-31 16:51:17 -0700

    add old masters test_airmass as test_atmosphere. fix absolute imports

---
+++ pvlib/test/test_atmosphere.py
@@ -0,0 +1,43 @@
+import logging
+pvl_logger = logging.getLogger('pvlib')
+
+import datetime
+
+import numpy as np
+import pandas as pd
+
+from nose.tools import raises
+
+from ..location import Location
+from .. import solarposition
+from .. import atmosphere
+
+
+# setup times and location to be tested.
+times = pd.date_range(start=datetime.datetime(2014,6,24), 
+                      end=datetime.datetime(2014,6,26), freq='1Min')
+
+tus = Location(32.2, -111, 'US/Arizona', 700)
+
+times_localized = times.tz_localize(tus.tz)
+
+ephem_data = solarposition.get_solarposition(times, tus)
+
+
+
+# two functions combined will generate unique unit tests for each model
+def test_airmasses():
+    models = ['simple', 'kasten1966', 'youngirvine1967', 'kastenyoung1989',
+              'gueymard1993', 'young1994', 'pickering2002', 'invalid']
+    for model in models:
+        yield run_airmass, ephem_data['zenith'], model
+    
+def run_airmass(zenith, model):
+    atmosphere.relativeairmass(zenith, model)
+    
+    
+    
+def test_absoluteairmass():
+    relative_am = atmosphere.relativeairmass(ephem_data['zenith'], 'simple')
+    atmosphere.absoluteairmass(relative_am)
+    atmosphere.absoluteairmass(relative_am, pressure=100000)
--- pvlib/test/test_clearsky.py
+++ pvlib/test/test_clearsky.py
@@ -8,9 +8,9 @@ import pandas as pd
 
 from nose.tools import raises
 
-from pvlib.location import Location
-import pvlib.clearsky
-import pvlib.solarposition
+from ..location import Location
+from .. import clearsky
+from .. import solarposition
 
 # setup times and location to be tested.
 times = pd.date_range(start=datetime.datetime(2014,6,24), 
@@ -20,7 +20,7 @@ tus = Location(32.2, -111, 'US/Arizona', 700)
 
 times_localized = times.tz_localize(tus.tz)
 
-ephem_data = pvlib.solarposition.get_solarposition(times, tus)
+ephem_data = solarposition.get_solarposition(times, tus)
 
 
 
@@ -28,29 +28,29 @@ ephem_data = pvlib.solarposition.get_solarposition(times, tus)
 
 def test_ineichen_required():
     # the clearsky function should lookup the linke turbidity on its own
-    pvlib.clearsky.ineichen(times, tus)
+    clearsky.ineichen(times, tus)
     
 def test_ineichen_supply_linke():
-    pvlib.clearsky.ineichen(times, tus, linke_turbidity=3)
+    clearsky.ineichen(times, tus, linke_turbidity=3)
 
 def test_ineichen_solpos():
-    pvlib.clearsky.ineichen(times, tus, linke_turbidity=3,
+    clearsky.ineichen(times, tus, linke_turbidity=3,
                             solarposition_method='pyephem')
 
 def test_ineichen_airmass():
-    pvlib.clearsky.ineichen(times, tus, linke_turbidity=3,
+    clearsky.ineichen(times, tus, linke_turbidity=3,
                             airmass_model='simple')
 
 def test_ineichen_keys():
-    clearsky_data = pvlib.clearsky.ineichen(times, tus, linke_turbidity=3)
+    clearsky_data = clearsky.ineichen(times, tus, linke_turbidity=3)
     assert 'GHI' in clearsky_data.columns
     assert 'DNI' in clearsky_data.columns
     assert 'DHI' in clearsky_data.columns
 
 # test the haurwitz clear sky implementation
 def test_haurwitz():
-    pvlib.clearsky.haurwitz(ephem_data['zenith'])
+    clearsky.haurwitz(ephem_data['zenith'])
 
 def test_haurwitz_keys():
-    clearsky_data = pvlib.clearsky.haurwitz(ephem_data['zenith'])
+    clearsky_data = clearsky.haurwitz(ephem_data['zenith'])
     assert 'GHI' in clearsky_data.columns

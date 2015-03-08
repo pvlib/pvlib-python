50302c92f909da0ffeefc047c79ddeabebf02b30
Will Holmgren <william.holmgren@gmail.com>
2015-03-08 14:01:47 -0700

    allow for missing spa files

--- setup.py
+++ setup.py
@@ -44,16 +44,25 @@ setuptools_kwargs = {
 # 'pyephem',
 
 # set up pvlib packages to be installed and extensions to be compiled
-PACKAGES = ['pvlib', 
-            'pvlib.spa_c_files']
+PACKAGES = ['pvlib']
 
 extensions = []
 
-spa_ext = Extension('pvlib.spa_c_files.spa_py', 
-                    sources = ['pvlib/spa_c_files/spa.c', 
-                              'pvlib/spa_c_files/spa_py.c'],
-                    depends = ['pvlib/spa_c_files/spa.h'])
-extensions.append(spa_ext)
+spa_sources = ['pvlib/spa_c_files/spa.c', 'pvlib/spa_c_files/spa_py.c']
+spa_depends = ['pvlib/spa_c_files/spa.h']
+spa_all_file_paths = map(lambda x: os.path.join(os.path.dirname(__file__), x),
+                         spa_sources + spa_depends)
+
+if all(map(os.path.exists, spa_all_file_paths)):
+    print('all spa_c files found')
+    PACKAGES.append('pvlib.spa_c_files')
+
+    spa_ext = Extension('pvlib.spa_c_files.spa_py', 
+                        sources=spa_sources, depends=spa_depends)
+    extensions.append(spa_ext)
+else:
+    print('WARNING: spa_c files not detected. ' +
+          'See installation instructions for more information.')
 
  
 setup(name=DISTNAME,

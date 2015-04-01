06fd22d45f025294132ea07891bab21e369c76c5
Will Holmgren <william.holmgren@gmail.com>
2015-04-01 09:27:05 -0700

    change tz handling. replace float() with .

--- pvlib/solarposition.py
+++ pvlib/solarposition.py
@@ -267,6 +267,10 @@ def ephemeris(time, location, pressure=101325, temperature=12):
 
     # Added by Rob Andrews (@Calama-Consulting), Calama Consulting, 2014
     # Edited by Will Holmgren (@wholmgren), University of Arizona, 2014
+    
+    # Most comments in this function are from PVLIB_MATLAB or from
+    # pvlib-python's attempt to understand and fix problems with the
+    # algorithm. The comments are *not* based on the reference material.
 
     pvl_logger.warning('Using solarposition.ephemeris is discouraged. '
                        'solarposition.pyephem and solarposition.spa are '
@@ -275,55 +279,50 @@ def ephemeris(time, location, pressure=101325, temperature=12):
     pvl_logger.debug('location={}, temperature={}, pressure={}'.format(
         location, temperature, pressure))
 
-    Latitude = location.latitude
-    ''' the inversion of longitude is due to the fact that this code was
-    originally written for the convention that positive longitude were for
-    locations west of the prime meridian. However, the correct convention (as
-    of 2009) is to use negative longitudes for locations west of the prime
-    meridian. Therefore, the user should input longitude values under the
-    correct convention (e.g. Albuquerque is at -106 longitude), but it needs
-    to be inverted for use in the code.
-    '''
+    # the inversion of longitude is due to the fact that this code was
+    # originally written for the convention that positive longitude were for
+    # locations west of the prime meridian. However, the correct convention (as
+    # of 2009) is to use negative longitudes for locations west of the prime
+    # meridian. Therefore, the user should input longitude values under the
+    # correct convention (e.g. Albuquerque is at -106 longitude), but it needs
+    # to be inverted for use in the code.
+    
     Latitude = location.latitude
     Longitude = 1 * location.longitude
-    Year = time.year
-    Hour = time.hour
-    Minute = time.minute
-    Second = time.second
-    DayOfYear = time.dayofyear
-
-    DecHours = Hour + Minute / float(60) + Second / float(3600)
-
-    Abber = 20 / float(3600)
+    
+    Abber = 20 / 3600.
     LatR = np.radians(Latitude)
-
-    # this code is needed to handle the new location.tz format string
-    try:
-        utc_offset = pytz.timezone(location.tz).utcoffset(
-            time[0]).total_seconds() / 3600.
-    except ValueError:
-        utc_offset = time.tzinfo.utcoffset(time[0]).total_seconds() / 3600.
-    pvl_logger.debug('utc_offset={}'.format(utc_offset))
+    
+    # the SPA algorithm needs time to be expressed in terms of
+    # decimal UTC hours of the day of the year.
+    
+    # first convert to utc
+    time_utc = localize_to_utc(time, location)
+    
+    # strip out the day of the year and calculate the decimal hour
+    DayOfYear = time_utc.dayofyear
+    DecHours = (time_utc.hour + time_utc.minute/60. + time_utc.second/3600.
+                time_utc.microsecond/3600.e6)
 
     UnivDate = DayOfYear
-
-    # ToDo: Will H: surprised to see the 0.5 here, but moving on...
-    UnivHr = DecHours + utc_offset - .5
+    UnivHr = DecHours
 
     Yr = Year - 1900
 
     YrBegin = 365 * Yr + np.floor((Yr - 1) / float(4)) - 0.5
 
     Ezero = YrBegin + UnivDate
-    T = Ezero / float(36525)
-    GMST0 = 6 / float(24) + 38 / float(1440) + (
-        45.836 + 8640184.542 * T + 0.0929 * T ** 2) / float(86400)
+    T = Ezero / 36525.
+    
+    # Calculate Greenwich Mean Sidereal Time (GMST)
+    GMST0 = 6 / 24. + 38 / 1440. + (
+        45.836 + 8640184.542 * T + 0.0929 * T ** 2) / 86400.
     GMST0 = 360 * (GMST0 - np.floor(GMST0))
     GMSTi = np.mod(GMST0 + 360 * (1.0027379093 * UnivHr / float(24)), 360)
 
     LocAST = np.mod((360 + GMSTi - Longitude), 360)
-    EpochDate = Ezero + UnivHr / float(24)
-    T1 = EpochDate / float(36525)
+    EpochDate = Ezero + UnivHr / 24.
+    T1 = EpochDate / 36525.
     ObliquityR = np.radians(
         23.452294 - 0.0130125 * T1 - 1.64e-06 * T1 ** 2 + 5.03e-07 * T1 ** 3)
     MlPerigee = 281.22083 + 4.70684e-05 * EpochDate + 0.000453 * T1 ** 2 + (
@@ -338,7 +337,6 @@ def ephemeris(time, location, pressure=101325, temperature=12):
         E = EccenAnom
         EccenAnom = MeanAnom + np.degrees(Eccen) * (np.sin(np.radians(E)))
 
-    # pdb.set_trace()
     TrueAnom = (
         2 * np.mod(np.degrees(np.arctan2(((1 + Eccen) / (1 - Eccen)) ** 0.5 *
                    np.tan(np.radians(EccenAnom) / float(2)), 1)), 360))
@@ -361,8 +359,8 @@ def ephemeris(time, location, pressure=101325, temperature=12):
                        (np.cos(HrAngleR)) + np.sin(LatR)*(np.sin(DecR)))))
     SolarTime = (180 + HrAngle) / float(15)
 
+    # replace with conditional array assignment
     Refract = []
-
     for Elevation in SunEl:
         TanEl = np.tan(np.radians(Elevation))
         if Elevation > 5 and Elevation <= 85:
@@ -378,7 +376,7 @@ def ephemeris(time, location, pressure=101325, temperature=12):
             Refract.append(0)
 
     Refract = (np.array(Refract) * ((283 / float(273 + temperature))) *
-               pressure / float(101325) / float(3600))
+               pressure / 101325. / 3600.)
 
     SunZen = 90 - SunEl
 

0dbca091cfbe2950240975ca517771bdff72fad1
bmu <diehose@freenet.de>
2015-04-23 09:28:13 +0200

    DOC: added local pvlib path to be able to build documentation for current local status of the repository

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -34,6 +34,7 @@ sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../sphinxext'))
+sys.path.insert(0, os.path.abspath('../../../'))
 
 # -- General configuration ------------------------------------------------
 

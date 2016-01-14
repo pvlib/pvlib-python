862e2dd8038594eba60c7661ed8d7321ac5a5294
Will Holmgren <william.holmgren@gmail.com>
2016-01-13 17:33:43 -0700

    add ipython directives to conf.py

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -50,7 +50,9 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.extlinks',
     'numpydoc',
-    'sphinx.ext.autosummary'
+    'sphinx.ext.autosummary',
+    'IPython.sphinxext.ipython_directive',
+    'IPython.sphinxext.ipython_console_highlighting',
 ]
 
 # Add any paths that contain templates here, relative to this directory.

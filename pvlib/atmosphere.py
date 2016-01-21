10595fe0b0290d0d9e4a63703bb0e14a95210fb2
Will Holmgren <william.holmgren@gmail.com>
2016-01-21 15:15:19 -0700

    add calc_pw. needs tests

--- pvlib/atmosphere.py
+++ pvlib/atmosphere.py
@@ -249,6 +249,64 @@ def relativeairmass(zenith, model='kastenyoung1989'):
     return AM
 
 
+def calc_pw(temp_air, relative_humidity):
+    """
+    Calculates precipitable water (cm) from ambient air temperature (C) and
+    relatively humidity (%) using an empirical model [1]. The model was
+    developed by expanding Eq. 1 in [2]:
+           w = 0.1 H_v \rho_v 
+    using Eq. 2 in [2]
+           \rho_v = 216.7 RH/T e_s 
+    H_v is the apparant water vapor scale height (km). The expression for 
+    H_v is Eq. 4 in [2]:
+           H_v = 0.4976 + 1.5265.*T./273.15 + exp(13.6897.*T./273.15 - 14.9188.*(T./273.15).^3)
+    \rho_v is the surface water vapor density (g/m^3).  In the expression 
+    \rho_v,  e_s is the saturation water vapor pressure (millibar).  The
+    expression for e_s is Eq. 1 in [3]
+          e_s = exp(22.330 - 49.140.*(100./T) - 10.922.*(100./T).^2 - 0.39015.*T./100)
+
+    Parameters
+    ----------
+    temp_air : array-like
+        ambient air temperature at the surface (C)
+    relative_humidity : array-like
+        relative humidity at the surface (%)
+
+    Returns
+    ------- 
+    pw : array-like
+        precipitable water (cm)
+
+    Reference:
+    [1] W. M. Keogh and A. W. Blakers, Accurate Measurement, Using Natural 
+        Sunlight, of Silicon Solar Cells, Prog. in Photovoltaics: Res. 
+        and Appl. 2004, vol 12, pp. 1-19 (DOI: 10.1002/pip.517)
+    [2] C. Gueymard, Analysis of Monthly Average Atmospheric Precipitable
+        Water and Turbidity in Canada and Northern United States,
+        Solar Energy vol 53(1), pp. 57-71, 1994.
+    [3] C. Gueymard, Assessment of the Accuracy and Computing Speed of
+        simplified saturation vapor equations using a new reference
+        dataset, J. of Applied Meteorology 1993, vol. 32(7), pp.
+        1294-1300.
+    """
+
+    T = temp_air + 273.15 # Convert to Kelvin
+    RH = relative_humidity
+
+    #RH[RH>100 | RH<=0] = NaN; #Filter RH for unreasonable Values
+
+    # Eq. 1 from Keogh and Blakers
+    pw = ( 0.1 * 
+           (0.4976 + 1.5265*T/273.15 +
+            np.exp(13.6897*T/273.15 - 14.9188*(T/273.15)**3)) *
+           (216.7*RH/(100.*T)*np.exp(22.330 - 49.140*(100./T) -
+            10.922*(100./T)**2 - 0.39015*T/100)))
+
+    pw = np.maximum(pw, 0.1)
+
+    return pw
+
+
 def first_solar_spectral_correction(pw, airmass_absolute, module_type=None,
                                     coefficients=None):
     """

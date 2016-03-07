5280cb9f40d34ec8eb75068866e22affcd05e76f
Will Holmgren <william.holmgren@gmail.com>
2016-03-06 20:53:38 -0700

    add test for orientation prop, altitude/pressure kwargs

--- pvlib/test/test_modelchain.py
+++ pvlib/test/test_modelchain.py
@@ -57,6 +57,9 @@ def run_orientation_strategy(strategy, expected):
 
     mc = ModelChain(system, location, orientation_strategy=strategy)
 
+    # the || accounts for the coercion of 'None' to None
+    assert (mc.orientation_strategy == strategy or
+            mc.orientation_strategy == None)
     assert system.surface_tilt == expected[0]
     assert system.surface_azimuth == expected[1]
 
@@ -96,6 +99,12 @@ def test_run_model_with_weather():
                          index=times)
     assert_series_equal(ac, expected)
 
+
+@raises(ValueError)
+def test_bad_get_orientation():
+    modelchain.get_orientation('bad value')
+
+
 @raises(ValueError)
 def test_basic_chain_required():
     times = pd.DatetimeIndex(start='20160101 1200-0700',
@@ -111,6 +120,7 @@ def test_basic_chain_required():
     dc, ac = modelchain.basic_chain(times, latitude, longitude,
                                     module_parameters, inverter_parameters,
                                     altitude=altitude)
+
 
 def test_basic_chain_alt_az():
     times = pd.DatetimeIndex(start='20160101 1200-0700',
@@ -128,12 +138,12 @@ def test_basic_chain_alt_az():
     dc, ac = modelchain.basic_chain(times, latitude, longitude,
                                     module_parameters, inverter_parameters,
                                     surface_tilt=surface_tilt,
-                                    surface_azimuth=surface_azimuth,
-                                    altitude=altitude)
+                                    surface_azimuth=surface_azimuth)
 
-    expected = pd.Series(np.array([  1.15771428788e+02,  -2.00000000e-02]),
+    expected = pd.Series(np.array([  1.14490928477e+02,  -2.00000000e-02]),
                          index=times)
     assert_series_equal(ac, expected)
+
 
 def test_basic_chain_strategy():
     times = pd.DatetimeIndex(start='20160101 1200-0700',
@@ -152,5 +162,39 @@ def test_basic_chain_strategy():
                                     altitude=altitude)
 
     expected = pd.Series(np.array([  1.82033563543e+02,  -2.00000000e-02]),
+                         index=times)
+    assert_series_equal(ac, expected)
+
+
+def test_basic_chain_altitude_pressure():
+    times = pd.DatetimeIndex(start='20160101 1200-0700',
+                             end='20160101 1800-0700', freq='6H')
+    latitude = 32.2
+    longitude = -111
+    altitude = 700
+    surface_tilt = 0
+    surface_azimuth = 0
+    modules = sam_data['sandiamod']
+    module_parameters = modules['Canadian_Solar_CS5P_220M___2009_']
+    inverters = sam_data['cecinverter']
+    inverter_parameters = inverters['ABB__MICRO_0_25_I_OUTD_US_208_208V__CEC_2014_']
+
+    dc, ac = modelchain.basic_chain(times, latitude, longitude,
+                                    module_parameters, inverter_parameters,
+                                    surface_tilt=surface_tilt,
+                                    surface_azimuth=surface_azimuth,
+                                    pressure=93194)
+
+    expected = pd.Series(np.array([  1.15771428788e+02,  -2.00000000e-02]),
+                         index=times)
+    assert_series_equal(ac, expected)
+
+    dc, ac = modelchain.basic_chain(times, latitude, longitude,
+                                    module_parameters, inverter_parameters,
+                                    surface_tilt=surface_tilt,
+                                    surface_azimuth=surface_azimuth,
+                                    altitude=altitude)
+
+    expected = pd.Series(np.array([  1.15771428788e+02,  -2.00000000e-02]),
                          index=times)
     assert_series_equal(ac, expected)

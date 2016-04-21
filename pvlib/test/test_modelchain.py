abd1984e273a544f9b99c7e581d2d0436f564d23
Will Holmgren <william.holmgren@gmail.com>
2016-04-21 16:40:15 -0700

    allow py3 conda failures. pep8

--- pvlib/test/test_modelchain.py
+++ pvlib/test/test_modelchain.py
@@ -8,8 +8,10 @@ from pvlib.pvsystem import PVSystem
 from pvlib.tracking import SingleAxisTracker
 from pvlib.location import Location
 
-from pandas.util.testing import assert_series_equal, assert_frame_equal
-from nose.tools import with_setup, raises
+from pandas.util.testing import assert_series_equal
+from nose.tools import raises
+
+from . import incompatible_conda_linux_py3
 
 # should store this test data locally, but for now...
 sam_data = {}
@@ -93,7 +95,7 @@ def singlediode_setup():
 def test_ModelChain_creation():
     system = PVSystem()
     location = Location(32.2, -111, altitude=700)
-    mc = ModelChain(system, location)
+    ModelChain(system, location)
 
 
 def test_orientation_strategy():
@@ -113,7 +115,7 @@ def run_orientation_strategy(strategy, expected):
 
     # the || accounts for the coercion of 'None' to None
     assert (mc.orientation_strategy == strategy or
-            mc.orientation_strategy == None)
+            mc.orientation_strategy is None)
     assert system.surface_tilt == expected[0]
     assert system.surface_azimuth == expected[1]
 
@@ -126,11 +128,12 @@ def test_run_model_ModelChain():
     mc.run_model()
 
 
+@incompatible_conda_linux_py3
 def test_run_model():
     times = pd.date_range('20160101 1200-0700', periods=2, freq='6H')
     expected = {}
     expected[SAPM] = \
-        pd.Series(np.array([  1.82033564e+02,  -2.00000000e-02]), index=times)
+        pd.Series(np.array([1.82033564e+02,  -2.00000000e-02]), index=times)
     expected[SingleDiode] = \
         pd.Series(np.array([179.720353871, np.nan]), index=times)
 
@@ -142,13 +145,14 @@ def test_run_model():
         yield run_test, mc
 
 
+@incompatible_conda_linux_py3
 def test_run_model_with_irradiance():
     times = pd.date_range('20160101 1200-0700', periods=2, freq='6H')
     irradiance = pd.DataFrame(
         {'dni': [900, 0], 'ghi': [600, 50], 'dhi': [150, 50]}, index=times)
     expected = {}
     expected[SAPM] = \
-        pd.Series(np.array([  1.90054749e+02,  -2.00000000e-02]), index=times)
+        pd.Series(np.array([1.90054749e+02,  -2.00000000e-02]), index=times)
     expected[SingleDiode] = \
         pd.Series(np.array([186.979595403, 7.89417460232]), index=times)
 
@@ -160,12 +164,13 @@ def test_run_model_with_irradiance():
         yield run_test, mc
 
 
+@incompatible_conda_linux_py3
 def test_run_model_with_weather():
     times = pd.date_range('20160101 1200-0700', periods=2, freq='6H')
     weather = pd.DataFrame({'wind_speed': 5, 'temp_air': 10}, index=times)
     expected = {}
     expected[SAPM] = \
-        pd.Series(np.array([  1.99952400e+02,  -2.00000000e-02]), index=times)
+        pd.Series(np.array([1.99952400e+02,  -2.00000000e-02]), index=times)
     expected[SingleDiode] = \
         pd.Series(np.array([198.13564009, np.nan]), index=times)
 
@@ -209,10 +214,9 @@ def test_basic_chain_required():
     latitude = 32
     longitude = -111
     altitude = 700
-    modules = sam_data['sandiamod']
-    module_parameters = modules['Canadian_Solar_CS5P_220M___2009_']
-    inverters = sam_data['cecinverter']
-    inverter_parameters = inverters['ABB__MICRO_0_25_I_OUTD_US_208_208V__CEC_2014_']
+
+    module_parameters = get_sapm_module_parameters()
+    inverter_parameters = get_cec_inverter_parameters()
 
     dc, ac = modelchain.basic_chain(times, latitude, longitude,
                                     module_parameters, inverter_parameters,
@@ -224,20 +228,18 @@ def test_basic_chain_alt_az():
                              end='20160101 1800-0700', freq='6H')
     latitude = 32.2
     longitude = -111
-    altitude = 700
     surface_tilt = 0
     surface_azimuth = 0
-    modules = sam_data['sandiamod']
-    module_parameters = modules['Canadian_Solar_CS5P_220M___2009_']
-    inverters = sam_data['cecinverter']
-    inverter_parameters = inverters['ABB__MICRO_0_25_I_OUTD_US_208_208V__CEC_2014_']
+
+    module_parameters = get_sapm_module_parameters()
+    inverter_parameters = get_cec_inverter_parameters()
 
     dc, ac = modelchain.basic_chain(times, latitude, longitude,
                                     module_parameters, inverter_parameters,
                                     surface_tilt=surface_tilt,
                                     surface_azimuth=surface_azimuth)
 
-    expected = pd.Series(np.array([  1.14490928477e+02,  -2.00000000e-02]),
+    expected = pd.Series(np.array([1.14490928477e+02,  -2.00000000e-02]),
                          index=times)
     assert_series_equal(ac, expected)
 
@@ -248,17 +250,15 @@ def test_basic_chain_strategy():
     latitude = 32.2
     longitude = -111
     altitude = 700
-    modules = sam_data['sandiamod']
-    module_parameters = modules['Canadian_Solar_CS5P_220M___2009_']
-    inverters = sam_data['cecinverter']
-    inverter_parameters = inverters['ABB__MICRO_0_25_I_OUTD_US_208_208V__CEC_2014_']
 
-    dc, ac = modelchain.basic_chain(times, latitude, longitude,
-                                    module_parameters, inverter_parameters,
-                                    orientation_strategy='south_at_latitude_tilt',
-                                    altitude=altitude)
+    module_parameters = get_sapm_module_parameters()
+    inverter_parameters = get_cec_inverter_parameters()
 
-    expected = pd.Series(np.array([  1.82033563543e+02,  -2.00000000e-02]),
+    dc, ac = modelchain.basic_chain(
+        times, latitude, longitude, module_parameters, inverter_parameters,
+        orientation_strategy='south_at_latitude_tilt', altitude=altitude)
+
+    expected = pd.Series(np.array([1.82033563543e+02,  -2.00000000e-02]),
                          index=times)
     assert_series_equal(ac, expected)
 
@@ -271,10 +271,9 @@ def test_basic_chain_altitude_pressure():
     altitude = 700
     surface_tilt = 0
     surface_azimuth = 0
-    modules = sam_data['sandiamod']
-    module_parameters = modules['Canadian_Solar_CS5P_220M___2009_']
-    inverters = sam_data['cecinverter']
-    inverter_parameters = inverters['ABB__MICRO_0_25_I_OUTD_US_208_208V__CEC_2014_']
+
+    module_parameters = get_sapm_module_parameters()
+    inverter_parameters = get_cec_inverter_parameters()
 
     dc, ac = modelchain.basic_chain(times, latitude, longitude,
                                     module_parameters, inverter_parameters,
@@ -282,7 +281,7 @@ def test_basic_chain_altitude_pressure():
                                     surface_azimuth=surface_azimuth,
                                     pressure=93194)
 
-    expected = pd.Series(np.array([  1.15771428788e+02,  -2.00000000e-02]),
+    expected = pd.Series(np.array([1.15771428788e+02,  -2.00000000e-02]),
                          index=times)
     assert_series_equal(ac, expected)
 
@@ -292,6 +291,6 @@ def test_basic_chain_altitude_pressure():
                                     surface_azimuth=surface_azimuth,
                                     altitude=altitude)
 
-    expected = pd.Series(np.array([  1.15771428788e+02,  -2.00000000e-02]),
+    expected = pd.Series(np.array([1.15771428788e+02,  -2.00000000e-02]),
                          index=times)
     assert_series_equal(ac, expected)

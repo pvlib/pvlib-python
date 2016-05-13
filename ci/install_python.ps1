b438cfd9fe902d8cd8009973e4308937d15886b9
Will Holmgren <william.holmgren@gmail.com>
2016-05-13 12:01:06 -0700

    try using conda env

--- ci/install_python.ps1
+++ ci/install_python.ps1
@@ -8,7 +8,7 @@ $BASE_URL = "https://www.python.org/ftp/python/"
 
 function DownloadMiniconda ($python_version, $platform_suffix) {
     $webclient = New-Object System.Net.WebClient
-    if ($python_version -eq "3.4") {
+    if ($python_version -like "3.*") {
         $filename = "Miniconda3-latest-Windows-" + $platform_suffix + ".exe"
     } else {
         $filename = "Miniconda2-latest-Windows-" + $platform_suffix + ".exe"

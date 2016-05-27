f9885d42b9a9c1fa781f261db53599066e91a84e
Will Holmgren <william.holmgren@gmail.com>
2016-05-27 10:08:45 -0700

    update setup.py descriptions

--- setup.py
+++ setup.py
@@ -15,14 +15,31 @@ except ImportError:
 import versioneer
 
 
-DESCRIPTION = 'The PVLIB toolbox provides a set of functions for simulating the performance of photovoltaic energy systems.'
-LONG_DESCRIPTION = break # fix me
+DESCRIPTION = ('A set of functions and classes for simulating the ' +
+               'performance of photovoltaic energy systems.')
+LONG_DESCRIPTION = """
+PVLIB Python is a community supported tool that provides a set of
+functions and classes for simulating the performance of photovoltaic
+energy systems. The toolbox was originally developed in MATLAB at Sandia
+National Laboratories and it implements many of the models and methods
+developed at the Labs. More information on Sandia Labs PV performance
+modeling programs can be found at https://pvpmc.sandia.gov/. We
+collaborate with the PVLIB-MATLAB project, but operate independently of
+it.
+
+We need your help to make pvlib-python a great tool!
+
+Documentation: http://pvlib-python.readthedocs.io
+
+Source code: https://github.com/pvlib/pvlib-python
+"""
 
 DISTNAME = 'pvlib'
 LICENSE = 'BSD 3-Clause'
 AUTHOR = 'PVLIB Python Developers'
 MAINTAINER_EMAIL = 'holmgren@email.arizona.edu'
 URL = 'https://github.com/pvlib/pvlib-python'
+
 INSTALL_REQUIRES = ['numpy >= 1.8.2',
                     'pandas >= 0.13.1',
                     'pytz',
@@ -45,10 +62,6 @@ CLASSIFIERS = [
     'Topic :: Scientific/Engineering',
 ]
 
-# check python version.
-if not sys.version_info[:2] in ((2,7), (3,3), (3,4), (3,5)):
-   sys.exit('%s requires Python 2.7, 3.3, or 3.4' % DISTNAME)
-
 setuptools_kwargs = {
     'zip_safe': False,
     'scripts': [],
@@ -69,14 +82,14 @@ if all(map(os.path.exists, spa_all_file_paths)):
     print('all spa_c files found')
     PACKAGES.append('pvlib.spa_c_files')
 
-    spa_ext = Extension('pvlib.spa_c_files.spa_py', 
+    spa_ext = Extension('pvlib.spa_c_files.spa_py',
                         sources=spa_sources, depends=spa_depends)
     extensions.append(spa_ext)
 else:
     print('WARNING: spa_c files not detected. ' +
           'See installation instructions for more information.')
 
- 
+
 setup(name=DISTNAME,
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),

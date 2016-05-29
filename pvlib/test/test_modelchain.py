217b0481e486afc64efd4810d486343ad9333dea
Johannes Oos <j.oos@hotmail.de>
2016-05-29 09:15:51 +0300

    correct bug in modelchain reppr test

--- pvlib/test/test_modelchain.py
+++ pvlib/test/test_modelchain.py
@@ -231,7 +231,7 @@ def test_ModelChain___repr__():
 
     # the || accounts for the coercion of 'None' to None
     assert mc.__repr__() == ('ModelChain for: PVSystem with tilt:32.2 and '+
-    'azimuth: 180 with Module: None and Inverter: None with '+
-    'orientation_startegy: south_at_latitude_tilt clearsky_model:ineichen '+
-    'transposition_model: haydavies solar_position_method: nrel_numpy '+
-    'airmass_model: kastenyoung1989')
+    'azimuth: 180 with Module: None and Inverter: None '+
+    'orientation_startegy: south_at_latitude_tilt clearsky_model: '+
+    'ineichentransposition_model: haydavies solar_position_method: '+
+    'nrel_numpyairmass_model: kastenyoung1989')

f29924fd04a5ae0d5b7cd7e9efb6731fb094a711
Will Holmgren <william.holmgren@gmail.com>
2016-07-16 15:39:35 -0700

    add print to conf

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -32,9 +32,11 @@ sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 ipython_mplbackend = 'Agg'
 
 # builds the font cache if it does not already exist.
-# import matplotlib
-# matplotlib.use('Agg')
-# import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+print(matplotlib.get_backend())
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the

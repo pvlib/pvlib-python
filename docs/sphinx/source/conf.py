67ba02a0f07949aff01193448e2b2d1e6e73f764
Will Holmgren <william.holmgren@gmail.com>
2016-07-16 12:25:11 -0700

    add mpl import to conf.py

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -29,6 +29,9 @@ class Mock(MagicMock):
 MOCK_MODULES = []
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
+# builds the font cache if it does not already exist.
+import matplotlib.pyplot as plt
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.

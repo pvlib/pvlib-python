Fix numerical overflow in v_from_i for some conditions (#226)
* implement log lambertw calculation in v_from_i

* add test

* more robust

* add note to whatsnew

* add issue note to test

* move the .real

* remove scipy version in appveyor config

pvlib/pvsystem.py
@@ -1827,14 +1827,29 @@ def v_from_i(resistance_shunt, resistance_series, nNsVth, current,
     IL = photocurrent
     I = current
 
-    argW = I0 * Rsh / nNsVth * np.exp(Rsh *(-I + IL + I0) / nNsVth)
-    lambertwterm = lambertw(argW)
+    argW = I0 * Rsh / nNsVth * np.exp(Rsh * (-I + IL + I0) / nNsVth)
+    lambertwterm = lambertw(argW).real
+
+    # Calculate using log(argW) in case argW is really big
+    logargW = (np.log(I0) + np.log(Rsh) - np.log(nNsVth) +
+               Rsh * (-I + IL + I0) / nNsVth)
+
+    # Three iterations of Newton-Raphson method to solve
+    # w+log(w)=logargW. The initial guess is w=logargW. Where direct
+    # evaluation (above) results in NaN from overflow, 3 iterations
+    # of Newton's method gives approximately 8 digits of precision.
+    w = logargW
+    for i in range(0, 3):
+        w = w * (1 - np.log(w) + logargW) / (1 + w)
+    lambertwterm_log = w
+
+    lambertwterm = np.where(np.isfinite(lambertwterm), lambertwterm,
+                            lambertwterm_log)
 
     # Eqn. 3 in Jain and Kapoor, 2004
-
     V = -I*(Rs + Rsh) + IL*Rsh - nNsVth*lambertwterm + I0*Rsh
 
-    return V.real
+    return V
 
 
 def i_from_v(resistance_shunt, resistance_series, nNsVth, voltage,
@@ -1898,12 +1913,12 @@ def i_from_v(resistance_shunt, resistance_series, nNsVth, voltage,
     argW = (Rs*I0*Rsh *
             np.exp(Rsh*(Rs*(IL+I0)+V) / (nNsVth*(Rs+Rsh))) /
             (nNsVth*(Rs + Rsh)))
-    lambertwterm = lambertw(argW)
+    lambertwterm = lambertw(argW).real
 
     # Eqn. 4 in Jain and Kapoor, 2004
     I = -V/(Rs + Rsh) - (nNsVth/Rs)*lambertwterm + Rsh*(IL + I0)/(Rs + Rsh)
 
-    return I.real
+    return I
 
 
 def snlinverter(v_dc, p_dc, inverter):
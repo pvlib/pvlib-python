c2a1ae94905885c213a697e54d7ca390251497dd
Will Holmgren <william.holmgren@gmail.com>
2016-12-24 12:38:15 -0700

    add classifier

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
     'Topic :: Scientific/Engineering',
 ]
 

18ec0ea6b36f9762bfd11dfe85a99f54238d2e41
VolkrB <VolkerBeu@gmail.com>
2017-01-17 12:14:34 -0700

    updated pvsystem with new adr-inv csv filename

--- pvlib/pvsystem.py
+++ pvlib/pvsystem.py
@@ -1128,7 +1128,7 @@ def retrieve_sam(name=None, path=None):
             csvdata = os.path.join(
                 data_path, 'sam-library-sandia-modules-2015-6-30.csv')
         elif name == 'adrinverter':
-            csvdata = os.path.join(data_path, 'adr-library.csv')
+            csvdata = os.path.join(data_path, 'adr-library-2013-10-01.csv')
         elif name in ['cecinverter', 'sandiainverter']:
             # Allowing either, to provide for old code,
             # while aligning with current expectations

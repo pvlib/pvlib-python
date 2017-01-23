c4928048661c2f02c28c0186444564081d422e28
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2017-01-23 15:43:46 -0800

    revert api changes from 6e9aaf0fce0214591225d1fa8aeafb8f5aa802cb
    * append linke_turb_forms.py to atmosphere.py
    * rm -rf atmosphere/
    
    Signed-off-by: Mark Mikofski <mark.mikofski@sunpowercorp.com>

--- pvlib/atmosphere/__init__.py
+++
@@ -1,8 +0,0 @@
-"""atmospheric equations"""
-
-from pvlib.atmosphere.core import (
-    absoluteairmass, alt2pres, first_solar_spectral_correction, gueymard94_pw,
-    pres2alt, relativeairmass, AIRMASS_MODELS, APPARENT_ZENITH_MODELS,
-    TRUE_ZENITH_MODELS
-)
-from pvlib.atmosphere.linke_turb_forms import kasten_96lt

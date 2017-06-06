37d84ecfff3d679ebdff4c9c124ac91448eca94c
uwe <uwe.krien@rl-institut.de>
2017-06-06 12:24:54 +0200

    fix test, because 'my name' is expected if my name is given

--- pvlib/test/test_pvsystem.py
+++ pvlib/test/test_pvsystem.py
@@ -751,7 +751,7 @@ def test_LocalizedPVSystem___repr__():
                                                   inverter='blarg',
                                                   name='my name')
 
-    expected = 'LocalizedPVSystem: \n  name: None\n  latitude: 32\n  longitude: -111\n  altitude: 0\n  tz: UTC\n  surface_tilt: 0\n  surface_azimuth: 180\n  module: blah\n  inverter: blarg\n  albedo: 0.25\n  racking_model: open_rack_cell_glassback'
+    expected = 'LocalizedPVSystem: \n  name: my name\n  latitude: 32\n  longitude: -111\n  altitude: 0\n  tz: UTC\n  surface_tilt: 0\n  surface_azimuth: 180\n  module: blah\n  inverter: blarg\n  albedo: 0.25\n  racking_model: open_rack_cell_glassback'
 
     assert localized_system.__repr__() == expected
 

c5248bb5c11839c8d8343e68b2dc6c40cc9c51b0
Mark Mikofski <bwana.marko@yahoo.com>
2018-01-31 02:04:19 -0800

    add the requires scipy decorator
    * still 2 tests in model chain strugling

--- pvlib/test/test_way_faster.py
+++ pvlib/test/test_way_faster.py
@@ -7,6 +7,7 @@ import logging
 import numpy as np
 from pvlib import pvsystem
 from pvlib.way_faster import faster_way, slower_way
+from conftest import requires_scipy
 
 logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
@@ -17,6 +18,7 @@ TCELL = 55
 CECMOD = pvsystem.retrieve_sam('cecmod')
 
 
+@requires_scipy
 def test_fast_spr_e20_327():
     spr_e20_327 = CECMOD.SunPower_SPR_E20_327
     x = pvsystem.calcparams_desoto(
@@ -50,6 +52,7 @@ def test_fast_spr_e20_327():
     return isc, voc, imp, vmp, pmp, pvs
 
 
+@requires_scipy
 def test_fast_fs_495():
     fs_495 = CECMOD.First_Solar_FS_495
     x = pvsystem.calcparams_desoto(
@@ -84,6 +87,7 @@ def test_fast_fs_495():
     return isc, voc, imp, vmp, pmp, i, v, p, pvs
 
 
+@requires_scipy
 def test_slow_spr_e20_327():
     spr_e20_327 = CECMOD.SunPower_SPR_E20_327
     x = pvsystem.calcparams_desoto(
@@ -117,6 +121,7 @@ def test_slow_spr_e20_327():
     return isc, voc, imp, vmp, pmp, pvs
 
 
+@requires_scipy
 def test_slow_fs_495():
     fs_495 = CECMOD.First_Solar_FS_495
     x = pvsystem.calcparams_desoto(

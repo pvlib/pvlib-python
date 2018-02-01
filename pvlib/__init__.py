086e73f1a2f6ed4f02f670d50ceed5dae927a613
Mark Mikofski <bwana.marko@yahoo.com>
2018-01-31 17:27:49 -0800

    okay I think I fixed it, now return pvlib module objects

--- pvlib/__init__.py
+++ pvlib/__init__.py
@@ -1,15 +1,15 @@
 import logging
 logging.basicConfig()
 from pvlib.version import __version__
-# from pvlib import tools
-# from pvlib import atmosphere
-# from pvlib import clearsky
-# # from pvlib import forecast
-# from pvlib import irradiance
-# from pvlib import location
-# from pvlib import solarposition
-# from pvlib import tmy
-# from pvlib import tracking
-# from pvlib import pvsystem
-# from pvlib import spa
-# from pvlib import modelchain
+from pvlib import tools
+from pvlib import atmosphere
+from pvlib import clearsky
+# from pvlib import forecast
+from pvlib import irradiance
+from pvlib import location
+from pvlib import solarposition
+from pvlib import tmy
+from pvlib import tracking
+from pvlib import pvsystem
+from pvlib import spa
+from pvlib import modelchain

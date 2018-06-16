bd3d734bd81cc18379d4118d24d9f557bec901ab
Will Holmgren <william.holmgren@gmail.com>
2018-06-15 18:10:30 -0700

    remove extra comments

--- pvlib/test/test_conftest.py
+++ pvlib/test/test_conftest.py
@@ -3,16 +3,6 @@ import pytest
 from conftest import fail_on_pvlib_version
 
 
-# try:
-#     @fail_on_pvlib_version('0.0')
-#     def test_fail_on_pvlib_version():
-#         pass
-# except Exception:
-#     pass
-# else:
-#     raise Exception('This test should fail on the decorator, not right here.')
-
-
 @pytest.mark.xfail(strict=True,
                    reason='fail_on_pvlib_version should cause test to fail')
 def test_fail_on_pvlib_version():

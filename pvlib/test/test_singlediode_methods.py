9d96585cb81678a49a68eaf3749a357eeefe24b3
Mark Mikofski <bwana.marko@yahoo.com>
2018-07-31 15:42:13 -0700

    WIP: ENH: TST: BUG: in test compare desoto vs. pvsyst

--- pvlib/test/test_singlediode_methods.py
+++ pvlib/test/test_singlediode_methods.py
@@ -4,7 +4,7 @@ testing single-diode methods using JW Bishop 1988
 
 import numpy as np
 from pvlib import pvsystem
-from pvlib.singlediode_methods import bishop88, estimate_voc
+from pvlib.singlediode_methods import bishop88, estimate_voc, VOLTAGE_BUILTIN
 from conftest import requires_scipy
 
 POA = 888
@@ -128,43 +128,101 @@ def test_brentq_fs_495():
     return isc, voc, imp, vmp, pmp, i, v, pvs
 
 
-def test_fs_495_recombination_loss():
+def pvsyst_fs_495():
+    """
+    PVSyst First Solar FS-495 parameters.
+
+    Returns
+    -------
+    dictionary of PVSyst First Solar FS-495 parameters
+    """
+    fs_495 = dict(d2mutau=1.31, alpha_sc=0.00039, gamma_ref=1.48,
+                  mu_gamma=0.001, I_o_ref=0.962e-9, R_sh_ref=5000,
+                  R_sh_0=12500, R_sh_exp=3.1, R_s=4.6, beta_oc=-0.2116,
+                  EgRef=1.475, cells_in_series=108, cells_in_parallel=2,
+                  I_sc_ref=1.55, V_oc_ref=86.5, I_mp_ref=1.4,
+                  V_mp_ref=67.85,
+                  temp_ref=25, irrad_ref=1000)
+    Vt = 0.025693001600485238  # thermal voltage at reference (V)
+    nNsVt = fs_495['cells_in_series'] * fs_495['gamma_ref'] * Vt
+    Vd = fs_495['I_sc_ref'] * fs_495['R_s']  # diode voltage at short circuit
+    Id = fs_495['I_o_ref'] * (np.exp(Vd / nNsVt) - 1)  # diode current (A)
+    Ish = Vd / fs_495['R_sh_ref']  # shunt current (A)
+    # builtin potential difference (V)
+    dv = VOLTAGE_BUILTIN * fs_495['cells_in_series'] - Vd
+    # calculate photo-generated current at reference condition (A)
+    fs_495['I_L_ref'] = (
+        (fs_495['I_sc_ref'] + Id + Ish) / (1 - fs_495['d2mutau'] / dv)
+    )
+    return fs_495
+
+
+PVSYST_FS_495 = pvsyst_fs_495()  # PVSyst First Solar FS-495 parameters
+
+
+def test_pvsyst_fs_495_recombination_loss():
     """test pvsystem.singlediode with Brent method on SPR-E20-327"""
-    fs_495 = CECMOD.First_Solar_FS_495
-    d2mutau_fs_495 = 1.3  # pvsyst recombination loss parameter (volts)
-    x = pvsystem.calcparams_desoto(
-        effective_irradiance=POA, temp_cell=TCELL,
-        alpha_sc=fs_495.alpha_sc, a_ref=fs_495.a_ref, I_L_ref=fs_495.I_L_ref,
-        I_o_ref=fs_495.I_o_ref, R_sh_ref=fs_495.R_sh_ref, R_s=fs_495.R_s,
-        EgRef=1.475, dEgdT=-0.0003)
-    il, io, rs, rsh, nnsvt = x
-    voc_est = estimate_voc(photocurrent=il, saturation_current=io,
-                           nNsVth=nnsvt)
-    vd = np.linspace(0, voc_est, 1000)
-    noloss = bishop88(
-        diode_voltage=vd, photocurrent=il, saturation_current=io,
-        resistance_series=rs, resistance_shunt=rsh, nNsVth=nnsvt,
-             d2mutau=0, cells_in_series=fs_495.N_s/2, gradients=False)
-    wloss = bishop88(
-        diode_voltage=vd, photocurrent=il, saturation_current=io,
-        resistance_series=rs, resistance_shunt=rsh, nNsVth=nnsvt,
-        d2mutau=d2mutau_fs_495, cells_in_series=fs_495.N_s/2, gradients=False)
+    poa, temp_cell = 1000.0, 25.0  # test conditions
+
+    # first evaluate DeSoto model
+    cec_fs_495 = CECMOD.First_Solar_FS_495  # CEC parameters for
+    il_cec, io_cec, rs_cec, rsh_cec, nnsvt_cec = pvsystem.calcparams_desoto(
+        effective_irradiance=poa, temp_cell=temp_cell,
+        alpha_sc=cec_fs_495.alpha_sc, a_ref=cec_fs_495.a_ref,
+        I_L_ref=cec_fs_495.I_L_ref, I_o_ref=cec_fs_495.I_o_ref,
+        R_sh_ref=cec_fs_495.R_sh_ref, R_s=cec_fs_495.R_s,
+        EgRef=1.475, dEgdT=-0.0003
+    )
+    voc_est_cec = estimate_voc(photocurrent=il_cec, saturation_current=io_cec,
+                               nNsVth=nnsvt_cec)
+    vd_cec = np.linspace(0, voc_est_cec, 1000)
+    desoto = bishop88(
+        diode_voltage=vd_cec, photocurrent=il_cec, saturation_current=io_cec,
+        resistance_series=rs_cec, resistance_shunt=rsh_cec, nNsVth=nnsvt_cec
+    )
+
+    # now evaluate PVSyst model with thin-film recombination loss current
+    pvsyst_fs_495 = PVSYST_FS_495
+    x = pvsystem.calcparams_pvsyst(
+        effective_irradiance=poa, temp_cell=temp_cell,
+        alpha_sc=pvsyst_fs_495['alpha_sc'],
+        gamma_ref=pvsyst_fs_495['gamma_ref'],
+        mu_gamma=pvsyst_fs_495['mu_gamma'], I_L_ref=pvsyst_fs_495['I_L_ref'],
+        I_o_ref=pvsyst_fs_495['I_o_ref'], R_sh_ref=pvsyst_fs_495['R_sh_ref'],
+        R_sh_0=pvsyst_fs_495['R_sh_0'], R_sh_exp=pvsyst_fs_495['R_sh_exp'],
+        R_s=pvsyst_fs_495['R_s'],
+        cells_in_series=pvsyst_fs_495['cells_in_series'],
+        EgRef=pvsyst_fs_495['EgRef']
+    )
+    il_pvsyst, io_pvsyst, rs_pvsyst, rsh_pvsyst, nnsvt_pvsyst = x
+    voc_est_pvsyst = estimate_voc(photocurrent=il_pvsyst,
+                                  saturation_current=io_pvsyst,
+                                  nNsVth=nnsvt_pvsyst)
+    vd_pvsyst = np.linspace(0, voc_est_pvsyst, 1000)
+    pvsyst = bishop88(
+        diode_voltage=vd_pvsyst, photocurrent=il_pvsyst,
+        saturation_current=io_pvsyst, resistance_series=rs_pvsyst,
+        resistance_shunt=rsh_pvsyst, nNsVth=nnsvt_pvsyst,
+        d2mutau=pvsyst_fs_495['d2mutau'],
+        cells_in_series=pvsyst_fs_495['cells_in_series']
+    )
+
     # test expected change in max power
-    assert np.isclose(max(noloss[2]) - max(wloss[2]), 2.906285338766608)
+    assert np.isclose(max(desoto[2]) - max(pvsyst[2]), 0.01949420697212645)
     # test expected change in short circuit current
-    isc_noloss = np.interp(0, noloss[1], noloss[0])
-    isc_wloss = np.interp(0, wloss[1], wloss[0])
-    assert np.isclose(isc_noloss - isc_wloss, 0.020758929386382796)
+    isc_desoto = np.interp(0, desoto[1], desoto[0])
+    isc_pvsyst = np.interp(0, pvsyst[1], pvsyst[0])
+    assert np.isclose(isc_desoto - isc_pvsyst, -7.955827628380874e-05)
     # test expected change in open circuit current
-    voc_noloss = np.interp(0, noloss[0][::-1], noloss[1][::-1])
-    voc_wloss = np.interp(0, wloss[0][::-1], wloss[1][::-1])
-    assert np.isclose(voc_noloss - voc_wloss, 0.2098581317348902)
-    return noloss, wloss
+    voc_desoto = np.interp(0, desoto[0][::-1], desoto[1][::-1])
+    voc_pvsyst = np.interp(0, pvsyst[0][::-1], pvsyst[1][::-1])
+    assert np.isclose(voc_desoto - voc_pvsyst, -0.04184247739671321)
+    return desoto, pvsyst
 
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
-    a, b = test_fs_495_recombination_loss()
+    a, b = test_pvsyst_fs_495_recombination_loss()
     ab0 = np.interp(a[1], b[1], b[0])
     pmpa, pmpb = max(a[2]), max(b[2])
     isca = np.interp(0, a[1], a[0])
@@ -176,13 +234,13 @@ if __name__ == '__main__':
              a[1], a[2] - ab0 * a[1])
     plt.plot([a[1][0], a[1][-1]], [pmpa]*2, ':',
              [b[1][0], b[1][-1]], [pmpb]*2, ':')
-    plt.legend(['no loss', 'w/loss interp', 'w/loss', '$\Delta P_{mp}$',
-                '$P_{mp,noloss}=%4.1f$' % pmpa,
-                '$P_{mp,wloss}=%4.1f$' % pmpb])
+    plt.legend(['DeSoto', 'PVSyst interpolated', 'PVSyst', '$\Delta P$',
+                '$P_{mp,DeSoto}=%4.1f$' % pmpa,
+                '$P_{mp,PVSyst}=%4.1f$' % pmpb])
     plt.grid()
     plt.xlabel('voltage (V)')
-    plt.ylabel('power (P)')
-    plt.title('FS-495 power with and w/o recombination loss')
+    plt.ylabel('power (W)')
+    plt.title('FS-495 power, DeSoto vs. PVSyst with recombination loss')
     f1.show()
     f2 = plt.figure('current')
     plt.plot(a[1], a[0], a[1], ab0, b[1], b[0], '--', a[1], a[0] - ab0)
@@ -190,13 +248,13 @@ if __name__ == '__main__':
              [b[1][0], b[1][-1]], [iscb]*2, ':')
     plt.plot([voca]*2, [a[0][0], a[0][-1]], ':',
              [vocb]*2, [b[0][0], b[0][-1]], ':')
-    plt.legend(['no loss', 'w/loss interp', 'w/loss', '$\Delta I$',
-                '$I_{sc,noloss}=%4.2f$' % isca,
-                '$I_{sc,wloss}=%4.2f$' % iscb,
-                '$V_{oc,noloss}=%4.1f$' % voca,
-                '$V_{oc,wloss}=%4.1f$' % vocb])
+    plt.legend(['DeSoto', 'PVSyst interpolated', 'PVSyst', '$\Delta I$',
+                '$I_{sc,DeSoto}=%4.2f$' % isca,
+                '$I_{sc,PVSyst}=%4.2f$' % iscb,
+                '$V_{oc,DeSoto}=%4.1f$' % voca,
+                '$V_{oc,PVSyst}=%4.1f$' % vocb])
     plt.grid()
     plt.xlabel('voltage (V)')
-    plt.ylabel('current (I)')
-    plt.title('FS-495 IV-curve with and w/o recombination loss')
+    plt.ylabel('current (A)')
+    plt.title('FS-495 IV-curve, DeSoto vs. PVSyst with recombination loss')
     f2.show()

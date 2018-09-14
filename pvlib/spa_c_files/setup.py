44c2037592ad5acda97a4f2a7f50dd46bc3bb39c
Mark Mikofski <bwana.marko@yahoo.com>
2018-09-14 12:06:14 -0700

    with the function

--- pvlib/spa_c_files/setup.py
+++ pvlib/spa_c_files/setup.py
@@ -10,7 +10,7 @@ DIRNAME = os.path.dirname(__file__)
 # patch spa.c
 with open(os.path.join(DIRNAME, 'spa.c'), 'rb') as f:
     SPA_C = f.read()
-# replace timezone with time_zone to avoid a nameclash the function
+# replace timezone with time_zone to avoid nameclash with the function
 # __timezone which is defined by a MACRO in pyconfig.h as timezone
 # see https://bugs.python.org/issue24643
 SPA_C = SPA_C.replace(b'timezone', b'time_zone')
@@ -20,7 +20,7 @@ with open(os.path.join(DIRNAME, 'spa.c'), 'wb') as f:
 # patch spa.h
 with open(os.path.join(DIRNAME, 'spa.h'), 'rb') as f:
     SPA_H = f.read()
-# replace timezone with time_zone to avoid a nameclash the function
+# replace timezone with time_zone to avoid nameclash with the function
 # __timezone which is defined by a MACRO in pyconfig.h as timezone
 # see https://bugs.python.org/issue24643
 SPA_H = SPA_H.replace(b'timezone', b'time_zone')

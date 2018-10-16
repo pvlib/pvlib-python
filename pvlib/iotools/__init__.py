ef5418e778bc0c72fa7b2f7e2c3b4dcbf3fb552c
Leland Boeman <lboeman@email.arizona.edu>
2018-10-16 13:17:50 -0700

    fix comment style on import

--- pvlib/iotools/__init__.py
+++ pvlib/iotools/__init__.py
@@ -3,4 +3,4 @@ from pvlib.iotools.tmy import read_tmy3  # noqa: F401
 from pvlib.iotools.srml import read_srml  # noqa: F401
 from pvlib.iotools.srml import read_srml_month_from_solardat  # noqa: F401
 from pvlib.iotools.surfrad import read_surfrad  # noqa: F401
-from pvlib.iotools.midc import read_midc # noqa: F401
+from pvlib.iotools.midc import read_midc  # noqa: F401

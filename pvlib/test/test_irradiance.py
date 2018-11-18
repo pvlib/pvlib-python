cd7a596213aaaa208556a93b693a4ebfbc9eee6d
Kevin Anderson <kevinanderso@gmail.com>
2018-11-18 11:49:48 -0500

    added tests for irradiance.clearsky_index

--- pvlib/test/test_irradiance.py
+++ pvlib/test/test_irradiance.py
@@ -718,6 +718,43 @@ def test_kt_kt_prime_factor(airmass_kt):
     assert_allclose(out, expected, atol=1e-5)
 
 
+def test_clearsky_index():
+    ghi = np.array([-1., 0., 1., 500., 1000.])
+    ghi_measured, ghi_modeled = np.meshgrid(ghi, ghi)
+    # default max_clearsky_index
+    with np.errstate(invalid='ignore', divide='ignore'):
+        out = irradiance.clearsky_index(ghi_measured, ghi_modeled)
+    expected = np.array(
+        [[1.   , 0.   , 0.   , 0.   , 0.  ],
+         [0.   , nan  , 2.   , 2.   , 2.  ],
+         [0.   , 0.   , 1.   , 2.   , 2.  ],
+         [0.   , 0.   , 0.002, 1.   , 2.  ],
+         [0.   , 0.   , 0.001, 0.5  , 1.  ]])
+    assert_allclose(out, expected, atol=0.001)
+    # specify max_clearsky_index
+    with np.errstate(invalid='ignore', divide='ignore'):
+        out = irradiance.clearsky_index(ghi_measured, ghi_modeled,
+                                        max_clearsky_index=1.5)
+    expected = np.array(
+        [[1.   , 0.   , 0.   , 0.   , 0.  ],
+         [0.   , nan  , 1.5  , 1.5  , 1.5 ],
+         [0.   , 0.   , 1.   , 1.5  , 1.5 ],
+         [0.   , 0.   , 0.002, 1.   , 1.5 ],
+         [0.   , 0.   , 0.001, 0.5  , 1.  ]])
+    assert_allclose(out, expected, atol=0.001)
+    # scalars
+    out = irradiance.clearsky_index(10, 1000)
+    expected = 0.1
+    assert_allclose(out, expected, atol=0.001)
+    # series
+    times = pd.DatetimeIndex(start='20180601', periods=2, freq='12H')
+    ghi_measured = pd.Series([100,  500], index=times)
+    ghi_modeled  = pd.Series([500, 1000], index=times)
+    out = irradiance.clearness_index(ghi_measured, ghi_modeled)
+    expected = pd.Series([0.2, 0.5], index=times)
+    assert_series_equal(out, expected)
+
+
 def test_clearness_index():
     ghi = np.array([-1, 0, 1, 1000])
     solar_zenith = np.array([180, 90, 89.999, 0])

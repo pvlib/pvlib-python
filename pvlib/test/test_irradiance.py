8acfd58bcc7750005fee02091f502ec6e643cb4b
anomam <marc.abouanoma@sunpowercorp.com>
2018-12-13 18:17:34 +0900

    Add test inputs from github repo README.md TLDR section

--- pvlib/test/test_irradiance.py
+++ pvlib/test/test_irradiance.py
@@ -861,23 +861,42 @@ def test_clearness_index_zenith_independent(airmass_kt):
 @requires_future
 @requires_pvfactors
 def test_pvfactors_bifacial_irradiance():
+    """ Test that pvfactors is functional, using the TLDR inputs of the
+    package github repo README.md file"""
 
-    solar_azimuth = None
-    solar_zenith = None
-    surface_azimuth = None
-    surface_tilt = None
-    times = None
-    dni = None
-    dhi = None
-    gcr = None
-    pvrow_height = None
-    pvrow_width = None
-    albedo = None
+    # Create some inputs
+    timestamps = np.array([datetime.datetime(2017, 8, 31, 11),
+                           datetime.datetime(2017, 8, 31, 12)])
+    solar_zenith = np.array([20., 10.])
+    solar_azimuth = np.array([110., 140.])
+    surface_tilt = np.array([10., 0.])
+    surface_azimuth = np.array([90., 90.])
+    dni = np.array([1000., 300.])
+    dhi = np.array([50., 500.])
+    gcr = 0.4
+    pvrow_height = 1.75
+    pvrow_width = 2.44
+    albedo = 0.2
+    n_pvrows = 3
+    index_observed_pvrow = 1
+    rho_front_pvrow = 0.03
+    rho_back_pvrow = 0.05
+    horizon_band_angle = 15.
 
+    # Run calculations serially
     irradiance.pvfactors_bifacial_irradiance(
         solar_azimuth, solar_zenith, surface_azimuth, surface_tilt,
         times, dni, dhi, gcr, pvrow_height, pvrow_width, albedo,
-        n_pvrows=3, index_observed_pvrow=1,
-        rho_front_pvrow=0.03, rho_back_pvrow=0.05,
-        horizon_band_angle=15.,
+        n_pvrows=n_pvrows, index_observed_pvrow=index_observed_pvrow,
+        rho_front_pvrow=rho_front_pvrow, rho_back_pvrow=rho_back_pvrow,
+        horizon_band_angle=horizon_band_angle,
+        run_parallel_calculations=False, n_workers_for_parallel_calcs=None)
+
+    # Run calculations in parallel
+    irradiance.pvfactors_bifacial_irradiance(
+        solar_azimuth, solar_zenith, surface_azimuth, surface_tilt,
+        times, dni, dhi, gcr, pvrow_height, pvrow_width, albedo,
+        n_pvrows=n_pvrows, index_observed_pvrow=index_observed_pvrow,
+        rho_front_pvrow=rho_front_pvrow, rho_back_pvrow=rho_back_pvrow,
+        horizon_band_angle=horizon_band_angle,
         run_parallel_calculations=True, n_workers_for_parallel_calcs=None)

5d2a6c82449942346967fe7b9821c0eb524e3816
Cameron Stark <CameronTStark@users.noreply.github.com>
2019-01-17 09:10:48 -0700

    Add PVSystem pvsyst_celltemp methods (#636)
    * Add PVSystem pvsyst_celltemp methods
    
    * fix _delta_kt_prime_dirint end values (#638)
    
    * fix _delta_kt_prime_dirint end values
    
    * syntax, test fixes:
    
    * syntax and test fixes
    
    * more test fixes
    
    * more test fixes
    
    * and more test fixes
    
    * and still more test fixes
    
    * add comments, update whatsnew
    
    * delete space
    
    * Add PVSystem pvsyst_celltemp methods
    
    * create global TEMP_MODELS for sapm and pvsyst
    
    * correct model choice in pvsyst_celltemp function
    
    * Add PVSystem pvsyst_celltemp methods
    
    * create global TEMP_MODELS for sapm and pvsyst
    
    * correct model choice in pvsyst_celltemp function
    
    * convert sapm temp models to tuples
    
    * implement _build_kwargs into pvsyst_celltemp
    
    * amend with non-default parameters and new api
    
    * Stickler-ci polish

--- pvlib/pvsystem.py
+++ pvlib/pvsystem.py
@@ -46,6 +46,16 @@ DC_MODEL_PARAMS = {
 }
 
 
+TEMP_MODELS = {
+    'sapm': {'open_rack_cell_glassback': (-3.47, -.0594, 3),
+             'roof_mount_cell_glassback': (-2.98, -.0471, 1),
+             'open_rack_cell_polymerback': (-3.56, -.0750, 3),
+             'insulated_back_polymerback': (-2.81, -.0455, 0),
+             'open_rack_polymer_thinfilm_steel': (-3.58, -.113, 3),
+             '22x_concentrator_tracker': (-3.23, -.130, 13)},
+    'pvsyst': {'freestanding': (29.0, 0), 'insulated': (15.0, 0)}
+}
+
 # not sure if this belongs in the pvsystem module.
 # maybe something more like core.py? It may eventually grow to
 # import a lot more functionality from other modules.
@@ -510,6 +520,23 @@ class PVSystem(object):
         return sapm_effective_irradiance(
             poa_direct, poa_diffuse, airmass_absolute, aoi,
             self.module_parameters, reference_irradiance=reference_irradiance)
+
+    def pvsyst_celltemp(self, poa_global, wind_speed, temp_air):
+        """Uses :py:func:`pvsyst_celltemp` to calculate module temperatures
+        based on ``self.racking_model`` and the input parameters.
+
+        Parameters
+        ----------
+        See pvsystem.pvsyst_celltemp for details
+
+        Returns
+        -------
+        See pvsystem.pvsyst_celltemp for details
+        """
+        kwargs = _build_kwargs(['eta_m', 'alpha_absorption'],
+                               self.module_parameters)
+        return pvsyst_celltemp(poa_global, wind_speed, temp_air,
+                               temp_model=self.racking_model, **kwargs)
 
     def first_solar_spectral_loss(self, pw, airmass_absolute):
 
@@ -1860,13 +1887,7 @@ def sapm_celltemp(poa_global, wind_speed, temp_air,
     sapm
     '''
 
-    temp_models = {'open_rack_cell_glassback': [-3.47, -.0594, 3],
-                   'roof_mount_cell_glassback': [-2.98, -.0471, 1],
-                   'open_rack_cell_polymerback': [-3.56, -.0750, 3],
-                   'insulated_back_polymerback': [-2.81, -.0455, 0],
-                   'open_rack_polymer_thinfilm_steel': [-3.58, -.113, 3],
-                   '22x_concentrator_tracker': [-3.23, -.130, 13]
-                   }
+    temp_models = TEMP_MODELS['sapm']
 
     if isinstance(model, str):
         model = temp_models[model.lower()]
@@ -1880,9 +1901,9 @@ def sapm_celltemp(poa_global, wind_speed, temp_air,
 
     E0 = 1000.  # Reference irradiance
 
-    temp_module = pd.Series(poa_global*np.exp(a + b*wind_speed) + temp_air)
+    temp_module = pd.Series(poa_global * np.exp(a + b * wind_speed) + temp_air)
 
-    temp_cell = temp_module + (poa_global / E0)*(deltaT)
+    temp_cell = temp_module + (poa_global / E0) * (deltaT)
 
     return pd.DataFrame({'temp_cell': temp_cell, 'temp_module': temp_module})
 
@@ -1944,7 +1965,7 @@ def pvsyst_celltemp(poa_global, wind_speed, temp_air, eta_m=0.1,
     photovoltaic modules." Progress in Photovoltaics 16(4): 307-315.
     """
 
-    temp_models = {"freestanding": (29.0, 0), "insulated": (15.0, 0)}
+    temp_models = TEMP_MODELS['pvsyst']
 
     if isinstance(temp_model, str):
         natural_convenction_coeff, forced_convection_coeff = temp_models[

a6373c872bc7f1d19ccbaa0fb0bdcdae6f562543
Bennet Meyers <bennetm@stanford.edu>
2019-02-18 10:58:05 -0800

    importing new function in package init

--- pvlib/iotools/__init__.py
+++ pvlib/iotools/__init__.py
@@ -7,3 +7,4 @@ from pvlib.iotools.midc import read_midc  # noqa: F401
 from pvlib.iotools.midc import read_midc_raw_data_from_nrel  # noqa: F401
 from pvlib.iotools.ecmwf_macc import read_ecmwf_macc  # noqa: F401
 from pvlib.iotools.ecmwf_macc import get_ecmwf_macc  # noqa: F401
+from pvlib.iotools.pvdaq import get_pvdaq_data

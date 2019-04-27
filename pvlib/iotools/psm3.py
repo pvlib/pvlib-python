c52d5b716b1841da460950136b1d747460568f23
Mark Mikofski <bwana.marko@yahoo.com>
2019-04-27 00:44:30 -0700

    use astype, fix typos, change param names to match API

--- pvlib/iotools/psm3.py
+++ pvlib/iotools/psm3.py
@@ -12,13 +12,13 @@ import pytz
 
 URL = "http://developer.nrel.gov/api/solar/nsrdb_psm3_download.csv"
 
-# 'relative_humidity', 'total_precipitable_water' are note available
+# 'relative_humidity', 'total_precipitable_water' are not available
 ATTRIBUTES = [
     'air_temperature', 'dew_point', 'dhi', 'dni', 'ghi', 'surface_albedo',
     'surface_pressure', 'wind_direction', 'wind_speed']
 
 
-def get_psm3(latitude, longitude, tmy='tmy', interval=60):
+def get_psm3(latitude, longitude, names='tmy', interval=60):
     """
     Get PSM3 data
 
@@ -28,34 +28,37 @@ def get_psm3(latitude, longitude, tmy='tmy', interval=60):
         in decimal degrees, between -90 and 90, north is positive
     longitude : float or int
         in decimal degrees, between -180 and 180, east is positive
-    tmy : str
-        PSM3 parameter "name", see notes below for options, default ``'tmy'``
-    interval : integer
-        interval size in minutes, can be only either 30 or 60, default 60
+    names : str
+        PSM3 API parameter specifing year or TMY variant to download, see notes
+        below for options, default: ``'tmy'``
+    interval : int
+        interval size in minutes, can be only either 30 or 60, default: 60
 
     Returns
     -------
     headers : dict
-        meta data from NREL PSM-3 about the record, see notes for fields
+        metadata from NREL PSM3 about the record, see notes for fields
     data : pandas.DataFrame
         timeseries data from NREL PSM3
 
     Raises
     ------
     requests.HTTPError
+        if the request return status is not ok then the ``'errors'`` from the
+        JSON response will be returned as an exception
 
     Notes
     -----
-    The ``tmy`` parameter 'names' must be a single value from the following
-    list:
+    The PSM3 API `names` parameter must be a single value from the following
+    list::
 
         ['1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005',
          '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013',
          '2014', '2015', '2016', '2017', 'tmy', 'tmy-2016', 'tmy-2017',
          'tdy-2017', 'tgy-2017']
 
-    The return is a tuple with two items. The first item is a header with meta
-    data from NREL PSM3 about the record containing the following fields:
+    The return is a tuple with two items. The first item is a header with
+    metadata from NREL PSM3 about the record containing the following fields:
 
     * Source
     * Location ID
@@ -78,16 +81,19 @@ def get_psm3(latitude, longitude, tmy='tmy', interval=60):
     * Surface Albedo Units
     * Version
 
+    The second item is dataframe with the timeseries data downloaded. The index
+    is a :py:class:`pandas.DatetimeIndex` of the 
+
     See Also
     --------
     pvlib.iotools.read_tmy2, pvlib.iotools.read_tmy3
 
     References
     ----------
-    [1] `NREL Developer Network - Physical Solar Model (PSM) v3
-        <https://developer.nrel.gov/docs/solar/nsrdb/psm3_data_download/>`_
-    [2] `NREL National Solar Radiation Database (NSRDB)
-        <https://nsrdb.nrel.gov/>`_
+
+    * `NREL Developer Network - Physical Solar Model (PSM) v3 <https://developer.nrel.gov/docs/solar/nsrdb/psm3_data_download/>`_
+    * `NREL National Solar Radiation Database (NSRDB) <https://nsrdb.nrel.gov/>`_
+
     """
     longitude = ('%9.4f' % longitude).strip()
     latitude = ('%8.4f' % latitude).strip()
@@ -99,7 +105,7 @@ def get_psm3(latitude, longitude, tmy='tmy', interval=60):
         'reason': 'Example',
         'mailing_list': 'true',
         'wkt': 'POINT(%s %s)' % (longitude, latitude),
-        'names': tmy,
+        'names': names,
         'attributes':  ','.join(ATTRIBUTES),
         'leap_day': 'false',
         'utc': 'false',
@@ -120,11 +126,11 @@ def get_psm3(latitude, longitude, tmy='tmy', interval=60):
         z['Elevation'] = int(z['Elevation'])
         tz = pytz.timezone('Etc/GMT%+d' % -z['Time Zone'])
         w = pd.DataFrame(y[3:], columns=y[2], dtype=float)
-        w.Year = w.Year.apply(lambda yr: int(yr))
-        w.Month = w.Month.apply(lambda mt: int(mt))
-        w.Day = w.Day.apply(lambda dy: int(dy))
-        w.Hour = w.Hour.apply(lambda hr: int(hr))
-        w.Minute = w.Minute.apply(lambda mn: int(mn))
+        w.Year = w.Year.astype(int)
+        w.Month = w.Month.astype(int)
+        w.Day = w.Day.astype(int)
+        w.Hour = w.Hour.astype(int)
+        w.Minute = w.Minute.astype(int)
         w.index = w.apply(
             lambda dt: tz.localize(datetime.datetime(
                 int(dt.Year), int(dt.Month), int(dt.Day),

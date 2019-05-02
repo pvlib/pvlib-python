8884c7ff0e4f0789fddf83cd93aca738208e4894
Will Holmgren <william.holmgren@gmail.com>
2019-05-02 09:15:16 -0700

    remove "required" for api_key param in get_psm3
    Co-Authored-By: mikofski <bwana.marko@yahoo.com>

--- pvlib/iotools/psm3.py
+++ pvlib/iotools/psm3.py
@@ -34,7 +34,7 @@ def get_psm3(latitude, longitude, api_key, email, names='tmy', interval=60,
     longitude : float or int
         in decimal degrees, between -180 and 180, east is positive
     api_key : str
-        required NREL Developer Network API key
+        NREL Developer Network API key
     email : str
         required, system uses this to automatically communicate messages back
         to the user only if necessary

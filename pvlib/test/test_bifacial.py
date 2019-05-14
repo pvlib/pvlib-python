9930e2c513617bc0bb0dc8a72c7d9e45c071d63a
anomam <marc.abouanoma@sunpowercorp.com>
2019-05-14 17:44:52 +0900

    Update function and tests to work with new pvfactors version 101

--- pvlib/test/test_bifacial.py
+++ pvlib/test/test_bifacial.py
@@ -18,6 +18,7 @@ def test_pvfactors_timeseries():
     solar_azimuth = [110., 140.]
     surface_tilt = [10., 0.]
     surface_azimuth = [90., 90.]
+    axis_azimuth = 0.
     dni = [1000., 300.]
     dhi = [50., 500.]
     gcr = 0.4
@@ -31,38 +32,38 @@ def test_pvfactors_timeseries():
     horizon_band_angle = 15.
 
     # Expected values
-    expected_ipoa_front = pd.Series([1034.96216923, 795.4423259],
+    expected_ipoa_front = pd.Series([1034.95474708997, 795.4423259036623],
                                     index=timestamps,
-                                    name=(1, 'front', 'qinc'))
-    expected_ipoa_back = pd.Series([92.11871485, 70.39404124],
+                                    name=('total_inc_front'))
+    expected_ipoa_back = pd.Series([91.88707460262768, 78.05831585685215],
                                    index=timestamps,
-                                   name=(1, 'back', 'qinc'))
+                                   name=('total_inc_back'))
 
     # Test serial calculations
-    ipoa_front, ipoa_back, df_registries = pvfactors_timeseries(
+    ipoa_front, ipoa_back = pvfactors_timeseries(
         solar_azimuth, solar_zenith, surface_azimuth, surface_tilt,
+        axis_azimuth,
         timestamps, dni, dhi, gcr, pvrow_height, pvrow_width, albedo,
         n_pvrows=n_pvrows, index_observed_pvrow=index_observed_pvrow,
         rho_front_pvrow=rho_front_pvrow, rho_back_pvrow=rho_back_pvrow,
         horizon_band_angle=horizon_band_angle,
-        run_parallel_calculations=False, n_workers_for_parallel_calcs=None)
+        run_parallel_calculations=False, n_workers_for_parallel_calcs=-1)
 
     pd.testing.assert_series_equal(ipoa_front, expected_ipoa_front)
     pd.testing.assert_series_equal(ipoa_back, expected_ipoa_back)
-    pd.testing.assert_index_equal(timestamps, df_registries.index.unique())
 
     # Run calculations in parallel
-    ipoa_front, ipoa_back, df_registries = pvfactors_timeseries(
+    ipoa_front, ipoa_back = pvfactors_timeseries(
         solar_azimuth, solar_zenith, surface_azimuth, surface_tilt,
+        axis_azimuth,
         timestamps, dni, dhi, gcr, pvrow_height, pvrow_width, albedo,
         n_pvrows=n_pvrows, index_observed_pvrow=index_observed_pvrow,
         rho_front_pvrow=rho_front_pvrow, rho_back_pvrow=rho_back_pvrow,
         horizon_band_angle=horizon_band_angle,
-        run_parallel_calculations=True, n_workers_for_parallel_calcs=None)
+        run_parallel_calculations=True, n_workers_for_parallel_calcs=-1)
 
     pd.testing.assert_series_equal(ipoa_front, expected_ipoa_front)
     pd.testing.assert_series_equal(ipoa_back, expected_ipoa_back)
-    pd.testing.assert_index_equal(timestamps, df_registries.index.unique())
 
 
 @requires_pvfactors
@@ -79,6 +80,7 @@ def test_pvfactors_timeseries_pandas_inputs():
     solar_azimuth = pd.Series([110., 140.])
     surface_tilt = pd.Series([10., 0.])
     surface_azimuth = pd.Series([90., 90.])
+    axis_azimuth = 0.
     dni = pd.Series([1000., 300.])
     dhi = pd.Series([50., 500.])
     gcr = 0.4
@@ -92,35 +94,35 @@ def test_pvfactors_timeseries_pandas_inputs():
     horizon_band_angle = 15.
 
     # Expected values
-    expected_ipoa_front = pd.Series([1034.96216923, 795.4423259],
+    expected_ipoa_front = pd.Series([1034.95474708997, 795.4423259036623],
                                     index=timestamps,
-                                    name=(1, 'front', 'qinc'))
-    expected_ipoa_back = pd.Series([92.11871485, 70.39404124],
+                                    name=('total_inc_front'))
+    expected_ipoa_back = pd.Series([91.88707460262768, 78.05831585685215],
                                    index=timestamps,
-                                   name=(1, 'back', 'qinc'))
+                                   name=('total_inc_back'))
 
     # Test serial calculations
-    ipoa_front, ipoa_back, df_registries = pvfactors_timeseries(
+    ipoa_front, ipoa_back = pvfactors_timeseries(
         solar_azimuth, solar_zenith, surface_azimuth, surface_tilt,
+        axis_azimuth,
         timestamps, dni, dhi, gcr, pvrow_height, pvrow_width, albedo,
         n_pvrows=n_pvrows, index_observed_pvrow=index_observed_pvrow,
         rho_front_pvrow=rho_front_pvrow, rho_back_pvrow=rho_back_pvrow,
         horizon_band_angle=horizon_band_angle,
-        run_parallel_calculations=False, n_workers_for_parallel_calcs=None)
+        run_parallel_calculations=False, n_workers_for_parallel_calcs=-1)
 
     pd.testing.assert_series_equal(ipoa_front, expected_ipoa_front)
     pd.testing.assert_series_equal(ipoa_back, expected_ipoa_back)
-    pd.testing.assert_index_equal(timestamps, df_registries.index.unique())
 
     # Run calculations in parallel
-    ipoa_front, ipoa_back, df_registries = pvfactors_timeseries(
+    ipoa_front, ipoa_back = pvfactors_timeseries(
         solar_azimuth, solar_zenith, surface_azimuth, surface_tilt,
+        axis_azimuth,
         timestamps, dni, dhi, gcr, pvrow_height, pvrow_width, albedo,
         n_pvrows=n_pvrows, index_observed_pvrow=index_observed_pvrow,
         rho_front_pvrow=rho_front_pvrow, rho_back_pvrow=rho_back_pvrow,
         horizon_band_angle=horizon_band_angle,
-        run_parallel_calculations=True, n_workers_for_parallel_calcs=None)
+        run_parallel_calculations=True, n_workers_for_parallel_calcs=-1)
 
     pd.testing.assert_series_equal(ipoa_front, expected_ipoa_front)
     pd.testing.assert_series_equal(ipoa_back, expected_ipoa_back)
-    pd.testing.assert_index_equal(timestamps, df_registries.index.unique())

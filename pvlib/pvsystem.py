2177d78a111e287c86ee3d37f93ec0367dc30e7c
Anton Driesse <anton.driesse@pvperformancelabs.com>
2019-07-12 17:39:56 +0200

    transform docstring from matlab

--- pvlib/pvsystem.py
+++ pvlib/pvsystem.py
@@ -1066,7 +1066,61 @@ def physicaliam(aoi, n=1.526, K=4., L=0.002):
 
 def iam_martin_ruiz(aoi, a_r):
     '''
-    Determine the incidence angle modifier using the Martin & Ruiz model.
+    Determine the incidence angle modifier using the Martin
+    and Ruiz incident angle model
+
+    iam_martin_ruiz calculates the incidence angle modifier (angular
+    factor) as described by Martin and Ruiz in [1]. The information
+    required is the incident angle (theta) and the angular losses
+    coefficient (ar). Please note that [1] has a corrigendum which makes the
+    document much simpler to understand.
+
+    The incident angle modifier is defined as
+    [1-exp(-cos(theta/ar))] / [1-exp(-1/ar)], which is
+    presented as AL(alpha) = 1 - IAM in equation 4 of [1]. Thus IAM is
+    equal to 1 at theta = 0, and equal to 0 at theta = 90. IAM is a
+    column vector with the same number of elements as the largest input
+    vector.
+
+    Parameters
+    ----------
+    aoi : numeric, degrees
+        The angle of incidence between the module normal vector and the
+        sun-beam vector in degrees. Theta must be a numeric scalar or vector.
+        For any values of theta where abs(theta)>90, IAM is set to 0. For any
+        values of theta where -90 < theta < 0, theta is set to abs(theta) and
+        evaluated. A warning will be generated if any(theta<0 or theta>90).
+
+    ar : numeric
+        The angular losses coefficient described in equation 3 of [1].
+        This is an empirical dimensionless parameter. Values of ar are
+        generally on the order of 0.08 to 0.25 for flat-plate PV modules. ar
+        must be a numeric scalar or vector with all values > 0. If ar
+        is a vector, it must be the same size as all other input vectors.
+
+    Returns
+    -------
+    iam : numeric
+        The incident angle modifier
+
+
+
+    References
+    ----------
+    [1] N. Martin and J. M. Ruiz, "Calculation of the PV modules angular
+    losses under field conditions by means of an analytical model", Solar
+    Energy Materials & Solar Cells, vol. 70, pp. 25-38, 2001.
+
+    [2] N. Martin and J. M. Ruiz, "Corrigendum to 'Calculation of the PV
+    modules angular losses under field conditions by means of an
+    analytical model'", Solar Energy Materials & Solar Cells, vol. 110,
+    pp. 154, 2013.
+
+    See Also
+    --------
+    getaoi
+    physicaliam
+    ashraeiam
     '''
     raise NotImplementedError
 

7c36148ac3b0c98f8a474ef8d98f3f838e610282
Alexander Morgan <alexanderpmorgan@gmail.com>
2019-07-22 22:36:51 -0400

    Remove strict arguments.
    The case where these were relevant (Windows build using Python 2) is no
    longer used because Python 2 is no longer supported.

--- pvlib/test/test_conftest.py
+++ pvlib/test/test_conftest.py
@@ -3,8 +3,7 @@ import pytest
 from conftest import fail_on_pvlib_version, platform_is_windows
 
 
-@pytest.mark.xfail(strict=(not platform_is_windows),
-                   reason='fail_on_pvlib_version should cause test to fail')
+@pytest.mark.xfail(reason='fail_on_pvlib_version should cause test to fail')
 @fail_on_pvlib_version('0.0')
 def test_fail_on_pvlib_version():
     pass
@@ -15,8 +14,7 @@ def test_fail_on_pvlib_version_pass():
     pass
 
 
-@pytest.mark.xfail(strict=(not platform_is_windows),
-                   reason='ensure that the test is called')
+@pytest.mark.xfail(reason='ensure that the test is called')
 @fail_on_pvlib_version('100000.0')
 def test_fail_on_pvlib_version_fail_in_test():
     raise Exception

6f2e10527707efa90376fc080c1e6b3477ad0e61
Alexander Morgan <alexanderpmorgan@gmail.com>
2019-07-23 11:33:50 -0400

    Fix indentation mistake.
    Convert tabs to spaces.

--- pvlib/test/test_conftest.py
+++ pvlib/test/test_conftest.py
@@ -4,7 +4,7 @@ from conftest import fail_on_pvlib_version
 
 
 @pytest.mark.xfail(strict=True,
-				   reason='fail_on_pvlib_version should cause test to fail')
+                   reason='fail_on_pvlib_version should cause test to fail')
 @fail_on_pvlib_version('0.0')
 def test_fail_on_pvlib_version():
     pass

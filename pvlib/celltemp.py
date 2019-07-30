2a540ceb0a6699aac1c9c23ce124ad29e6eb8c19
Cliff Hansen <cwhanse@sandia.gov>
2019-07-30 15:43:42 -0600

    rename functions, change output type

--- pvlib/celltemp.py
+++ pvlib/celltemp.py
@@ -6,31 +6,31 @@ Created on Fri Mar 29 09:40:42 2019
 """
 
 import numpy as np
-import pandas as pd
 
 
-TEMP_MODEL_PARAMS = {
-    'sapm': {'open_rack_cell_glassback': (-3.47, -.0594, 3),
-             'roof_mount_cell_glassback': (-2.98, -.0471, 1),
-             'open_rack_cell_polymerback': (-3.56, -.0750, 3),
-             'insulated_back_polymerback': (-2.81, -.0455, 0),
-             'open_rack_polymer_thinfilm_steel': (-3.58, -.113, 3),
-             '22x_concentrator_tracker': (-3.23, -.130, 13)},
-    'pvsyst': {'freestanding': (29.0, 0), 'insulated': (15.0, 0)}
+TEMPERATURE_MODEL_PARAMETERS = {
+    'sapm': {
+        'open_rack_glass_glass': {'a': -3.47, 'b': -.0594, 'deltaT': 3},
+        'close_mount_glass_glass': {'a': -2.98, 'b': -.0471, 'deltaT': 1},
+        'open_rack_glass_polymer': {'a': -3.56, 'b': -.0750, 'deltaT': 3},
+        'insulated_back_glass_polymer': {'a': -2.81, 'b': -.0455, 'deltaT': 0},
+            },
+    'pvsyst': {'freestanding': {'u_c': 29.0, 'u_v': 0},
+               'insulated': {'u_c': 15.0, 'u_v': 0}}
 }
 
 
-def sapm(poa_global, temp_air, wind_speed, a, b, deltaT, irrad_ref=1000):
+def sapm_cell(poa_global, air_temperature, wind_speed, a, b, deltaT,
+              irrad_ref=1000):
     r'''
-    Estimate cell and module temperatures per the Sandia PV Array
-    Performance Model [1].
+    Calculate cell temperature per the Sandia PV Array Performance Model [1].
 
     Parameters
     ----------
     poa_global : float or Series
         Total incident irradiance [W/m^2].
 
-    temp_air : float or Series
+    air_temperature : float or Series
         Ambient dry bulb temperature [C].
 
     wind_speed : float or Series
@@ -49,14 +49,13 @@ def sapm(poa_global, temp_air, wind_speed, a, b, deltaT, irrad_ref=1000):
         Reference irradiance, parameter :math:`E_{0}` in :eq:`sapm2` [W/m^2].
 
     Returns
-    --------
-    DataFrame with columns 'temp_cell' and 'temp_module'.
-    Values in degrees C.
+    -------
+    float or Series, values in degrees C.
 
     Notes
     -----
-    The model for cell temperature :math:`T_{C}` and module temperature
-    :math:`T_{m}` is given by a pair of equations (Eq. 11 and 12 in [1]).
+    The model for cell temperature :math:`T_{C}` is given by a pair of
+    equations (Eq. 11 and 12 in [1]).
 
     .. math::
        :label: sapm1
@@ -67,6 +66,73 @@ def sapm(poa_global, temp_air, wind_speed, a, b, deltaT, irrad_ref=1000):
        :label: sapm2
 
         T_{C} = T_{m} + \frac{E}{E_{0}} \Delta T
+
+    The module back surface temperature :math:`T_{m}` is implemented in
+    ``cell_temperature.sapm_module``.
+
+    Inputs to the model are plane-of-array irradiance :math:`E` (W/m2) and
+    ambient air temperature :math:`T_{a}` (C). Model parameters depend both on
+    the module construction and its mounting. Parameter sets are provided in
+    [1] for representative modules and mounting, and are coded for convenience
+    in ``cell_temperature.TEMPERATURE_MODEL_PARAMETERS``.
+
+    +---------------+----------------+-------+---------+---------------------+
+    | Module        | Mounting       | a     | b       | :math:`\Delta T [C]`|
+    +===============+================+=======+=========+=====================+
+    | glass/glass   | open rack      | -3.47 | -0.0594 | 3                   |
+    +---------------+----------------+-------+---------+---------------------+
+    | glass/glass   | close roof     | -2.98 | -0.0471 | 1                   |
+    +---------------+----------------+-------+---------+---------------------+
+    | glass/polymer | open rack      | -3.56 | -0.075  | 3                   |
+    +---------------+----------------+-------+---------+---------------------+
+    | glass/polymer | insulated back | -2.81 | -0.0455 | 0                   |
+    +---------------+----------------+-------+---------+---------------------+
+
+    References
+    ----------
+    [1] King, D. et al, 2004, "Sandia Photovoltaic Array Performance
+    Model", SAND Report 3535, Sandia National Laboratories, Albuquerque,
+    NM.
+
+    '''
+    module_temperature = sapm_module(poa_global, air_temperature, wind_speed,
+                                     a, b)
+    return module_temperature + (poa_global / irrad_ref) * (deltaT)
+
+
+def sapm_module(poa_global, air_temperature, wind_speed, a, b):
+    r'''
+    Calculate module back surface temperature per the Sandia PV Array
+    Performance Model [1].
+
+    Parameters
+    ----------
+    poa_global : float or Series
+        Total incident irradiance [W/m^2].
+
+    air_temperature : float or Series
+        Ambient dry bulb temperature [C].
+
+    wind_speed : float or Series
+        Wind speed at a height of 10 meters [m/s].
+
+    a : float
+        Parameter :math:`a` in :eq:`sapm1`.
+
+    b : float
+        Parameter :math:`b` in :eq:`sapm1`.
+
+    Returns
+    -------
+    float or Series, values in degrees C.
+
+    Notes
+    -----
+    The model for module temperature :math:`T_{m}` is given by Eq. 11 in [1].
+
+    .. math::
+       :label: sapm1
+        T_{m} = E \times \exp (a + b \times WS) + T_{a}
 
     Inputs to the model are plane-of-array irradiance :math:`E` (W/m2) and
     ambient air temperature :math:`T_{a}` (C). Model outputs are surface
@@ -94,27 +160,22 @@ def sapm(poa_global, temp_air, wind_speed, a, b, deltaT, irrad_ref=1000):
     NM.
 
     '''
-    temp_module = pd.Series(poa_global * np.exp(a + b * wind_speed) + temp_air)
-    temp_cell = temp_module + (poa_global / irrad_ref) * (deltaT)
-    return pd.DataFrame({'temp_cell': temp_cell, 'temp_module': temp_module})
+    return poa_global * np.exp(a + b * wind_speed) + air_temperature
 
 
-def pvsyst(poa_global, temp_air, wind_speed=1.0, constant_loss_factor=29.0,
-           wind_loss_factor=0.0, eta_m=0.1, alpha_absorption=0.9):
+def pvsyst_cell(poa_global, air_temperature, wind_speed=1.0,
+                constant_loss_factor=29.0, wind_loss_factor=0.0, eta_m=0.1,
+                alpha_absorption=0.9):
     r"""
     Calculate cell temperature using an empirical heat loss factor model
     as implemented in PVsyst.
-
-    The heat loss factors provided through the 'model' argument
-    represent the combined effect of convection, radiation and conduction,
-    and their values are experimentally determined.
 
     Parameters
     ----------
     poa_global : float or Series
         Total incident irradiance [W/m^2].
 
-    temp_air : float or Series
+    air_temperature : float or Series
         Ambient dry bulb temperature [C].
 
     wind_speed : float or Series, default 1.0
@@ -122,14 +183,14 @@ def pvsyst(poa_global, temp_air, wind_speed=1.0, constant_loss_factor=29.0,
         factor was determined.  The default value 1.0 m/2 is the wind
         speed at module height used to determine NOCT. [m/s]
 
-    constant_loss_factor : float, default 29.0
+    u_c : float, default 29.0
         Combined heat loss factor coefficient. The default value is
         representative of freestanding modules with the rear surfaces exposed
         to open air (e.g., rack mounted). Parameter :math:`U_{c}` in
         :eq:`pvsyst` [W/(m^2 C)].
 
-    wind_loss_factor : float, default 0.0
-        Combined heat loss factor influenced by wind. Parameter :math:`U_{c}`
+    u_v : float, default 0.0
+        Combined heat loss factor influenced by wind. Parameter :math:`U_{v}`
         in :eq:`pvsyst` [(W/m^2 C)(m/s)].
 
     eta_m : numeric, default 0.1
@@ -141,7 +202,7 @@ def pvsyst(poa_global, temp_air, wind_speed=1.0, constant_loss_factor=29.0,
 
     Returns
     -------
-    DataFrame with columns 'temp_cell', values in degrees Celsius
+    float or Series, values in degrees Celsius
 
     Notes
     -----
@@ -155,8 +216,10 @@ def pvsyst(poa_global, temp_air, wind_speed=1.0, constant_loss_factor=29.0,
     Inputs to the model are plane-of-array irradiance :math:`E` (W/m2), ambient
     air temperature :math:`T_{a}` (C) and wind speed :math:`WS` (m/s). Model
     output is cell temperature :math:`T_{C}`. Model parameters depend both on
-    the module construction and its mounting. Parameter sets are provided in
-    [1] for open (freestanding) close (insulated) mounting configurations.
+    the module construction and its mounting. Parameters are provided in
+    [1] for open (freestanding) and close (insulated) mounting configurations.
+    The heat loss factors provided represent the combined effect of convection,
+    radiation and conduction, and their values are experimentally determined.
 
     +--------------+---------------+---------------+
     | Mounting     | :math:`U_{c}` | :math:`U_{v}` |
@@ -178,6 +241,4 @@ def pvsyst(poa_global, temp_air, wind_speed=1.0, constant_loss_factor=29.0,
     total_loss_factor = wind_loss_factor * wind_speed + constant_loss_factor
     heat_input = poa_global * alpha_absorption * (1 - eta_m)
     temp_difference = heat_input / total_loss_factor
-    temp_cell = pd.Series(temp_air + temp_difference)
-
-    return pd.DataFrame({'temp_cell': temp_cell})
+    return air_temperature + temp_difference
--- pvlib/test/test_celltemp.py
+++ pvlib/test/test_celltemp.py
@@ -5,9 +5,10 @@ Created on Fri Mar 29 10:07:25 2019
 @author: cwhanse
 """
 import pandas as pd
+import numpy as np
 
 import pytest
-from pandas.util.testing import assert_frame_equal
+from pandas.util.testing import assert_series_equal
 from numpy.testing import assert_allclose
 
 from pvlib import celltemp, pvsystem
@@ -17,52 +18,80 @@ from conftest import fail_on_pvlib_version
 
 
 @pytest.fixture
-def celltemp_sapm_default():
-    return celltemp.TEMP_MODEL_PARAMS['sapm']['open_rack_cell_glassback']
+def sapm_default():
+    return celltemp.TEMPERATURE_MODEL_PARAMETERS['sapm']\
+        ['open_rack_glass_glass']
 
 
-def test_sapm_celltemp(celltemp_sapm_default):
-    a, b, deltaT = celltemp_sapm_default
-    default = celltemp.sapm(900, 20, 5, a, b, deltaT)
-    assert_allclose(default['temp_cell'], 43.509, 3)
-    assert_allclose(default['temp_module'], 40.809, 3)
+def test_sapm_cell(sapm_default):
+    default = celltemp.sapm_cell(900, 20, 5, sapm_default['a'],
+                                 sapm_default['b'], sapm_default['deltaT'])
+    assert_allclose(default, 43.509, 3)
 
 
-def test_sapm_celltemp_with_index(celltemp_sapm_default):
-    a, b, deltaT = celltemp_sapm_default
+def test_sapm_module(sapm_default):
+    default = celltemp.sapm(900, 20, 5, sapm_default['a'], sapm_default['b'])
+    assert_allclose(default, 40.809, 3)
+
+
+def test_sapm_ndarray(sapm_default):
+    temps = np.array([0, 10, 5])
+    irrads = np.array([0, 500, 0])
+    winds = np.array([10, 5, 0])
+    cell_temps = celltemp.sapm_cell(irrads, temps, winds, sapm_default['a'],
+                                    sapm_default['b'], sapm_default['deltaT'])
+    module_temps = celltemp.sapm_module(irrads, temps, winds,
+                                        sapm_default['a'], sapm_default['b'])
+    expected_cell = np.array([0., 23.06066166, 5.])
+    expected_module = np.array([0., 21.56066166, 5.])
+    assert_allclose(expected_cell, cell_temps, 3)
+    assert_allclose(expected_module, module_temps, 3)
+
+
+def test_sapm_series(sapm_default):
     times = pd.date_range(start='2015-01-01', end='2015-01-02', freq='12H')
     temps = pd.Series([0, 10, 5], index=times)
     irrads = pd.Series([0, 500, 0], index=times)
     winds = pd.Series([10, 5, 0], index=times)
-    pvtemps = celltemp.sapm(irrads, temps, winds, a, b, deltaT)
-    expected = pd.DataFrame({'temp_cell': [0., 23.06066166, 5.],
-                             'temp_module': [0., 21.56066166, 5.]},
-                            index=times)
-    assert_frame_equal(expected, pvtemps)
+    cell_temps = celltemp.sapm_cell(irrads, temps, winds, sapm_default['a'],
+                                    sapm_default['b'], sapm_default['deltaT'])
+    module_temps = celltemp.sapm_module(irrads, temps, winds,
+                                        sapm_default['a'], sapm_default['b'])
+    expected_cell = pd.Series([0., 23.06066166, 5.], index=times)
+    expected_module = pd.Series([0., 21.56066166, 5.], index=times)
+    assert_series_equal(expected_cell, cell_temps)
+    assert_series_equal(expected_module, module_temps)
 
 
-def test_celltemp_pvsyst_default():
-    default = celltemp.pvsyst(900, 20, 5)
-    assert_allclose(default['temp_cell'], 45.137, 0.001)
+def test_pvsyst_cell_default():
+    result = celltemp.pvsyst_cell(900, 20, 5)
+    assert_allclose(result, 45.137, 0.001)
 
 
-def test_celltemp_pvsyst_kwargs():
-    result = celltemp.pvsyst(900, 20, wind_speed=5.0,
-                             constant_loss_factor=23.5, wind_loss_factor=6.25,
-                             eta_m=0.1)
-    assert_allclose(result['temp_cell'], 33.315, 0.001)
+def test_pvsyst_cell_kwargs():
+    result = celltemp.pvsyst_cell(900, 20, wind_speed=5.0, u_c=23.5, u_v=6.25,
+                                  eta_m=0.1)
+    assert_allclose(result, 33.315, 0.001)
 
 
-def test_celltemp_pvsyst_with_index():
+def test_pvsyst_cell_ndarray():
+    temps = np.array([0, 10, 5])
+    irrads = np.array([0, 500, 0])
+    winds = np.array([10, 5, 0])
+    result = celltemp.pvsyst_cell(irrads, temps, wind_speed=winds)
+    expected = np.array([0.0, 23.96551, 5.0])
+    assert_allclose(expected, result, 3)
+
+
+def test_pvsyst_cell_series():
     times = pd.date_range(start="2015-01-01", end="2015-01-02", freq="12H")
     temps = pd.Series([0, 10, 5], index=times)
     irrads = pd.Series([0, 500, 0], index=times)
     winds = pd.Series([10, 5, 0], index=times)
 
-    pvtemps = celltemp.pvsyst(irrads, temps, wind_speed=winds)
-    expected = pd.DataFrame([0.0, 23.96551, 5.0], index=times,
-                            columns=['temp_cell'])
-    assert_frame_equal(expected, pvtemps)
+    result = celltemp.pvsyst_cell(irrads, temps, wind_speed=winds)
+    expected = pd.Series([0.0, 23.96551, 5.0], index=times)
+    assert_series_equal(expected, result)
 
 
 @fail_on_pvlib_version('0.8')

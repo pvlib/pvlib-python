03528149e92635f7cbd3cc057b5016fb80c8cf49
Joseph Palakapilly <joseph.palakapilly@kwhanalytics.com>
2019-08-13 16:41:40 -0700

    fixed lint errors.

--- pvlib/test/test_snowcoverage.py
+++ pvlib/test/test_snowcoverage.py
@@ -10,12 +10,13 @@ from pvlib import snowcoverage
 
 def test_snow_slide_amount():
     tilt = 45
-    sliding_coefficient = 2
+    sc = 2
     expected_specified = 2**.5
     expected_default = 1.97/2*2**.5
     actual_default = snowcoverage.snow_slide_amount(tilt)
     assert_almost_equal(expected_default, actual_default)
-    actual_specified = snowcoverage.snow_slide_amount(tilt, sliding_coefficient=sliding_coefficient)
+    actual_specified = snowcoverage.snow_slide_amount(tilt,
+                                                      sliding_coefficient=sc)
     assert_almost_equal(expected_specified, actual_specified)
 
 
@@ -29,14 +30,17 @@ def test_snow_slide_event():
 
 def test_fully_covered_panel():
     snowfall_data = pd.Series([1, 5, .6, 4, .23, -5, 19])
-    expected_snowfall = pd.Series([True, True, False, True, False, False, True])
+    expected_snowfall = pd.Series([True, True, False, True,
+                                   False, False, True])
     actual_snowfall = snowcoverage.fully_covered_panel(snowfall_data)
     assert_series_equal(actual_snowfall, expected_snowfall)
 
     snowdepth_data = pd.Series([.5, 1, 2, 4, .23, 4, 19])
-    expected_snowdepth = pd.Series([False, False, True, True, False, True, True])
-    actual_snowdepth = snowcoverage.fully_covered_panel(snowdepth_data, snow_data_type="snow_depth")
-    assert_series_equal(actual_snowdepth, expected_snowdepth)
+    expected_snowdepth = pd.Series([False, False, True, True,
+                                    False, True, True])
+    actual = snowcoverage.fully_covered_panel(snowdepth_data,
+                                              snow_data_type="snow_depth")
+    assert_series_equal(actual, expected_snowdepth)
 
 
 def test_snow_coverage_model():
@@ -47,16 +51,20 @@ def test_snow_coverage_model():
 
     snowfall_data = pd.Series([1, .5, .6, .4, .23, -5, .1, .1])
     snow_coverage = snowcoverage.snow_coverage_model(snowfall_data, "snowfall",
-                                                     poa_irradiance, temperature,
+                                                     poa_irradiance,
+                                                     temperature,
                                                      tilt)
     expected = pd.Series(1-slide*np.array([1, 2, 3, 4, 5, 6, 7, 1.0/slide]))
     assert_series_equal(snow_coverage, expected)
 
     snowdepth_data = pd.Series([.5, 1, 2, 4, .23, 4, 11, .1])
-    snow_coverage = snowcoverage.snow_coverage_model(snowdepth_data, "snow_depth",
-                                                     poa_irradiance, temperature,
+    snow_coverage = snowcoverage.snow_coverage_model(snowdepth_data,
+                                                     "snow_depth",
+                                                     poa_irradiance,
+                                                     temperature,
                                                      tilt)
-    expected = pd.Series(1-slide*np.array([1.0/slide, 1.0/slide, 1, 1, 2, 1, 1, 2]))
+    expected = pd.Series(1-slide*np.array([1.0/slide, 1.0/slide, 1,
+                                           1, 2, 1, 1, 2]))
     assert_series_equal(snow_coverage, expected)
 
 

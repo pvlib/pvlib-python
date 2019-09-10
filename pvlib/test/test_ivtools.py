b8a713b2cc9f6361092213bcc4a2d60432b388ff
Cliff Hansen <cwhanse@sandia.gov>
2019-09-10 10:40:06 -0600

    hardwire test output rather than read from old CEC file

--- pvlib/test/test_ivtools.py
+++ pvlib/test/test_ivtools.py
@@ -20,14 +20,12 @@ def get_test_iv_params():
 
 @pytest.fixture
 def get_cec_params_cansol_cs5p_220p():
-    return {'V_mp_ref': 46.6, 'I_mp_ref': 4.73, 'V_oc_ref': 58.3,
-            'I_sc_ref': 5.05, 'alpha_sc': 0.0025, 'beta_voc': -0.19659,
-            'gamma_pmp': -0.43, 'cells_in_series': 96}
-
-
-@pytest.fixture()
-def sam_data():
-    return pvsystem.retrieve_sam('cecmod')
+    return {'input': {'V_mp_ref': 46.6, 'I_mp_ref': 4.73, 'V_oc_ref': 58.3,
+                      'I_sc_ref': 5.05, 'alpha_sc': 0.0025,
+                      'beta_voc': -0.19659, 'gamma_pmp': -0.43,
+                      'cells_in_series': 96},
+            'output': {'a_ref': 2.3674, 'I_L_ref': 5.056, 'I_o_ref': 1.01e-10,
+                       'R_sh_ref': 837.51, 'R_s': 1.004, 'Adjust': 2.3}}
 
 
 @pytest.fixture()
@@ -84,31 +82,24 @@ def test_fit_sde_sandia_bad_iv(get_bad_iv_curves):
 
 
 @requires_pysam
-def test_fit_sdm_cec_sam(cec_module_parameters,
-                         get_cec_params_cansol_cs5p_220p):
-    sam_parameters = cec_module_parameters
-    cec_list_data = get_cec_params_cansol_cs5p_220p
+def test_fit_sdm_cec_sam(get_cec_params_cansol_cs5p_220p):
+    input_data = get_cec_params_cansol_cs5p_220p['input']
+    expected = get_cec_params_cansol_cs5p_220p['output']
     I_L_ref, I_o_ref, R_sh_ref, R_s, a_ref, Adjust = \
         ivtools.fit_sdm_cec_sam(
-            celltype='polySi', v_mp=cec_list_data['V_mp_ref'],
-            i_mp=cec_list_data['I_mp_ref'], v_oc=cec_list_data['V_oc_ref'],
-            i_sc=cec_list_data['I_sc_ref'], alpha_sc=cec_list_data['alpha_sc'],
-            beta_voc=cec_list_data['beta_voc'],
-            gamma_pmp=cec_list_data['gamma_pmp'],
-            cells_in_series=cec_list_data['cells_in_series'])
-    modeled = pd.Series(index=sam_parameters.index, data=cec_list_data)
+            celltype='polySi', v_mp=input_data['V_mp_ref'],
+            i_mp=input_data['I_mp_ref'], v_oc=input_data['V_oc_ref'],
+            i_sc=input_data['I_sc_ref'], alpha_sc=input_data['alpha_sc'],
+            beta_voc=input_data['beta_voc'],
+            gamma_pmp=input_data['gamma_pmp'],
+            cells_in_series=input_data['cells_in_series'])
+    modeled = pd.Series(index=expected.index, data=np.nan)
     modeled['a_ref'] = a_ref
     modeled['I_L_ref'] = I_L_ref
     modeled['I_o_ref'] = I_o_ref
     modeled['R_sh_ref'] = R_sh_ref
     modeled['R_s'] = R_s
     modeled['Adjust'] = Adjust
-    modeled['gamma_r'] = cec_list_data['gamma_pmp']
-    modeled['N_s'] = cec_list_data['cells_in_series']
-    modeled = modeled.dropna()
-    expected = pd.Series(index=modeled.index, data=np.nan)
-    for k in modeled.keys():
-        expected[k] = sam_parameters[k]
     assert np.allclose(modeled.values, expected.values, rtol=5e-2)
     # test for fitting failure
     with pytest.raises(RuntimeError):

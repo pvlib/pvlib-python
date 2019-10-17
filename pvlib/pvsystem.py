c55fa1ca6e526e23c03fb1313d5f292e5be5df71
Veronica Guo <vguo@opusonesolutions.com>
2019-10-17 00:22:37 -0400

    Fix lint error

--- pvlib/pvsystem.py
+++ pvlib/pvsystem.py
@@ -1800,7 +1800,8 @@ def retrieve_sam(name=None, path=None):
 
     Notes
     -----
-    Files available at https://github.com/NREL/SAM/tree/develop/deploy/libraries
+    Files available at
+        https://github.com/NREL/SAM/tree/develop/deploy/libraries
     Documentation for module and inverter data sets:
         https://sam.nrel.gov/photovoltaic/pv-sub-page-2.html
 

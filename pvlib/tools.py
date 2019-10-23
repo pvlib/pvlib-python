7452e0e638a267d71084163f36839781abe97d3e
Cameron Stark <ctstark@sandia.gov>
2019-10-23 14:51:07 -0600

    force value to np.ndarray to enable universal use of .item()

--- pvlib/tools.py
+++ pvlib/tools.py
@@ -212,7 +212,7 @@ def _scalar_out(input):
     else:  #
         # works if it's a 1 length array and
         # will throw a ValueError otherwise
-        output = input.values.item()
+        output = np.asarray(input).item()
 
     return output
 

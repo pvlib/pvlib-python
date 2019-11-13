ce33db97aa9c3477ac6ef3902d78cbf10e7fe5a0
Joe Ranalli <jar339@psu.edu>
2019-11-13 10:11:18 +0100

    Changes following review. WVM accepts list of xy tuples as inputs. Cleanup of docs.

--- pvlib/scaling.py
+++ pvlib/scaling.py
@@ -7,8 +7,7 @@ import numpy as np
 import pandas as pd
 
 
-def wvm(cs_series, latitude, longitude, cloud_speed,
-        method, capacity=None, density=41, dt=None):
+def wvm(clearsky_index, positions, cloud_speed, dt=None):
     """
     Compute spatial aggregation time series smoothing on clear sky index based
     on the Wavelet Variability model of Lave et al [1-2]. Implementation is
@@ -16,52 +15,33 @@ def wvm(cs_series, latitude, longitude, cloud_speed,
 
     Parameters
     ----------
-    cs_series : numeric or pandas.Series
+    clearsky_index : numeric or pandas.Series
         Clear Sky Index time series that will be smoothed.
 
-    latitude : numeric
-        Latitudes making up the plant in degrees. Type depends on method used.
-        'discrete' uses a list of lat/lon points representing discrete sites.
-        'square' uses a single valued lat/lon pair for the plant center.
-        'polygon' uses a list of lat/lon points representing the corners of
-        a polygon making up the site.
-
-    longitude : numeric
-        Longitudes making up the plant in degrees. Type depends on method used.
-        'discrete' uses a list of lat/lon points representing discrete sites.
-        'square' uses a single valued lat/lon pair for the plant center.
-        'polygon' uses a list of lat/lon points representing the corners of
-        a polygon making up the site.
+    positions : numeric
+        Array of coordinate distances as (x,y) pairs representing the
+        easting, northing of the site positions in meters [m]. Distributed
+        plants could be simulated by gridded points throughout the plant
+        footprint.
 
     cloud_speed : numeric
-        Speed of cloud movement in meters per second
+        Speed of cloud movement in meters per second [m/s].
 
-    method : string
-        The type of plant distribution to model.
-        Options are ``'discrete', 'square', 'polygon'``.
-
-    capacity : numeric
-        The plant capacity in MW. Must be specified for 'square' method,
-        ignored otherwise.
-
-    density : numeric, default 41
-        The density of installed PV in W/m^2. Must be specified for 'square'
-        method, ignored otherwise. Default of 41 W/m^2 is 1MW per 6 acres.
-
-    dt : numeric
-        The time series time delta. By default, is inferred from the cs_series.
-        Must be specified for a time series that doesn't include an index.
+    dt : float, default None
+        The time series time delta. By default, is inferred from the
+        clearsky_index. Must be specified for a time series that doesn't
+        include an index. Units of seconds [s].
 
     Returns
     -------
-    smoothed : pandas.Series or numeric, depending on inlet type
-        The clear sky index time series smoothed for the described plant.
+    smoothed : numeric or pandas.Series
+        The Clear Sky Index time series smoothed for the described plant.
 
     wavelet: numeric
-        The individual wavelets for the time series before smoothing
+        The individual wavelets for the time series before smoothing.
 
     tmscales: numeric
-        The timescales (in sec) associated with the wavelets
+        The timescales associated with the wavelets in seconds [s].
 
     References
     ----------
@@ -81,14 +61,16 @@ def wvm(cs_series, latitude, longitude, cloud_speed,
 
     try:
         import scipy.optimize
+        from scipy.spatial.distance import pdist
     except ImportError:
         raise ImportError("The WVM function requires scipy.")
 
-    dist = _compute_distances(longitude, latitude, method, capacity, density)
-    wavelet, tmscales = _compute_wavelet(cs_series, dt)
+    pos = np.array(positions)
+    dist = pdist(pos, 'euclidean')
+    wavelet, tmscales = _compute_wavelet(clearsky_index, dt)
 
+    # Find effective length of position vector, 'dist' is full pairwise
     n_pairs = len(dist)
-    # Find eff length of position vector, 'dist' is full pairwise
 
     def fn(x):
         return np.abs((x ** 2 - x) / 2 - n_pairs)
@@ -115,40 +97,46 @@ def wvm(cs_series, latitude, longitude, cloud_speed,
     outsignal = np.sum(wavelet_smooth, 0)
 
     try:  # See if there's an index already, if so, return as a pandas Series
-        smoothed = pd.Series(outsignal, index=cs_series.index)
+        smoothed = pd.Series(outsignal, index=clearsky_index.index)
     except AttributeError:
         smoothed = outsignal  # just output the numpy signal
 
     return smoothed, wavelet, tmscales
 
 
-def _latlon_to_dist(latitude, longitude):
+def latlon_to_xy(coordinates):
     """
     Convert latitude and longitude in degrees to a coordinate system measured
     in meters from zero deg latitude, zero deg longitude.
 
+    This is a convenience method to support inputs to wvm. Note that the
+    methodology used is only suitable for short distances. For conversions of
+    longer distances, users should consider use of Universal Transverse
+    Mercator (UTM) or other suitable cartographic projection. Consider
+    packages built for cartographic projection such as pyproj (e.g.
+    pyproj.transform()) [2].
+
     Parameters
     ----------
-    latitude : numeric
-        Latitude in degrees
 
-    longitude : numeric
-        Longitude in degrees
+    coordinates : numeric
+        Array or list of (latitude, longitude) coordinate pairs. Use decimal
+        degrees notation.
 
     Returns
     -------
-    ypos : numeric
-        the northward distance in meters
-
-    xpos: numeric
-        the eastward distance in meters.
+    xypos : numeric
+        Array of coordinate distances as (x,y) pairs representing the
+        easting, northing of the position in meters [m].
 
     References
     ----------
     [1] H. Moritz. Geodetic Reference System 1980, Journal of Geodesy, vol. 74,
     no. 1, pp 128–133, 2000.
 
-    [2] Wavelet Variability Model - Matlab Code:
+    [2] https://pypi.org/project/pyproj/
+
+    [3] Wavelet Variability Model - Matlab Code:
     https://pvpmc.sandia.gov/applications/wavelet-variability-model/
     """
 
@@ -156,110 +144,43 @@ def _latlon_to_dist(latitude, longitude):
 
     r_earth = 6371008.7714  # mean radius of Earth, in meters
     m_per_deg_lat = r_earth * np.pi / 180
-    m_per_deg_lon = r_earth * np.cos(np.pi/180*np.mean(latitude)) * np.pi/180
-
     try:
-        xpos = m_per_deg_lon * longitude
-        ypos = m_per_deg_lat * latitude
-    except TypeError:  # When longitude and latitude are a list
-        xpos = m_per_deg_lon * np.array(longitude)
-        ypos = m_per_deg_lat * np.array(latitude)
+        meanlat = np.mean([lat for (lat, lon) in coordinates])  # Mean latitude
+    except TypeError:  # Assume it's a single value?
+        meanlat = coordinates[0]
+    m_per_deg_lon = r_earth * np.cos(np.pi/180 * meanlat) * np.pi/180
 
-    return ypos, xpos
+    # Conversion
+    pos = coordinates * np.array(m_per_deg_lat, m_per_deg_lon)
 
-
-def _compute_distances(longitude, latitude, method, capacity=None, density=41):
-    """
-    Compute points representing a plant and the pairwise distances between them
-
-    Parameters
-    ----------
-    latitude : numeric
-        Latitudes making up the plant in degrees. Type depends on method used.
-        'discrete' uses a list of lat/lon points representing discrete sites.
-        'square' uses a single valued lat/lon pair for the plant center.
-        'polygon' uses a list of lat/lon points representing the corners of
-        a polygon making up the site.
-
-    longitude : numeric
-        Longitudes making up the plant in degrees. Type depends on method used.
-        'discrete' uses a list of lat/lon points representing discrete sites.
-        'square' uses a single valued lat/lon pair for the plant center.
-        'polygon' uses a list of lat/lon points representing the corners of
-        a polygon making up the site.
-
-    method : string
-        The type of plant distribution to model.
-        Options are ``'discrete', 'square', 'polygon'``.
-
-    capacity : numeric
-        The plant capacity in MW. Must be specified for 'square' method,
-        ignored otherwise.
-
-    density : numeric, default 41
-        The density of installed PV in W/m^2. Must be specified for 'square'
-        method, ignored otherwise. Default of 41 W/m^2 is 1MW per 6 acres.
-
-    Returns
-    -------
-    dist : numeric
-        The complete set of pairwise distances of all points representing the
-        plant being modelled.
-
-
-    References
-    ----------
-    [1] Wavelet Variability Model - Matlab Code:
-    https://pvpmc.sandia.gov/applications/wavelet-variability-model/
-    """
-
+    # reshape as (x,y) pairs to return
     try:
-        from scipy.spatial.distance import pdist
-    except ImportError:
-        raise ImportError("The WVM function requires scipy.")
-
-    # Convert latitude and longitude points to distances in meters
-    ypos, xpos = _latlon_to_dist(latitude, longitude)
-
-    if method == "discrete":
-        # Positions are individual plant centers.
-        # Treat as existing subscale points.
-        pos = np.array([xpos, ypos]).transpose()
-
-    elif method == "square":
-        raise NotImplementedError("To be implemented")
-
-    elif method == "polygon":
-        raise NotImplementedError("To be implemented")
-
-    else:
-        raise ValueError("Plant distance calculation method must be one of: "
-                         "discrete, square, polygon")
-
-    # Compute the full list of point-to-point distances
-    return pdist(pos, 'euclidean')
+        return np.column_stack([pos[:, 1], pos[:, 0]])
+    except IndexError:  # Assume it's a single value, which has a 1D shape
+        return np.array((pos[1], pos[0]))
 
 
-def _compute_wavelet(cs_series, dt=None):
+def _compute_wavelet(clearsky_index, dt=None):
     """
     Compute the wavelet transform on the input clear_sky time series.
 
     Parameters
     ----------
-    cs_series : numeric or pandas.Series
+    clearsky_index : numeric or pandas.Series
         Clear Sky Index time series that will be smoothed.
 
-    dt : numeric
-        The time series time delta. By default, is inferred from the cs_series.
-        Must be specified for a time series that doesn't include an index.
+    dt : float, default None
+        The time series time delta. By default, is inferred from the
+        clearsky_index. Must be specified for a time series that doesn't
+        include an index. Units of seconds [s].
 
     Returns
     -------
     wavelet: numeric
-        The individual wavelets for the time series before smoothing
+        The individual wavelets for the time series
 
     tmscales: numeric
-        The timescales (in sec) associated with the wavelets
+        The timescales associated with the wavelets in seconds [s]
 
     References
     ----------
@@ -274,13 +195,13 @@ def _compute_wavelet(cs_series, dt=None):
     # Added by Joe Ranalli (@jranalli), Penn State Hazleton, 2019
 
     try:  # Assume it's a pandas type
-        vals = cs_series.values.flatten()
+        vals = clearsky_index.values.flatten()
         try:  # Assume it's a time series type index
-            dt = (cs_series.index[1] - cs_series.index[0]).seconds
+            dt = (clearsky_index.index[1] - clearsky_index.index[0]).seconds
         except AttributeError:  # It must just be a numeric index
-            dt = (cs_series.index[1] - cs_series.index[0])
+            dt = (clearsky_index.index[1] - clearsky_index.index[0])
     except AttributeError:  # Assume it's a numpy type
-        vals = cs_series.flatten()
+        vals = clearsky_index.flatten()
         if dt is None:
             raise ValueError("dt must be specified for numpy type inputs.")
 
--- pvlib/test/test_scaling.py
+++ pvlib/test/test_scaling.py
@@ -7,96 +7,63 @@ from numpy.testing import assert_almost_equal
 from pvlib import scaling
 from conftest import requires_scipy
 
-# All expected_xxxxxx variable results computed in Matlab code
 
 # Sample positions
 lat = np.array((9.99, 10, 10.01))
 lon = np.array((4.99, 5, 5.01))
+coordinates = np.array([(lati, loni) for (lati, loni) in zip(lat, lon)])
+
 # Sample cloud speed
 cloud_speed = 5
 # Generate a sample clear_sky_index and time vector.
 clear_sky_index = np.ones(10000)
 clear_sky_index[5000:5005] = np.array([1, 1, 1.1, 0.9, 1])
 time = np.arange(0, len(clear_sky_index))
+
 # Sample dt
 dt = 1
 
-# Expected distance and positions for sample lat/lon given above
-expect_dist = np.array((1560.6, 3121.3, 1560.6))
+# Expected positions for sample lat/lon given above (calculated manually)
 expect_xpos = np.array([554863.4, 555975.4, 557087.3])
-expect_ypos = np.array([1106611.8, 1107719.5, 1108827.2])
+expect_ypos = np.array([1110838.8, 1111950.8, 1113062.7])
+
+# Sample positions based on the previous lat/lon
+positions = np.array([pt for pt in zip(expect_xpos, expect_ypos)])
 
 # Expected timescales for dt = 1
 expect_tmscale = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
 
-# Expected wavelet for indices 5000:5004 using clear_sky_index above
+# Expected wavelet for indices 5000:5004 using clear_sky_index above (Matlab)
 expect_wavelet = np.array([[-0.025, 0.05, 0., -0.05, 0.025],
                            [0.025, 0., 0., 0., -0.025],
                            [0., 0., 0., 0., 0.]])
 
-# Expected smoothed clear sky index for indices 5000:5004 using inputs above
+# Expected smoothed clear sky index for indices 5000:5004 (Matlab)
 expect_cs_smooth = np.array([1., 1.0289, 1., 0.9711, 1.])
 
 
-def test_latlon_to_dist_zero():
-    lat = 0
-    lon = 0
-    xpos_e = 0
-    ypos_e = 0
-    xpos, ypos = scaling._latlon_to_dist(lon, lat)
-    assert_almost_equal(xpos, xpos_e, decimal=1)
-    assert_almost_equal(ypos, ypos_e, decimal=1)
+def test_latlon_to_xy_zero():
+    coord = [0, 0]
+    pos_e = [0, 0]
+    pos = scaling.latlon_to_xy(coord)
+    assert_almost_equal(pos, pos_e, decimal=1)
 
 
-def test_latlon_to_dist_single():
+def test_latlon_to_xy_single():
     # Must test against central value, because latlon_to_dist uses the mean
-    xpos, ypos = scaling._latlon_to_dist(lon[1], lat[1])
-    assert_almost_equal(xpos, expect_xpos[1], decimal=1)
-    assert_almost_equal(ypos, expect_ypos[1], decimal=1)
+    coord = (lat[1], lon[1])
+    pos = scaling.latlon_to_xy(coord)
+    assert_almost_equal(pos, (expect_xpos[1], expect_ypos[1]), decimal=1)
 
 
-def test_latlon_to_dist_array():
-    xpos, ypos = scaling._latlon_to_dist(lon, lat)
-    assert_almost_equal(xpos, expect_xpos, decimal=1)
-    assert_almost_equal(ypos, expect_ypos, decimal=1)
+def test_latlon_to_xy_array():
+    pos = scaling.latlon_to_xy(coordinates)
+    assert_almost_equal(pos, positions, decimal=1)
 
 
-def test_latlon_to_dist_list():
-    xpos, ypos = scaling._latlon_to_dist(lon.tolist(), lat.tolist())
-    assert_almost_equal(xpos, expect_xpos, decimal=1)
-    assert_almost_equal(ypos, expect_ypos, decimal=1)
-
-
-@requires_scipy
-def test_compute_distances_invalid():
-    with pytest.raises(ValueError):
-        scaling._compute_distances(0, 0, method='invalid')
-
-
-@requires_scipy
-def test_compute_distances_square():
-    with pytest.raises(NotImplementedError):
-        scaling._compute_distances(0, 0, method='square')
-
-
-@requires_scipy
-def test_compute_distances_polygon():
-    with pytest.raises(NotImplementedError):
-        scaling._compute_distances(0, 0, method='polygon')
-
-
-@requires_scipy
-def test_compute_distances_discrete_zero():
-    lat = np.array((0, 0))
-    lon = np.array((0, 0))
-    assert_almost_equal(scaling._compute_distances(lon, lat, 'discrete'), 0)
-
-
-@requires_scipy
-def test_compute_distances_discrete_array():
-
-    dist = scaling._compute_distances(lon, lat, 'discrete')
-    assert_almost_equal(dist, expect_dist, decimal=1)
+def test_latlon_to_xy_list():
+    pos = scaling.latlon_to_xy(coordinates.tolist())
+    assert_almost_equal(pos, positions, decimal=1)
 
 
 def test_compute_wavelet_series():
@@ -120,18 +87,24 @@ def test_compute_wavelet_array_invalid():
 @requires_scipy
 def test_wvm_series():
     csi_series = pd.Series(clear_sky_index, index=time)
-    cs_sm, _, _ = scaling.wvm(csi_series, lat, lon, cloud_speed, "discrete")
+    cs_sm, _, _ = scaling.wvm(csi_series, positions, cloud_speed)
     assert_almost_equal(cs_sm[5000:5005], expect_cs_smooth, decimal=4)
 
 
 @requires_scipy
 def test_wvm_array():
-    cs_sm, _, _ = scaling.wvm(clear_sky_index, lat, lon, cloud_speed,
-                              "discrete", dt=dt)
+    cs_sm, _, _ = scaling.wvm(clear_sky_index, positions, cloud_speed, dt=dt)
     assert_almost_equal(cs_sm[5000:5005], expect_cs_smooth, decimal=4)
 
 
 @requires_scipy
-def test_wvm_array_invalid():
+def test_wvm_series_xyaslist():
+    csi_series = pd.Series(clear_sky_index, index=time)
+    cs_sm, _, _ = scaling.wvm(csi_series, positions.tolist(), cloud_speed)
+    assert_almost_equal(cs_sm[5000:5005], expect_cs_smooth, decimal=4)
+
+
+@requires_scipy
+def test_wvm_invalid():
     with pytest.raises(ValueError):
-        scaling.wvm(clear_sky_index, lat, lon, cloud_speed, "discrete")
+        scaling.wvm(clear_sky_index, positions, cloud_speed)

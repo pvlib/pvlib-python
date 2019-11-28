84cd0fb28282e11e0bf1ed6f31c3883b364d9ced
Ranalli, Joe <jar339@psu.edu>
2019-11-28 09:55:55 +0100

    convert tests to use fixtures

--- pvlib/test/test_scaling.py
+++ pvlib/test/test_scaling.py
@@ -8,38 +8,62 @@ from pvlib import scaling
 from conftest import requires_scipy
 
 
-# Sample positions
-lat = np.array((9.99, 10, 10.01))
-lon = np.array((4.99, 5, 5.01))
-coordinates = np.array([(lati, loni) for (lati, loni) in zip(lat, lon)])
-
 # Sample cloud speed
 cloud_speed = 5
-# Generate a sample clear_sky_index and time vector.
-clear_sky_index = np.ones(10000)
-clear_sky_index[5000:5005] = np.array([1, 1, 1.1, 0.9, 1])
-time = np.arange(0, len(clear_sky_index))
 
 # Sample dt
 dt = 1
 
-# Expected positions for sample lat/lon given above (calculated manually)
-expect_xpos = np.array([554863.4, 555975.4, 557087.3])
-expect_ypos = np.array([1110838.8, 1111950.8, 1113062.7])
 
-# Sample positions based on the previous lat/lon
-positions = np.array([pt for pt in zip(expect_xpos, expect_ypos)])
+@pytest.fixture
+def coordinates():
+    # Sample positions in lat/lon
+    lat = np.array((9.99, 10, 10.01))
+    lon = np.array((4.99, 5, 5.01))
+    coordinates = np.array([(lati, loni) for (lati, loni) in zip(lat, lon)])
+    return coordinates
 
-# Expected timescales for dt = 1
-expect_tmscale = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
 
-# Expected wavelet for indices 5000:5004 using clear_sky_index above (Matlab)
-expect_wavelet = np.array([[-0.025, 0.05, 0., -0.05, 0.025],
-                           [0.025, 0., 0., 0., -0.025],
-                           [0., 0., 0., 0., 0.]])
+@pytest.fixture
+def clear_sky_index():
+    # Generate a sample clear_sky_index
+    clear_sky_index = np.ones(10000)
+    clear_sky_index[5000:5005] = np.array([1, 1, 1.1, 0.9, 1])
+    return clear_sky_index
 
-# Expected smoothed clear sky index for indices 5000:5004 (Matlab)
-expect_cs_smooth = np.array([1., 1.0289, 1., 0.9711, 1.])
+
+@pytest.fixture
+def time(clear_sky_index):
+    # Sample time vector
+    return np.arange(0, len(clear_sky_index))
+
+
+@pytest.fixture
+def positions():
+    # Sample positions based on the previous lat/lon (calculated manually)
+    expect_xpos = np.array([554863.4, 555975.4, 557087.3])
+    expect_ypos = np.array([1110838.8, 1111950.8, 1113062.7])
+    return np.array([pt for pt in zip(expect_xpos, expect_ypos)])
+
+
+@pytest.fixture
+def expect_tmscale():
+    # Expected timescales for dt = 1
+    return [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
+
+
+@pytest.fixture
+def expect_wavelet():
+    # Expected wavelet for indices 5000:5004 with clear_sky_index above (Matlab)
+    return np.array([[-0.025, 0.05, 0., -0.05, 0.025],
+                     [0.025, 0., 0., 0., -0.025],
+                     [0., 0., 0., 0., 0.]])
+
+
+@pytest.fixture
+def expect_cs_smooth():
+    # Expected smoothed clear sky index for indices 5000:5004 (Matlab)
+    return np.array([1., 1.0289, 1., 0.9711, 1.])
 
 
 def test_latlon_to_xy_zero():
@@ -49,31 +73,33 @@ def test_latlon_to_xy_zero():
     assert_almost_equal(pos, pos_e, decimal=1)
 
 
-def test_latlon_to_xy_single():
-    # Must test against central value, because latlon_to_dist uses the mean
-    coord = (lat[1], lon[1])
+def test_latlon_to_xy_single(coordinates, positions):
+    # Must test against central value, because latlon_to_xy uses the mean
+    coord = coordinates[1]
     pos = scaling.latlon_to_xy(coord)
-    assert_almost_equal(pos, (expect_xpos[1], expect_ypos[1]), decimal=1)
+    assert_almost_equal(pos, positions[1], decimal=1)
 
 
-def test_latlon_to_xy_array():
+def test_latlon_to_xy_array(coordinates, positions):
     pos = scaling.latlon_to_xy(coordinates)
     assert_almost_equal(pos, positions, decimal=1)
 
 
-def test_latlon_to_xy_list():
+def test_latlon_to_xy_list(coordinates, positions):
     pos = scaling.latlon_to_xy(coordinates.tolist())
     assert_almost_equal(pos, positions, decimal=1)
 
 
-def test_compute_wavelet_series():
+def test_compute_wavelet_series(clear_sky_index, time,
+                                expect_tmscale, expect_wavelet):
     csi_series = pd.Series(clear_sky_index, index=time)
     wavelet, tmscale = scaling._compute_wavelet(csi_series)
     assert_almost_equal(tmscale, expect_tmscale)
     assert_almost_equal(wavelet[0:3, 5000:5005], expect_wavelet)
 
 
-def test_compute_wavelet_series_numindex():
+def test_compute_wavelet_series_numindex(clear_sky_index, time,
+                                         expect_tmscale, expect_wavelet):
     dtindex = pd.to_datetime(time, unit='s')
     csi_series = pd.Series(clear_sky_index, index=dtindex)
     wavelet, tmscale = scaling._compute_wavelet(csi_series)
@@ -81,38 +107,39 @@ def test_compute_wavelet_series_numindex():
     assert_almost_equal(wavelet[0:3, 5000:5005], expect_wavelet)
 
 
-def test_compute_wavelet_array():
+def test_compute_wavelet_array(clear_sky_index, expect_tmscale, expect_wavelet):
     wavelet, tmscale = scaling._compute_wavelet(clear_sky_index, dt)
     assert_almost_equal(tmscale, expect_tmscale)
     assert_almost_equal(wavelet[0:3, 5000:5005], expect_wavelet)
 
 
-def test_compute_wavelet_array_invalid():
+def test_compute_wavelet_array_invalid(clear_sky_index):
     with pytest.raises(ValueError):
         scaling._compute_wavelet(clear_sky_index)
 
 
 @requires_scipy
-def test_wvm_series():
+def test_wvm_series(clear_sky_index, time, positions, expect_cs_smooth):
     csi_series = pd.Series(clear_sky_index, index=time)
     cs_sm, _, _ = scaling.wvm(csi_series, positions, cloud_speed)
     assert_almost_equal(cs_sm[5000:5005], expect_cs_smooth, decimal=4)
 
 
 @requires_scipy
-def test_wvm_array():
+def test_wvm_array(clear_sky_index, positions, expect_cs_smooth):
     cs_sm, _, _ = scaling.wvm(clear_sky_index, positions, cloud_speed, dt=dt)
     assert_almost_equal(cs_sm[5000:5005], expect_cs_smooth, decimal=4)
 
 
 @requires_scipy
-def test_wvm_series_xyaslist():
+def test_wvm_series_xyaslist(clear_sky_index, time, positions,
+                             expect_cs_smooth):
     csi_series = pd.Series(clear_sky_index, index=time)
     cs_sm, _, _ = scaling.wvm(csi_series, positions.tolist(), cloud_speed)
     assert_almost_equal(cs_sm[5000:5005], expect_cs_smooth, decimal=4)
 
 
 @requires_scipy
-def test_wvm_invalid():
+def test_wvm_invalid(clear_sky_index, positions):
     with pytest.raises(ValueError):
         scaling.wvm(clear_sky_index, positions, cloud_speed)

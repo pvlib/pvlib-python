4cb0b0de2200c832c32afcd0da4783a58b1a9edc
Kevin Anderson <kevinanderso@gmail.com>
2019-12-09 20:01:24 -0700

    improve docstring sphinx rendering in tracking.singleaxis

--- pvlib/tracking.py
+++ pvlib/tracking.py
@@ -305,17 +305,16 @@ def singleaxis(apparent_zenith, apparent_azimuth,
     Returns
     -------
     dict or DataFrame with the following columns:
-
-    * tracker_theta: The rotation angle of the tracker.
-        tracker_theta = 0 is horizontal, and positive rotation angles are
-        clockwise.
-    * aoi: The angle-of-incidence of direct irradiance onto the
-        rotated panel surface.
-    * surface_tilt: The angle between the panel surface and the earth
-        surface, accounting for panel rotation.
-    * surface_azimuth: The azimuth of the rotated panel, determined by
-        projecting the vector normal to the panel's surface to the earth's
-        surface.
+        * `tracker_theta`: The rotation angle of the tracker.
+          tracker_theta = 0 is horizontal, and positive rotation angles are
+          clockwise.
+        * `aoi`: The angle-of-incidence of direct irradiance onto the
+          rotated panel surface.
+        * `surface_tilt`: The angle between the panel surface and the earth
+          surface, accounting for panel rotation.
+        * `surface_azimuth`: The azimuth of the rotated panel, determined by
+          projecting the vector normal to the panel's surface to the earth's
+          surface.
 
     References
     ----------

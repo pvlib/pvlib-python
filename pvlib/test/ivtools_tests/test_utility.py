383a1f8d784bdb5b8d6245f568e6fb46a92b18cc
Cliff Hansen <cwhanse@sandia.gov>
2019-12-20 16:40:07 -0700

    fix expected outcome for test_rectify_

--- pvlib/test/ivtools_tests/test_utility.py
+++ pvlib/test/ivtools_tests/test_utility.py
@@ -33,14 +33,14 @@ def test_rectify_iv_curve(ivcurve):
     voltage, current = ivcurve
 
     vexp_no_dec = np.array([0., 1.,  5., 10., 25., 25.00001, 28., 30., 47.,
-                            50.])
-    iexp_no_dec = np.array([8., 6., 6., 5., 4., 3., 2.5, 2.7, 0.5, 0.])
+                            51.])
+    iexp_no_dec = np.array([7., 6., 6., 5., 4., 3., 2.5, 2.7, 0.5, 0.])
     v, i = rectify_iv_curve(voltage, current)
     np.testing.assert_allclose(v, vexp_no_dec, atol=.0001)
     np.testing.assert_allclose(i, iexp_no_dec, atol=.0001)
 
-    vexp = np.array([0., 1., 5., 10., 25., 28., 30., 47., 50.])
-    iexp = np.array([8., 6., 6., 5., 3.5, 2.5, 2.7, 0.5, 0.])
+    vexp = np.array([0., 1., 5., 10., 25., 28., 30., 47., 51.])
+    iexp = np.array([7., 6., 6., 5., 3.5, 2.5, 2.7, 0.5, 0.])
     v, i = rectify_iv_curve(voltage, current, decimals=5)
     np.testing.assert_allclose(v, vexp, atol=.0001)
     np.testing.assert_allclose(i, iexp, atol=.0001)

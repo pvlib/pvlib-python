2d3335cf2c08b78747c24bb9d35a539bd2b790be
Mark Mikofski <bwana.marko@yahoo.com>
2020-01-09 00:05:34 -0800

    use fixtures for test_pvgis
    * also move META dict to a json file in pvlib/data

--- pvlib/test/test_pvgis.py
+++ pvlib/test/test_pvgis.py
@@ -1,6 +1,7 @@
 """
 test the pvgis IO tools
 """
+import json
 from pathlib import Path
 import numpy as np
 import pandas as pd
@@ -11,106 +12,144 @@ from pvlib.iotools import get_pvgis_tmy
 TESTS = Path(__file__).parent
 PROJECT = TESTS.parent
 DATA = PROJECT / 'data'
-EXPECTED = pd.read_csv(DATA / 'pvgis_tmy_test.dat', index_col='time(UTC)')
-USERHORIZON = pd.read_json(DATA / 'tmy_45.000_8.000_userhorizon.json')
-MONTHS_SELECTED = [
-    2009, 2012, 2014, 2010, 2011, 2013, 2011, 2011, 2013, 2013, 2013, 2011]
-INPUTS = {
-    'location': {'latitude': 45.0, 'longitude': 8.0, 'elevation': 250.0},
-    'meteo_data': {
-        'radiation_db': 'PVGIS-SARAH',
-        'meteo_db': 'ERA-Interim',
-        'year_min': 2005,
-        'year_max': 2016,
-        'use_horizon': True,
-        'horizon_db': 'DEM-calculated'}}
-EPW_META = {
-    'loc': 'LOCATION',
-    'city': 'unknown',
-    'state-prov': '-',
-    'country': 'unknown',
-    'data_type': 'ECMWF/ERA',
-    'WMO_code': 'unknown',
-    'latitude': 45.0,
-    'longitude': 8.0,
-    'TZ': 1.0,
-    'altitude': 250.0}
 
 
-def test_get_pvgis_tmy():
+@pytest.fixture
+def expected():
+    return pd.read_csv(DATA / 'pvgis_tmy_test.dat', index_col='time(UTC)')
+
+
+@pytest.fixture
+def userhorizon_expected():
+    return pd.read_json(DATA / 'tmy_45.000_8.000_userhorizon.json')
+
+
+@pytest.fixture
+def month_year_expected():
+    return [
+        2009, 2012, 2014, 2010, 2011, 2013, 2011, 2011, 2013, 2013, 2013, 2011]
+
+
+@pytest.fixture
+def inputs_expected():
+    return {
+        'location': {'latitude': 45.0, 'longitude': 8.0, 'elevation': 250.0},
+        'meteo_data': {
+            'radiation_db': 'PVGIS-SARAH',
+            'meteo_db': 'ERA-Interim',
+            'year_min': 2005,
+            'year_max': 2016,
+            'use_horizon': True,
+            'horizon_db': 'DEM-calculated'}}
+
+
+@pytest.fixture
+def epw_meta():
+    return {
+        'loc': 'LOCATION',
+        'city': 'unknown',
+        'state-prov': '-',
+        'country': 'unknown',
+        'data_type': 'ECMWF/ERA',
+        'WMO_code': 'unknown',
+        'latitude': 45.0,
+        'longitude': 8.0,
+        'TZ': 1.0,
+        'altitude': 250.0}
+
+
+@pytest.fixture
+def meta_expected():
+    with open(DATA / 'pvgis_tmy_meta.json') as f:
+        return json.load(f)
+
+
+@pytest.fixture
+def csv_meta(meta_expected):
+    return [
+        '%s: %s (%s)' % (k, v['description'], v['units']) for k, v
+        in meta_expected['outputs']['tmy_hourly']['variables'].items()]
+
+
+def test_get_pvgis_tmy(expected, month_year_expected, inputs_expected,
+                       meta_expected):
     data, months_selected, inputs, meta = get_pvgis_tmy(45, 8)
     # check each column of output separately
-    for outvar in META['outputs']['tmy_hourly']['variables'].keys():
-        assert np.allclose(data[outvar], EXPECTED[outvar], equal_nan=True)
+    for outvar in meta_expected['outputs']['tmy_hourly']['variables'].keys():
+        assert np.allclose(data[outvar], expected[outvar])
     assert np.allclose(
         [_['month'] for _ in months_selected], np.arange(1, 13, 1))
-    assert np.allclose([_['year'] for _ in months_selected], MONTHS_SELECTED)
-    assert inputs['location']['latitude'] == INPUTS['location']['latitude']
-    assert inputs['location']['longitude'] == INPUTS['location']['longitude']
-    assert inputs['location']['elevation'] == INPUTS['location']['elevation']
+    assert np.allclose(
+        [_['year'] for _ in months_selected], month_year_expected)
+    inputs_loc = inputs['location']
+    assert inputs_loc['latitude'] == inputs_expected['location']['latitude']
+    assert inputs_loc['longitude'] == inputs_expected['location']['longitude']
+    assert inputs_loc['elevation'] == inputs_expected['location']['elevation']
     inputs_met_data = inputs['meteo_data']
-    expected_met_data = INPUTS['meteo_data']
+    expected_met_data = inputs_expected['meteo_data']
     assert (
         inputs_met_data['radiation_db'] == expected_met_data['radiation_db'])
     assert inputs_met_data['year_min'] == expected_met_data['year_min']
     assert inputs_met_data['year_max'] == expected_met_data['year_max']
     assert inputs_met_data['use_horizon'] == expected_met_data['use_horizon']
     assert inputs_met_data['horizon_db'] == expected_met_data['horizon_db']
-    assert meta == META
+    assert meta == meta_expected
 
 
-def test_get_pvgis_tmy_kwargs():
+def test_get_pvgis_tmy_kwargs(userhorizon_expected):
     _, _, inputs, _ = get_pvgis_tmy(45, 8, usehorizon=False)
     assert inputs['meteo_data']['use_horizon'] is False
     data, _, _, _ = get_pvgis_tmy(
         45, 8, userhorizon=[0, 10, 20, 30, 40, 15, 25, 5])
     assert np.allclose(
-        data['G(h)'], USERHORIZON['G(h)'].values, equal_nan=True)
+        data['G(h)'], userhorizon_expected['G(h)'].values)
     assert np.allclose(
-        data['Gb(n)'], USERHORIZON['Gb(n)'].values, equal_nan=True)
+        data['Gb(n)'], userhorizon_expected['Gb(n)'].values)
     assert np.allclose(
-        data['Gd(h)'], USERHORIZON['Gd(h)'].values, equal_nan=True)
+        data['Gd(h)'], userhorizon_expected['Gd(h)'].values)
     _, _, inputs, _ = get_pvgis_tmy(45, 8, startyear=2005)
     assert inputs['meteo_data']['year_min'] == 2005
     _, _, inputs, _ = get_pvgis_tmy(45, 8, endyear=2016)
     assert inputs['meteo_data']['year_max'] == 2016
 
 
-def test_get_pvgis_tmy_basic():
+def test_get_pvgis_tmy_basic(expected, meta_expected):
     data, _, _, _ = get_pvgis_tmy(45, 8, outputformat='basic')
     # check each column of output separately
-    for outvar in META['outputs']['tmy_hourly']['variables'].keys():
-        assert np.allclose(data[outvar], EXPECTED[outvar], equal_nan=True)
+    for outvar in meta_expected['outputs']['tmy_hourly']['variables'].keys():
+        assert np.allclose(data[outvar], expected[outvar])
 
 
-def test_get_pvgis_tmy_csv():
+def test_get_pvgis_tmy_csv(expected, month_year_expected, inputs_expected,
+                           meta_expected, csv_meta):
     data, months_selected, inputs, meta = get_pvgis_tmy(
         45, 8, outputformat='csv')
     # check each column of output separately
-    for outvar in META['outputs']['tmy_hourly']['variables'].keys():
-        assert np.allclose(data[outvar], EXPECTED[outvar], equal_nan=True)
+    for outvar in meta_expected['outputs']['tmy_hourly']['variables'].keys():
+        assert np.allclose(data[outvar], expected[outvar])
     assert np.allclose(
         [_['month'] for _ in months_selected], np.arange(1, 13, 1))
-    assert np.allclose([_['year'] for _ in months_selected], MONTHS_SELECTED)
-    assert inputs['latitude'] == INPUTS['location']['latitude']
-    assert inputs['longitude'] == INPUTS['location']['longitude']
-    assert inputs['elevation'] == INPUTS['location']['elevation']
+    assert np.allclose(
+        [_['year'] for _ in months_selected], month_year_expected)
+    assert inputs['latitude'] == inputs_expected['location']['latitude']
+    assert inputs['longitude'] == inputs_expected['location']['longitude']
+    assert inputs['elevation'] == inputs_expected['location']['elevation']
     for meta_value in meta:
         if not meta_value:
             continue
         if meta_value == 'PVGIS (c) European Communities, 2001-2020':
             continue
-        assert meta_value in META_VALUES
+        assert meta_value in csv_meta
 
 
-def test_get_pvgis_tmy_epw():
+def test_get_pvgis_tmy_epw(expected, epw_meta):
     data, _, _, meta = get_pvgis_tmy(
         45, 8, outputformat='epw')
-    assert np.allclose(data.ghi, EXPECTED['G(h)'], equal_nan=True)
-    assert np.allclose(data.dni, EXPECTED['Gb(n)'], equal_nan=True)
-    assert np.allclose(data.dhi, EXPECTED['Gd(h)'], equal_nan=True)
-    assert np.allclose(data.temp_air, EXPECTED['T2m'], equal_nan=True)
-    assert meta == EPW_META
+    assert np.allclose(data.ghi, expected['G(h)'])
+    assert np.allclose(data.dni, expected['Gb(n)'])
+    assert np.allclose(data.dhi, expected['Gd(h)'])
+    assert np.allclose(data.temp_air, expected['T2m'])
+    assert meta == epw_meta
 
 
 def test_get_pvgis_tmy_error():
@@ -119,64 +158,3 @@ def test_get_pvgis_tmy_error():
         get_pvgis_tmy(45, 8, outputformat='bad')
     with pytest.raises(requests.HTTPError, match='404 Client Error'):
         get_pvgis_tmy(45, 8, url='https://re.jrc.ec.europa.eu/')
-
-
-META = {
-    'inputs': {
-        'location': {
-            'description': 'Selected location',
-            'variables': {
-                'latitude': {
-                    'description': 'Latitude', 'units': 'decimal degree'},
-                'longitude': {
-                    'description': 'Longitude', 'units': 'decimal degree'},
-                'elevation': {'description': 'Elevation', 'units': 'm'}}},
-        'meteo_data': {
-            'description': 'Sources of meteorological data',
-            'variables': {
-                'radiation_db': {'description': 'Solar radiation database'},
-                'meteo_db': {
-                    'description': 'Database used for meteorological variables'
-                                   ' other than solar radiation'},
-                'year_min': {'description': 'First year of the calculations'},
-                'year_max': {'description': 'Last year of the calculations'},
-                'use_horizon': {'description': 'Include horizon shadows'},
-                'horizon_db': {'description': 'Source of horizon data'}}}},
-    'outputs': {
-        'months_selected': {
-            'type': 'time series',
-            'timestamp': 'monthly',
-            'description': 'months selected for the TMY'},
-        'tmy_hourly': {
-            'type': 'time series',
-            'timestamp': 'hourly',
-            'variables': {
-                'T2m': {
-                    'description': '2-m air temperature',
-                    'units': 'degree Celsius'},
-                'RH': {'description': 'relative humidity', 'units': '%'},
-                'G(h)': {
-                    'description': 'Global irradiance on the horizontal plane',
-                    'units': 'W/m2'},
-                'Gb(n)': {
-                    'description': 'Beam/direct irradiance on a plane always'
-                                   ' normal to sun rays',
-                    'units': 'W/m2'},
-                'Gd(h)': {
-                    'description': 'Diffuse irradiance on the horizontal'
-                                   ' plane',
-                    'units': 'W/m2'},
-                'IR(h)': {
-                    'description': 'Surface infrared (thermal) irradiance on a'
-                                   ' horizontal plane',
-                    'units': 'W/m2'},
-                'WS10m': {
-                    'description': '10-m total wind speed', 'units': 'm/s'},
-                'WD10m': {
-                    'description': '10-m wind direction (0 = N, 90 = E)',
-                    'units': 'degree'},
-                'SP': {
-                    'description': 'Surface (air) pressure', 'units': 'Pa'}}}}}
-META_VALUES = [
-    '%s: %s (%s)' % (k, v['description'], v['units'])
-    for k, v in META['outputs']['tmy_hourly']['variables'].items()]

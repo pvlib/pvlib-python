15d4d6b47344e49e03c2738e6747df26511733fa
Cliff Hansen <cwhanse@sandia.gov>
2020-01-23 08:57:24 -0700

    convert update_rsh test, move to test_sdm.py

--- pvlib/tests/ivtools/test__update_rsh_fixed_pt.py
+++ pvlib/tests/ivtools/test__update_rsh_fixed_pt.py
@@ -1,89 +1,40 @@
 import numpy as np
+import pytest
+
 from pvlib.tests.conftest import requires_scipy
 from pvlib.ivtools.sdm import _update_rsh_fixed_pt
 
 
 @requires_scipy
-def test_answer():
-    outrsh = _update_rsh_fixed_pt(np.array([2.]), np.array([2.]),
-                                  np.array([2.]), np.array([2.]),
-                                  np.array([2.]), np.array([2.]),
-                                  np.array([2.]))
+@pytest.mark.parametrize('vmp, imp, iph, io, rs, rsh, nnsvth, expected', [
+    (2., 2., 2., 2., 2., 2., 2., np.nan),
+    (2., 0., 2., 2., 2., 2., 2., np.nan),
+    (2., 2., 0., 2., 2., 2., 2., np.nan),
+    (2., 2., 2., 0., 2., 2., 2., np.nan),
+    (2., 2., 2., 2., 0., 2., 2., np.nan),
+    (2., 2., 2., 2., 2., 0., 2., np.nan),
+    (2., 2., 2., 2., 2., 2., 0., np.nan)])
+def test__update_rsh_fixed_pt_nans(vmp, imp, iph, io, rs, rsh, nnsvth,
+                                   expected):
+    outrsh = _update_rsh_fixed_pt(vmp, imp, iph, io, rs, rsh, nnsvth)
     assert np.isnan(outrsh)
 
-
 @requires_scipy
-def test_answer1():
-    outrsh1 = _update_rsh_fixed_pt(np.array([0.]), np.array([2.]),
-                                   np.array([2.]), np.array([2.]),
-                                   np.array([2.]), np.array([2.]),
-                                   np.array([2.]))
-    assert np.isnan(outrsh1)
+def test__update_rsh_fixed_pt_vmp0():
+    outrsh = _update_rsh_fixed_pt(vmp=0., imp=2., iph=2., io=2., rs=2.,
+                                  rsh=2., nnsvth=2.)
+    np.testing.assert_allclose(outrsh, np.array([502]), atol=.0001)
 
 
 @requires_scipy
-def test_answer2():
-    outrsh2 = _update_rsh_fixed_pt(np.array([2.]), np.array([0.]),
-                                   np.array([2.]), np.array([2.]),
-                                   np.array([2.]), np.array([2.]),
-                                   np.array([2.]))
-    assert np.isnan(outrsh2)
-
-
-@requires_scipy
-def test_answer3():
-    outrsh3 = _update_rsh_fixed_pt(np.array([2.]), np.array([2.]),
-                                   np.array([0.]), np.array([2.]),
-                                   np.array([2.]), np.array([2.]),
-                                   np.array([2.]))
-    assert np.isnan(outrsh3)
-
-
-@requires_scipy
-def test_answer4():
-    outrsh4 = _update_rsh_fixed_pt(np.array([2.]), np.array([2.]),
-                                   np.array([2.]), np.array([0.]),
-                                   np.array([2.]), np.array([2.]),
-                                   np.array([2.]))
-    assert np.isnan(outrsh4)
-
-
-@requires_scipy
-def test_answer5():
-    outrsh5 = _update_rsh_fixed_pt(np.array([2.]), np.array([2.]),
-                                   np.array([2.]), np.array([2.]),
-                                   np.array([0.]), np.array([2.]),
-                                   np.array([2.]))
-    assert np.isnan(outrsh5)
-
-
-@requires_scipy
-def test_answer6():
-    outrsh6 = _update_rsh_fixed_pt(np.array([2.]), np.array([2.]),
-                                   np.array([2.]), np.array([2.]),
-                                   np.array([2.]), np.array([0.]),
-                                   np.array([2.]))
-    assert np.isnan(outrsh6)
-
-
-@requires_scipy
-def test_answer7():
-    outrsh7 = _update_rsh_fixed_pt(np.array([2.]), np.array([2.]),
-                                   np.array([2.]), np.array([2.]),
-                                   np.array([2.]), np.array([2.]),
-                                   np.array([0.]))
-    np.testing.assert_allclose(outrsh7, np.array([502]), atol=.0001)
-
-
-@requires_scipy
-def test_answer8():
-    outrsh8 = _update_rsh_fixed_pt(np.array([-1., 3, .5]),
-                                   np.array([1., -.5, 2.]),
-                                   np.array([.2, .3, -.4]),
-                                   np.array([-.1, 1, 3]),
-                                   np.array([4., -.2, .1]),
-                                   np.array([.2, .2, -1]),
-                                   np.array([0., -1, 0.]))
+def test__update_rsh_fixed_pt_vector():
+    outrsh8 = _update_rsh_fixed_pt(rsh=np.array([-1., 3, .5]),
+                                   rs=np.array([1., -.5, 2.]),
+                                   io=np.array([.2, .3, -.4]),
+                                   iph=np.array([-.1, 1, 3]),
+                                   nnsvth=np.array([4., -.2, .1]),
+                                   imp=np.array([.2, .2, -1]),
+                                   vmp=np.array([0., -1, 0.]))
     assert np.isnan(outrsh8[0])
     assert np.isnan(outrsh8[1])
     assert np.isnan(outrsh8[2])
--- pvlib/tests/ivtools/test_sdm.py
+++ pvlib/tests/ivtools/test_sdm.py
@@ -270,3 +270,38 @@ def test_fit_pvsyst_sandia(disp=False, npts=3000):
     assert np.allclose(
         expected['rsh'][rshmask], pvsyst['rsh'][:npts][rshmask],
         equal_nan=True, rtol=0.63)
+
+
+@requires_scipy
+@pytest.mark.parametrize('vmp, imp, iph, io, rs, rsh, nnsvth, expected', [
+    (2., 2., 2., 2., 2., 2., 2., np.nan),
+    (2., 0., 2., 2., 2., 2., 2., np.nan),
+    (2., 2., 0., 2., 2., 2., 2., np.nan),
+    (2., 2., 2., 0., 2., 2., 2., np.nan),
+    (2., 2., 2., 2., 0., 2., 2., np.nan),
+    (2., 2., 2., 2., 2., 0., 2., np.nan),
+    (2., 2., 2., 2., 2., 2., 0., np.nan)])
+def test__update_rsh_fixed_pt_nans(vmp, imp, iph, io, rs, rsh, nnsvth,
+                                   expected):
+    outrsh = sdm._update_rsh_fixed_pt(vmp, imp, iph, io, rs, rsh, nnsvth)
+    assert np.isnan(outrsh)
+
+@requires_scipy
+def test__update_rsh_fixed_pt_vmp0():
+    outrsh = sdm._update_rsh_fixed_pt(vmp=0., imp=2., iph=2., io=2., rs=2.,
+                                      rsh=2., nnsvth=2.)
+    np.testing.assert_allclose(outrsh, np.array([502]), atol=.0001)
+
+
+@requires_scipy
+def test__update_rsh_fixed_pt_vector():
+    outrsh8 = sdm._update_rsh_fixed_pt(rsh=np.array([-1., 3, .5]),
+                                       rs=np.array([1., -.5, 2.]),
+                                       io=np.array([.2, .3, -.4]),
+                                       iph=np.array([-.1, 1, 3]),
+                                       nnsvth=np.array([4., -.2, .1]),
+                                       imp=np.array([.2, .2, -1]),
+                                       vmp=np.array([0., -1, 0.]))
+    assert np.isnan(outrsh8[0])
+    assert np.isnan(outrsh8[1])
+    assert np.isnan(outrsh8[2])

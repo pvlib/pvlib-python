e0870466a9c98854a36a6e7b958208d6399fba2f
Kevin Anderson <57452607+kanderso-nrel@users.noreply.github.com>
2020-01-29 18:47:44 -0700

    Apply suggestions from code review
    Co-Authored-By: Cliff Hansen <cwhanse@sandia.gov>

--- docs/examples/plot_singlediode.py
+++ docs/examples/plot_singlediode.py
@@ -1,5 +1,5 @@
 """
-Single-diode equation
+Calculating a module's IV curves
 =====================
 
 Examples of modeling IV curves using a single-diode circuit equivalent model.
@@ -8,7 +8,7 @@ Examples of modeling IV curves using a single-diode circuit equivalent model.
 #%%
 # Calculating a module IV curve for certain operating conditions is a two-step
 # process.  Multiple methods exist for both parts of the process.  Here we use
-# the De Soto 5-parameter model to calculate the electrical characteristics
+# the De Soto model [DeSoto]_ to calculate the electrical parameters for an IV curve at a certain irradiance```
 # of a PV module at a certain irradiance and temperature using the module's
 # base characteristics at reference conditions.  Those parameters are then used
 # to calculate the module's IV curve by solving the single-diode equation using
@@ -19,7 +19,12 @@ Examples of modeling IV curves using a single-diode circuit equivalent model.
 # conditions.  For more details on the single-diode equation and the five
 # parameters, see the `PVPMC single diode page
 # <https://pvpmc.sandia.gov/modeling-steps/2-dc-module-iv/diode-equivalent-circuit-models/>`_.
-#
+    References
+    ----------
+#    .. [1] W. De Soto et al., "Improvement and validation of a model for
+       photovoltaic array performance", Solar Energy, vol 80, pp. 78-88,
+       2006.
+
 # Calculating IV Curves
 # -----------------------
 # This example uses :py:meth:`pvlib.pvsystem.calcparams_desoto` to calculate

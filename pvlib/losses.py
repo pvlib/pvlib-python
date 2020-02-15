64a6498b689d27d77bb5967e13eab9bc0018d68e
Mark Mikofski <bwana.marko@yahoo.com>
2020-02-15 06:22:30 -0800

    add Kimber soiling model (#860)
    * add Kimber soiling model
    
    * remove spaces, thx stickler
    
    * typos: aa = a, rest = reset
    
    * move soiling to losses
    
    * add test for kimber soiling model
    
    * also add a gallery example draft
    
    * add kimber to api.rst
    
    * use pd.to_datetime for plt
    * start to add manwash test using datetime
    
    * add test for manual wash
    
    * add test data for manual wash
    * try to fix example plot using to_pydatetime
    
    * please fix kimber gallery example
    
    * Fix indent on references
    
    * add test for no rain, max soil
    
    * reuse rainfall events
    * combine zero soil assignments
    
    * fix typos in Kimber soiling model
    
    * correct Adrianne spelling
    * add table of soiling rates from figure 3 from Kimber paper
    * fix reference indents in docstring
    
    * update what's new w/ kimber model
    
    * fix table malformed table in losses
    * fix long line in docstring
    
    * Apply suggestions from code review
    
    * change `rainfall_timeseries` input arg to just `rainfall`
    * clarification of rainfall input, put units in brackets at end of definition
    * change `threshold` input arg to `cleaning_threashold`
    * change `soiling_rate` input arg to `soiling_loss_rate` and clarify definition as energy loss putting units [unitless] in brackets at the end of the definition
    * remove suggestion regarding grace period to the Notes section
    * fix formatting of return, there is no name, only return type
    * improve notes section to add that grace period may also vary by region and to use energy loss rate instead of soiling since it's not a mass rate
    
    Co-Authored-By: Cliff Hansen <cwhanse@sandia.gov>
    
    * update rainfall and threshold args in kimber
    
    * to match docstring improvement suggested in code review
    * wrap long lines
    * be more consistent with HSU docstring
    
    * change soiling_rate to soiling_loss_rate
    
    * combine panels cleaned today with grace-period,
     since it includes today
    
    * add a 1-liner for `soiling_kimber` in the sphinx docs
    
    * also fix missing "for" in note
    
    Co-Authored-By: Kevin Anderson <57452607+kanderso-nrel@users.noreply.github.com>
    
    * separate tests for Kimber
    
    * remove extra whitespace
    
    * delete trailing whitespace in kimber test
    
    * vectorize kimber soiling
    
    * use a rolling window and rain_accum_period to get more accurate rain coverage
     overnite instead of only discrete days from midnite to midnite
    * add rain_accum_period to allow deviation from kimber model, recommends
    only 24h but why not be flexible?
    * add istmy=False to fix last hour of tmy to be monotonically increasing
    * add another test for initial condition
    * fix example input arg cleaning_threshold
    
    * fix trailing whitespace in kimber
    
    * Kimber needs pandas-0.22
    
    Co-authored-by: Cliff Hansen <cwhanse@sandia.gov>
    Co-authored-by: Kevin Anderson <57452607+kanderso-nrel@users.noreply.github.com>

--- pvlib/losses.py
+++ pvlib/losses.py
@@ -4,6 +4,7 @@ This module contains functions for losses of various types: soiling, mismatch,
 snow cover, etc.
 """
 
+import datetime
 import numpy as np
 import pandas as pd
 from pvlib.tools import cosd
@@ -85,3 +86,130 @@ def soiling_hsu(rainfall, cleaning_threshold, tilt, pm2_5, pm10,
     soiling_ratio = 1 - 0.3437 * erf(0.17 * accum_mass**0.8473)
 
     return soiling_ratio
+
+
+def soiling_kimber(rainfall, cleaning_threshold=6, soiling_loss_rate=0.0015,
+                   grace_period=14, max_soiling=0.3, manual_wash_dates=None,
+                   initial_soiling=0, rain_accum_period=24, istmy=False):
+    """
+    Calculate soiling ratio with rainfall data and a daily soiling rate using
+    the Kimber soiling model [1]_.
+
+    Kimber soiling model assumes soiling builds up at a daily rate unless
+    the daily rainfall is greater than a threshold. The model also assumes that
+    if daily rainfall has exceeded the threshold within a grace period, then
+    the ground is too damp to cause soiling build-up. The model also assumes
+    there is a maximum soiling build-up. Scheduled manual washes and rain
+    events are assumed to reset soiling to zero.
+
+    Parameters
+    ----------
+    rainfall: pandas.Series
+        Accumulated rainfall at the end of each time period. [mm]
+    cleaning_threshold: float, default 6
+        Amount of daily rainfall required to clean the panels. [mm]
+    soiling_loss_rate: float, default 0.0015
+        Fraction of energy lost due to one day of soiling. [unitless]
+    grace_period : int, default 14
+        Number of days after a rainfall event when it's assumed the ground is
+        damp, and so it's assumed there is no soiling. [days]
+    max_soiling : float, default 0.3
+        Maximum fraction of energy lost due to soiling. Soiling will build up
+        until this value. [unitless]
+    manual_wash_dates : sequence or None, default None
+        List or tuple of dates as Python ``datetime.date`` when the panels were
+        washed manually. Note there is no grace period after a manual wash, so
+        soiling begins to build up immediately.
+    initial_soiling : float, default 0
+        Initial fraction of energy lost due to soiling at time zero in the
+        `rainfall` series input. [unitless]
+    rain_accum_period : int, default 24
+        Period for accumulating rainfall to check against `cleaning_threshold`.
+        The Kimber model defines this period as one day. [hours]
+    istmy : bool, default False
+        Fix last timestep in TMY so that it is monotonically increasing.
+
+    Returns
+    -------
+    pandas.Series
+        fraction of energy lost due to soiling, has same intervals as input
+
+    Notes
+    -----
+    The soiling loss rate depends on both the geographical region and the
+    soiling environment type. Rates measured by Kimber [1]_ are summarized in
+    the following table:
+
+    ===================  =======  =========  ======================
+    Region/Environment   Rural    Suburban   Urban/Highway/Airport
+    ===================  =======  =========  ======================
+    Central Valley       0.0011   0.0019     0.0020
+    Northern CA          0.0011   0.0010     0.0016
+    Southern CA          0        0.0016     0.0019
+    Desert               0.0030   0.0030     0.0030
+    ===================  =======  =========  ======================
+
+    Rainfall thresholds and grace periods may also vary by region. Please
+    consult [1]_ for more information.
+
+    References
+    ----------
+    .. [1] "The Effect of Soiling on Large Grid-Connected Photovoltaic Systems
+       in California and the Southwest Region of the United States," Adrianne
+       Kimber, et al., IEEE 4th World Conference on Photovoltaic Energy
+       Conference, 2006, :doi:`10.1109/WCPEC.2006.279690`
+    """
+    # convert rain_accum_period to timedelta
+    rain_accum_period = datetime.timedelta(hours=rain_accum_period)
+
+    # convert grace_period to timedelta
+    grace_period = datetime.timedelta(days=grace_period)
+
+    # get rainfall timezone, timestep as timedelta64, and timestep in int days
+    rain_tz = rainfall.index.tz
+    rain_index = rainfall.index.values
+    timestep_interval = (rain_index[1] - rain_index[0])
+    day_fraction = timestep_interval / np.timedelta64(24, 'h')
+
+    # if TMY fix to be monotonically increasing by rolling index by 1 interval
+    # and then adding 1 interval, while the values stay the same
+    if istmy:
+        rain_index = np.roll(rain_index, 1) + timestep_interval
+        # NOTE: numpy datetim64[ns] has no timezone
+        # convert to datetimeindex at UTC and convert to original timezone
+        rain_index = pd.DatetimeIndex(rain_index, tz='UTC').tz_convert(rain_tz)
+        # fixed rainfall timeseries with monotonically increasing index
+        rainfall = pd.Series(
+            rainfall.values, index=rain_index, name=rainfall.name)
+
+    # accumulate rainfall
+    accumulated_rainfall = rainfall.rolling(
+        rain_accum_period, closed='right').sum()
+
+    # soiling rate
+    soiling = np.ones_like(rainfall.values) * soiling_loss_rate * day_fraction
+    soiling[0] = initial_soiling
+    soiling = np.cumsum(soiling)
+
+    # rainfall events that clean the panels
+    rain_events = accumulated_rainfall > cleaning_threshold
+
+    # grace periods windows during which ground is assumed damp, so no soiling
+    grace_windows = rain_events.rolling(grace_period, closed='right').sum() > 0
+
+    # clean panels by subtracting soiling for indices in grace period windows
+    cleaning = pd.Series(float('NaN'), index=rainfall.index)
+    cleaning.iloc[0] = 0.0
+    cleaning[grace_windows] = soiling[grace_windows]
+
+    # manual wash dates
+    if manual_wash_dates is not None:
+        manual_wash_dates = pd.DatetimeIndex(manual_wash_dates, tz=rain_tz)
+        soiling = pd.Series(soiling, index=rain_index, name='soiling')
+        cleaning[manual_wash_dates] = soiling[manual_wash_dates]
+
+    # remove soiling by foward filling cleaning where NaN
+    soiling -= cleaning.ffill()
+
+    # check if soiling has reached the maximum
+    return soiling.where(soiling < max_soiling, max_soiling)

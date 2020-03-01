57882234845c7097fc80e56c62a99cca8b259acc
Mark Mikofski <bwana.marko@yahoo.com>
2020-03-01 07:25:36 -0800

    add read pvgis tmy (#907)
    * add read_pvgis_tmy
    
    * for files downloaded from pvgis tool
    * update tests, api, and docs
    * allow commit .csv to pvlib/data
    
    * update what's new with read_pvgis_tmy
    
    * closes #880
    * test import from api vs. full namespace path
    
    * py35 no likey fstring in read_pvgis_tmy
    
    * don't use is for string comparison, ditto
    * state returns, add see also
    
    * use str in json.load in read_pvgis
    
    * test read_pvgis_tmy_json, don't use binary buffer
    
    * try parse_epw in read_pvgis_tmy first
    
    * since attribute error only occurs for str/path
    
    * more comments, space between pvgis outputformats
    
    * raise ValueError if unknown output format
    * test if ValueError raised for bad output format
    
    * trailing new line in pvgis.py
    
    * use `pvgis_format` instead of confusing arg
    
    - infer parser format from file extension for csv, epw, and json
    - much more detailed docstring for `pvgis_format`
    - update tests for inferring format from extensions as well as failing
     if basic output format or file buffer isn't explicit
    
    * Apply pvgis suggestions from code review to docstring
    
    thanks Cliff!
    
    Co-Authored-By: Cliff Hansen <cwhanse@sandia.gov>
    
    * wrap long lines in read pvgis docstring
    
    * wordsmithing and formatting, hope it's okay
    * don't need to index filename to get pvgis_format,
     just use rsplit(',', 1)[-1] to file extension
    
    * use pathlib instead of rsplit in read_pvgis
    
    - TypeError raised if filename is buffer, not ValueError
    
    * python 3.5 has differnet messae for pathlib
    
    - py>3.5: expected str, bytes or os.PathLike object
    - py=3.5: argument should be a path or str object
    
    Co-authored-by: Cliff Hansen <cwhanse@sandia.gov>

--- pvlib/iotools/__init__.py
+++ pvlib/iotools/__init__.py
@@ -12,4 +12,4 @@ from pvlib.iotools.solrad import read_solrad  # noqa: F401
 from pvlib.iotools.psm3 import get_psm3  # noqa: F401
 from pvlib.iotools.psm3 import read_psm3  # noqa: F401
 from pvlib.iotools.psm3 import parse_psm3  # noqa: F401
-from pvlib.iotools.pvgis import get_pvgis_tmy  # noqa: F401
+from pvlib.iotools.pvgis import get_pvgis_tmy, read_pvgis_tmy  # noqa: F401

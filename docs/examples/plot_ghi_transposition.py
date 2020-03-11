e806e290facb6e66b8e8919201a6abe385fa4fae
ericf900 <61256041+ericf900@users.noreply.github.com>
2020-03-11 09:35:31 -0400

    Update docs/examples/plot_ghi_transposition.py
    Co-Authored-By: Kevin Anderson <57452607+kanderso-nrel@users.noreply.github.com>

--- docs/examples/plot_ghi_transposition.py
+++ docs/examples/plot_ghi_transposition.py
@@ -7,7 +7,7 @@ Example of generating clearsky GHI and POA irradiance.
 
 # %%
 # This example shows how to use the get_clearsky method to generate clearsky
-# GHI data as well as how to use the get_total_iradiance function to transpose
+# GHI data as well as how to use the :py:meth:`pvlib.irradiance.get_total_irradiance` function to transpose
 # GHI data to Plane of Array (POA) irradiance.
 
 from pvlib import location

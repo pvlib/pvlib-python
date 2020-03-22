8db2f63bbae9ecae445530e6f5921a32b025fef1
Kevin Anderson <kevin.anderson@nrel.gov>
2020-03-21 21:56:18 -0600

    add more benchmarks for location and solarposition

--- asv_bench/benchmarks/location.py
+++ asv_bench/benchmarks/location.py
@@ -1,20 +1,37 @@
-# Write the benchmarking functions here.
-# See "Writing benchmarks" in the asv docs for more information.
+"""
+ASV benchmarks for location.py
+"""
 
 import pandas as pd
 import pvlib
 
+
 class TimeSuite:
-    """
-    An example benchmark that times the performance of various kinds
-    of iterating over dictionaries in Python.
-    """
+
     def setup(self):
-        self.location = pvlib.location.Location(32, -110, altitude=700)
-        self.times = pd.DatetimeIndex(start='20180601', freq='3min',
-                                      periods=1440)
+        self.location = pvlib.location.Location(32, -110, altitude=700,
+                                                tz='Etc/GMT+7')
+        self.times = pd.date_range(start='20180601', freq='3min',
+                                   periods=1440)
+        self.days = pd.date_range(start='20180101', freq='d', periods=365,
+                                  tz=self.location.tz)
         self.solar_position = self.location.get_solarposition(self.times)
 
     # GH 502
     def time_location_get_airmass(self):
         self.location.get_airmass(solar_position=self.solar_position)
+
+    def time_location_get_solarposition(self):
+        self.location.get_solarposition(times=self.times)
+
+    def time_location_get_clearsky(self):
+        self.location.get_clearsky(times=self.times,
+                                   solar_position=self.solar_position)
+
+    def time_location_get_sun_rise_set_transit_pyephem(self):
+        self.location.get_sun_rise_set_transit(times=self.days,
+                                               method='pyephem')
+
+    def time_location_get_sun_rise_set_transit_spa(self):
+        self.location.get_sun_rise_set_transit(times=self.days,
+                                               method='spa')
--- asv_bench/benchmarks/solarposition.py
+++ asv_bench/benchmarks/solarposition.py
@@ -1,23 +1,34 @@
-# Write the benchmarking functions here.
-# See "Writing benchmarks" in the asv docs for more information.
+"""
+ASV benchmarks for location.py
+"""
 
 import pandas as pd
-import pvlib
+from pvlib import solarposition
+
 
 class TimeSuite:
-    """
-    An example benchmark that times the performance of various kinds
-    of iterating over dictionaries in Python.
-    """
+
     def setup(self):
-        self.times = pd.DatetimeIndex(start='20180601', freq='1min',
-                                      periods=14400)
+        self.times = pd.date_range(start='20180601', freq='1min',
+                                   periods=14400)
         self.times_localized = self.times.tz_localize('Etc/GMT+7')
+        self.lat = 35.1
+        self.lon = -106.6
 
     # GH 512
     def time_ephemeris(self):
-        pvlib.solarposition.ephemeris(self.times, 35.1, -106.6)
+        solarposition.ephemeris(self.times, self.lat, self.lon)
 
     # GH 512
     def time_ephemeris_localized(self):
-        pvlib.solarposition.ephemeris(self.times_localized, 35.1, -106.6)
+        solarposition.ephemeris(self.times_localized, self.lat, self.lon)
+
+    def time_spa_python(self):
+        solarposition.spa_python(self.times_localized[::5], self.lat, self.lon)
+
+    def time_sun_rise_set_transit_spa(self):
+        solarposition.sun_rise_set_transit_spa(self.times_localized[::30],
+                                               self.lat, self.lon)
+
+    def time_nrel_earthsun_distance(self):
+        solarposition.nrel_earthsun_distance(self.times_localized)

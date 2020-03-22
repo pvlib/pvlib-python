636bcafad7229c3f07693cddfab1df6585a0cd77
Kevin Anderson <kevin.anderson@nrel.gov>
2020-03-22 11:49:24 -0600

    add tracking benchmark file

---
+++ asv_bench/benchmarks/tracking.py
@@ -0,0 +1,32 @@
+"""
+ASV benchmarks for tracking.py
+"""
+
+import pandas as pd
+from pvlib import tracking, solarposition
+
+
+class TimeSuite:
+
+    def setup(self):
+        self.times = pd.date_range(start='20180601', freq='1min',
+                                   periods=14400)
+        self.lat = 35.1
+        self.lon = -106.6
+        self.solar_position = solarposition.get_solarposition(self.times,
+                                                              self.lat,
+                                                              self.lon)
+        self.tracker = tracking.SingleAxisTracker()
+
+    def time_singleaxis(self):
+        tracking.singleaxis(self.solar_position.apparent_zenith,
+                            self.solar_position.apparent_azimuth,
+                            axis_tilt=0,
+                            axis_azimuth=0,
+                            max_angle=60,
+                            backtrack=True,
+                            gcr=0.45)
+
+    def time_tracker_singleaxis(self):
+        self.tracker.singleaxis(self.solar_position.apparent_zenith,
+                                self.solar_position.apparent_azimuth)

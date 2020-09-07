daf7e40531e6116a4da4ea47f2c9f2d77cb5ef29
Kevin Anderson <kevin.anderson@nrel.gov>
2020-09-07 11:46:19 -0600

    do version switching in setup() instead of in global scope

--- benchmarks/temperature.py
+++ benchmarks/temperature.py
@@ -5,7 +5,7 @@ ASV benchmarks for irradiance.py
 import pandas as pd
 import pvlib
 from pkg_resources import parse_version
-import functools
+from functools import partial
 
 
 def set_weather_data(obj):
@@ -16,25 +16,26 @@ def set_weather_data(obj):
     obj.windspeed = pd.Series(2, index=obj.times)
 
 
-if parse_version(pvlib.__version__) >= parse_version('0.7.0'):
-    params = pvlib.temperature.TEMPERATURE_MODEL_PARAMETERS['sapm']
-    params = params['open_rack_glass_glass']
-    sapm_cell = functools.partial(pvlib.temperature.sapm_cell, **params)
-else:
-    sapm_celltemp = pvlib.pvsystem.sapm_celltemp
-
-    def sapm_cell(poa_global, temp_air, wind_speed):
-        return sapm_celltemp(poa_global, wind_speed, temp_air)
-
-
 class SAPM:
 
     def setup(self):
         set_weather_data(self)
+        if parse_version(pvlib.__version__) >= parse_version('0.7.0'):
+            kwargs = pvlib.temperature.TEMPERATURE_MODEL_PARAMETERS['sapm']
+            kwargs = kwargs['open_rack_glass_glass']
+            self.sapm_cell_wrapper = partial(pvlib.temperature.sapm_cell,
+                                            **kwargs)
+        else:
+            sapm_celltemp = pvlib.pvsystem.sapm_celltemp
+
+            def sapm_cell_wrapper(poa_global, temp_air, wind_speed):
+                # just swap order; model params are provided by default
+                return sapm_celltemp(poa_global, wind_speed, temp_air)
+            self.sapm_cell_wrapper = sapm_cell_wrapper
 
     def time_sapm_cell(self):
         # use version-appropriate wrapper
-        sapm_cell(self.poa, self.tamb, self.windspeed)
+        self.sapm_cell_wrapper(self.poa, self.tamb, self.windspeed)
 
 
 class Fuentes:

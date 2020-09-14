3e8e2562ef4c88b64a0420e2191779e1b1c444c9
Will Holmgren <william.holmgren@gmail.com>
2020-09-14 14:23:58 -0700

    add ephem and geometric transet benchmarks

--- benchmarks/benchmarks/solarposition.py
+++ benchmarks/benchmarks/solarposition.py
@@ -19,10 +19,12 @@ class SolarPosition:
 
     def setup(self):
         self.times = pd.date_range(start='20180601', freq='1min',
-                                   periods=14400)
+                                   periods=14400)  # 100 days
         self.times_localized = self.times.tz_localize('Etc/GMT+7')
         self.lat = 35.1
         self.lon = -106.6
+        self.times_daily = pd.date_range(
+            start='20180601', freq='24h', periods=100, tz='Etc/GMT+7')
 
     # GH 512
     def time_ephemeris(self):
@@ -33,11 +35,26 @@ class SolarPosition:
         solarposition.ephemeris(self.times_localized, self.lat, self.lon)
 
     def time_spa_python(self):
-        solarposition.spa_python(self.times_localized[::5], self.lat, self.lon)
+        solarposition.spa_python(self.times_localized, self.lat, self.lon)
 
     def time_sun_rise_set_transit_spa(self):
-        sun_rise_set_transit_spa(self.times_localized[::30],
-                                 self.lat, self.lon)
+        sun_rise_set_transit_spa(self.times_daily, self.lat, self.lon)
+
+    def time_sun_rise_set_transit_ephem(self):
+        solarposition.sun_rise_set_transit_ephem(
+            self.times_daily, self.lat, self.lon)
+
+    def time_sun_rise_set_transit_ephem_horizon(self):
+        solarposition.sun_rise_set_transit_ephem(
+            self.times_daily, self.lat, self.lon, horizon='3:00')
+
+    def time_sun_rise_set_transit_geometric(self):
+        dayofyear = self.times_daily.dayofyear
+        declination = solarposition.declination_spencer71(dayofyear)
+        equation_of_time = solarposition.equation_of_time_spencer71(dayofyear)
+        solarposition.sun_rise_set_transit_geometric(
+            self.times_daily, self.lat, self.lon, declination,
+            equation_of_time)
 
     def time_nrel_earthsun_distance(self):
         solarposition.nrel_earthsun_distance(self.times_localized)

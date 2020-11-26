bc33c0f1b02b3e22f0b3ee2a6139dacb0f5bd27a
michael <mjurasovic@gmail.com>
2020-11-27 09:31:10 +1100

    bump toggleprompt version to one that handles "Out[xx]:"

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ EXTRAS_REQUIRE = {
     'doc': ['ipython', 'matplotlib', 'sphinx == 3.1.2',
             'sphinx_rtd_theme==0.5.0', 'sphinx-gallery', 'docutils == 0.15.2',
             'pillow', 'netcdf4', 'siphon', 'tables',
-            'sphinx-toggleprompt >= 0.0.4'],
+            'sphinx-toggleprompt >= 0.0.5'],
     'test': TESTS_REQUIRE
 }
 EXTRAS_REQUIRE['all'] = sorted(set(sum(EXTRAS_REQUIRE.values(), [])))

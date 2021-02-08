ad8d45a23ffa2181ba056c69749d4bc114af97c1
Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
2021-02-08 10:00:02 +0100

    Reference to FTP updated

--- pvlib/iotools/bsrn.py
+++ pvlib/iotools/bsrn.py
@@ -28,8 +28,9 @@ def read_bsrn(filename):
     global, diffuse, direct and downwelling long-wave radiation [2]_. Future
     updates may include parsing of additional data and meta-data.
 
-    Required username and password are easily obtainable as described in the
-    BSRN's Data Release Guidelines [4]_.
+    BSRN files are freely avaiable and can be accessed via FTP [3]_. Required
+    username and password are easily obtainable as described in the BSRN's
+    Data Release Guidelines [4]_.
 
 
 

2d20a386c5be95337c0724c2c0a43fd2d29507e4
Cliff Hansen <cwhanse@sandia.gov>
2021-02-08 09:14:17 -0700

    remove old test

--- pvlib/tests/test_modelchain.py
+++ pvlib/tests/test_modelchain.py
@@ -1120,22 +1120,6 @@ def test_run_model_from_effective_irradiance_minimal_input(
     assert not mc.results.ac.empty
 
 
-def test_run_model_from_effective_irradiance_missing_poa(
-        sapm_dc_snl_ac_system_Array, location, total_irrad):
-    data_incomplete = pd.DataFrame(
-        {'effective_irradiance': total_irrad['poa_global'],
-         'poa_global': total_irrad['poa_global']},
-        index=total_irrad.index)
-    data_complete = pd.DataFrame(
-        {'effective_irradiance': total_irrad['poa_global'],
-         'cell_temperature': 30},
-        index=total_irrad.index)
-    mc = ModelChain(sapm_dc_snl_ac_system_Array, location)
-    with pytest.raises(ValueError,
-                       match="you must provide 'poa_global' for all Arrays"):
-        mc.run_model_from_effective_irradiance(
-            (data_complete, data_incomplete))
-
 
 def test_run_model_singleton_weather_single_array(cec_dc_snl_ac_system,
                                                   location, weather):

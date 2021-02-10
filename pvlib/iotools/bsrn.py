6b52a72506c155bdd681056582c5957564012eee
Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
2021-02-11 00:03:51 +0100

    Changed to_timedelta unit from minute' to 'T'

--- pvlib/iotools/bsrn.py
+++ pvlib/iotools/bsrn.py
@@ -136,6 +136,6 @@ def read_bsrn(filename):
     # Set datetime index
     data.index = (start_date
                   + pd.to_timedelta(data['day']-1, unit='d')
-                  + pd.to_timedelta(data['minute'], unit='minute'))
+                  + pd.to_timedelta(data['minute'], unit='T'))
 
     return data

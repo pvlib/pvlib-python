af3067e4798fca25067d8314df03e77b611d9fcc
AdamRJensen <adam-r-j@hotmail.com>
2021-08-08 22:30:55 +0200

    Remove grib support and update variable_map

--- pvlib/iotools/era5.py
+++ pvlib/iotools/era5.py
@@ -3,8 +3,8 @@
 """
 
 import requests
-import warnings
 
+# Can probably remove the netCDF4 dependency
 try:
     import netCDF4
 except ImportError:
@@ -32,27 +32,33 @@ except ImportError:
             raise ImportError(
                 'Retrieving ERA5 data requires cdsapi to be installed.')
 
+CDSAPI_URL = 'https://cds.climate.copernicus.eu/api/v2'
+
+
 ERA5_DEFAULT_VARIABLES = [
-    '10m_u_component_of_wind',
-    '10m_v_component_of_wind',
-    '2m_temperature',
-    'surface_pressure',
-    'mean_surface_downward_short_wave_radiation_flux',
-    'mean_surface_downward_short_wave_radiation_flux_clear_sky',
-    'mean_surface_direct_short_wave_radiation_flux',
-    'mean_surface_direct_short_wave_radiation_flux_clear_sky',
-    'mean_top_downward_short_wave_radiation_flux']
+    '2m_temperature',  # t2m
+    '10m_u_component_of_wind',  # u10
+    '10m_v_component_of_wind',  # v10
+    'surface_pressure',  # sp
+    'mean_surface_downward_short_wave_radiation_flux',  # msdwswrf
+    'mean_surface_downward_short_wave_radiation_flux_clear_sky',  # msdwswrfcs
+    'mean_surface_direct_short_wave_radiation_flux', # msdrswrf
+    'mean_surface_direct_short_wave_radiation_flux_clear_sky',  # msdrswrfcs
+    ]
 
-
+# The returned data uses short-names, whereas the request requires CDS variable
+# names - passing short-names results in an "Ambiguous" error being raised
 ERA5_VARIABLE_MAP = {
-    '2m_temperature': 'temp_air',
-    'surface_pressure': 'pressure',
-    'mean_surface_downward_short_wave_radiation_flux': 'ghi',
-    'mean_surface_downward_short_wave_radiation_flux_clear_sky': 'ghi_clear',
-    'mean_surface_direct_short_wave_radiation_flux': 'bhi',
-    'mean_surface_direct_short_wave_radiation_flux_clear_sky': 'bhi_clear',
-    'mean_top_downward_short_wave_radiation_flux': 'ghi_extra'}
-
+    't2m': 'temp_air',
+    'd2m': 'temp_dew',
+    'sp': 'pressure',
+    'msdwswrf': 'ghi',
+    'msdwswrfcs': 'ghi_clear',
+    'msdwlwrf': 'lwd',
+    'msdwlwrfcs': 'lwd_clear',
+    'msdrswrf': 'bhi',  # check this
+    'msdrswrfcs': 'bhi_clear',  # check this
+    'mtdwswrf': 'ghi_extra'}
 
 ERA5_EXTENSIONS = {
     'netcdf': '.nc',
@@ -64,11 +70,10 @@ ERA5_HOURS = [
     '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00']
 
 
-def get_era5(latitude, longitude, start, end,
+def get_era5(latitude, longitude, start, end, api_key=None,
              variables=ERA5_DEFAULT_VARIABLES,
              dataset_name='reanalysis-era5-single-levels',
-             product_type='reanalysis',
-             grid=(0.25, 0.25), local_path=None, file_format='netcdf',
+             product_type='reanalysis', grid=(0.25, 0.25), local_path=None,
              cds_client=None, map_variables=True):
     """
     Retrieve ERA5 reanalysis data from the Copernicus Data Store (CDS).
@@ -80,86 +85,83 @@ def get_era5(latitude, longitude, start, end,
     Temporal resolution: hourly
     Spatial resolution: 0.25° by 0.25°
     Spatial coverage: global
-    Projection: regular latitude-longitude grid
 
     Time-stamp: from the previous time stamp and backwards, i.e. end of period
     For the reanalysis, the accumulation period is over the 1 hour up to the validity date and time.
 
+    Note the variables specified should be the variable names used by in the
+    CDS. The returned data contains the short-name versions of the variables.
+    See [4]_ for a list of variables names and units. 
+
     Hint
     ----
-    In order to use the this function the package [cdsapi](https://github.com/ecmwf/cdsapi)
-    needs to be installed [3]_.
+    In order to use the this function the package
+    [cdsapi](https://github.com/ecmwf/cdsapi) needs to be installed [3]_.
 
-    Also, access requires user registration [3]_ and creating a local file with
-    your personal API key, see [4]_ for instructions.
+    Access requires user registration [3]_. The obtaining API key can either be
+    passed directly to the function or be saved in a local file as described in
+    [4]_.
 
-    Important
-    ---------
-    Retrieving data from the CDS data store is significanlty slower than
-    other solar radiation services. Requests are first queued before being
-    executed, and the process can easily exceed 20 minutes.
-
-    You can check the status of your api at
-    https://cds.climate.copernicus.eu/cdsapp#!/yourrequests
-
-    The status of all queued requests can be seen at
-    https://cds.climate.copernicus.eu/live/queue
+    You can check the status of your requests
+    [here](https://cds.climate.copernicus.eu/cdsapp#!/yourrequests) and the
+    status of all queued requests
+    [here](https://cds.climate.copernicus.eu/live/queue),
 
     Parameters
     ----------
-    start : datetime like
-        First timestamp of the requested period
-    end : datetime like
-        Last timestamp of the requested period
     latitude: float or list
         in decimal degrees, between -90 and 90, north is positive (ISO 19115)
         If latitude is a list, it should have the format [S, N] and
         latitudes within the range are selected according to the grid.
-    longitude : float or list
+    longitude: float or list
         in decimal degrees, between -180 and 180, east is positive (ISO 19115)
         If longitude is a list, it should have the format [W, E] and
         longitudes within the range are selected according to the grid.
-    variables
-    dataset_name: str, default: 'reanalysis-era5-single-levels'
-        Name of the dataset. Options are 'reanalysis-era5-single-levels',
-        'reanalysis-era5-single-levels-monthly-means',
-        'reanalysis-era5-land', or 'reanalysis-era5-land-monthly-means'
+    start: datetime like
+        First day of the requested period
+    end: datetime like
+        Last day of the requested period
+    api_key: str, optional
+        Personal API key for the CDS
+    variables: list, default: ERA5_DEFAULT_VARIABLES
+        List of variables to retrieve (requires CDS variable names)
+    dataset: str, default: {'reanalysis-era5-single-levels', 'reanalysis-era5-land'}
+        Name of the dataset
     product_type: str, {'reanalysis', 'ensemble_members', 'ensemble_mean',
                    'ensemble_spread'}, default: 'reanalysis'
-    grid : list or tuple, default: (0.25, 0.25)
-    file_format : {grib, netcdf}, default: netcdf
-        File format of retrieved data. Note NetCDF is experimental.
+        ERA5 product type
+    grid: list or tuple, default: (0.25, 0.25)
+        User specified grid resolution
     cds_client: CDS API client object, optional
+        CDS API client
     map_variables : bool, default: True
         When true, renames columns of the DataFrame to pvlib variable names
         where applicable. See variable ERA5_VARIABLE_MAP.
 
     Notes
     -----
-    The returned data DataFrame includes the following fields by default:
+    The returned data includes the following fields by default:
 
     ========================  ======  =========================================
     Key, mapped key           Format  Description
     ========================  ======  =========================================
     **Mapped field names are returned when the map_variables argument is True**
     ---------------------------------------------------------------------------
-    10m_u_component_of_wind   float   Horizontal air speed towards the east, at a height of 10 m [m/s]
-    10m_v_component_of_wind   float   Horizontal air speed towards the north, at a height of 10 m [m/s]
-    Clear sky GHI, ghi_clear  float   Clear sky global radiation on horizontal
-    2m_temperature            float   Air temperature at 2m above the ground (K)
-    surface_pressure          float   Atmospheric pressure at the ground (Pa)
-    toa_incident_solar_radiation float Horizontal solar irradiation at top of atmosphere (J/m^2)
-    clear_sky_direct_solar_radiation_at_surface float Clear-sky direct horizontal irradiance (J/m^2)
-    surface_solar_radiation_downward_clear_sky float Clear-sky global iradiation (J/m^2)
-    surface_solar_radiation_downwards float Global horizontal irradiation (GHI) (J/m^2)
-    total_sky_direct_solar_radiation_at_surface float Direct/beam horizontal irradiation (BHI) (J/m^2)
+    2tm, temp_air             float   Air temperature at 2 m above ground (K)
+    u10                       float   Horizontal air speed towards east at 10 m [m/s]
+    v10                       float   Horizontal air speed towards north at 10 m [m/s]
+    sp, pressure              float   Atmospheric pressure at the ground (Pa)
+    msdwswrf, ghi             float   Mean surface downward short-wave radiation flux [W/m^2]
+    msdwswrfcs, ghi_clear     float   Mean surface downward short-wave radiation flux, clear sky [W/m^2]
+    msdrswrf, bhi             float   Mean surface direct short-wave radiation flux [W/m^2]
+    msdrswrfcs, bhi_clear     float   Mean surface direct short-wave radiation flux, clear sky [W/m^2]
     ========================  ======  =========================================
 
     References
     ----------
     .. [1] `ERA5 hourly data on single levels from 1979 to present
        <https://cds.climate.copernicus.eu/cdsapp#!/dataset/reanalysis-era5-single-levels?tab=overview>`
-    .. [2] `ERA5 hourly data on single levels from 1979 to present
+    .. [2] `ERA5: data documentation
        <https://confluence.ecmwf.int/display/CKB/ERA5%3A+data+documentation>`
     .. [3] `How to use the CDS API
        <https://cds.climate.copernicus.eu/api-how-to>`
@@ -167,17 +169,18 @@ def get_era5(latitude, longitude, start, end,
        <https://cds.climate.copernicus.eu/user/register>`
     """  # noqa: E501
     if cds_client is None:
-        cds_client = cdsapi.Client()
+        cds_client = cdsapi.Client(url=CDSAPI_URL, key=api_key)
 
-    # Area is selected by a box made by the four coordinates: [N,W,S,E]
+    # Area is selected by a box made by the four coordinates: [N, W, S, E]
     if type(latitude) == list:
         area = [latitude[1], longitude[0], latitude[0], longitude[1]]
     else:
-        area = [latitude+0.001, longitude, latitude, longitude+0.001]
+        area = [latitude+0.005, longitude-0.005,
+                latitude-0.005, longitude+0.005]
 
     params = {
         'product_type': product_type,
-        'format': file_format,
+        'format': 'netcdf',
         'variable': variables,
         'date': start.strftime('%Y-%m-%d') + '/' + end.strftime('%Y-%m-%d'),
         'time': ERA5_HOURS,
@@ -187,27 +190,15 @@ def get_era5(latitude, longitude, start, end,
     # Retrieve path to the file
     file_location = cds_client.retrieve(dataset_name, params)
 
-    if file_format not in list(ERA5_EXTENSIONS):
-        raise ValueError(f"File format must be in {list(ERA5_EXTENSIONS)}")
-
     # Load file into memory
     with requests.get(file_location.location) as res:
-        if file_format == 'netcdf':  # open netcdf files using xarray
-            ds = xr.open_dataset(res.content)
 
         # Save the file locally if local_path has been specified
         if local_path is not None:
             with open(local_path, 'wb') as f:
                 f.write(res.content)
 
-    if (file_format == 'grib') & (local_path is not None):
-        warnings.warn('Parsing of grib files is not supported.')
-        ds = xr.open_dataset(local_path, engine='cfgrib')
-    elif (file_format == 'grib') & (local_path is None):
-        raise ValueError('local_path must be specified when file format is '
-                         'grib.')
-
-    return ds
+        return read_era5(res.content, map_variables=map_variables)
 
 
 def read_era5(filename, map_variables=True):
@@ -219,11 +210,22 @@ def read_era5(filename, map_variables=True):
     e.g., the time stamp 12:00 for hourly data refers to the period from 11.00
     to 12:00.
 
+
+    If the file contains only one location, then a DataFrame is returned,
+    otherwise a xarray dataset is returned.
+
     """
-    # load into memory
-    # with urlopen(filename) as f:
-    #     ds = xr.open_dataset(f.read())
+    ds = xr.open_dataset(filename)
 
-    ds = xr.open_dataset(filename)  # chunks=chunks)
+    metadata = ds.attrs
 
-    return ds
+    if map_variables:
+        # Renaming of xarray datasets throws an error if keys are missing
+        ds = ds.rename_vars(
+            {k: v for k, v in ERA5_VARIABLE_MAP.items() if k in list(ds)})
+
+    if (ds['latitude'].size == 1) & (ds['longitude'].size == 1):
+        data = ds.to_dataframe().droplevel(['latitude','longitude'])
+        return data, metadata
+    else:
+        return ds, metadata

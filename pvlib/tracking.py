8a588a3ea5c32383ea0834f2581ee4a5953a78a9
Mark Mikofski <bwana.marko@yahoo.com>
2021-08-07 21:55:08 -0700

    [sty] fix stickler

--- pvlib/tracking.py
+++ pvlib/tracking.py
@@ -458,8 +458,9 @@ def singleaxis(apparent_zenith, apparent_azimuth,
     sun_vec = np.array([xp, yp, zp])
 
     # calculate angle-of-incidence on panel
-    # TODO: use irradiance.aoi if can pass projection instead of solpos & surface
-    aoi = np.degrees(np.arccos(np.clip(np.sum(sun_vec*panel_norm, axis=0), -1, 1)))
+    # TODO: use irradiance.aoi
+    projection = np.clip(np.sum(sun_vec*panel_norm, axis=0), -1, 1)
+    aoi = np.degrees(np.arccos(projection))
 
     # Calculate panel tilt and azimuth in a coordinate system where the panel
     # tilt is the angle from horizontal, and the panel azimuth is the compass

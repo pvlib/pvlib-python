592563cd3b2a8a4ff6302cdd301170febee0140c
AdamRJensen <adam-r-j@hotmail.com>
2021-08-08 11:40:45 +0200

    Add merra2 functions to iotools init file

--- pvlib/iotools/__init__.py
+++ pvlib/iotools/__init__.py
@@ -21,3 +21,5 @@ from pvlib.iotools.bsrn import parse_bsrn  # noqa: F401
 from pvlib.iotools.sodapro import get_cams  # noqa: F401
 from pvlib.iotools.sodapro import read_cams  # noqa: F401
 from pvlib.iotools.sodapro import parse_cams  # noqa: F401
+from pvlib.iotools.merra2 import get_merra2  # noqa: F401
+from pvlib.iotools.merra2 import read_merra2  # noqa: F401

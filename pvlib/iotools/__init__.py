9f6b15fb3c81590672a18db34b2f0dc97f7c6c14
AdamRJensen <adam-r-j@hotmail.com>
2021-08-09 11:54:40 +0200

    Fix error in __init__.py

--- pvlib/iotools/__init__.py
+++ pvlib/iotools/__init__.py
@@ -17,5 +17,5 @@ from pvlib.iotools.bsrn import read_bsrn  # noqa: F401
 from pvlib.iotools.sodapro import get_cams  # noqa: F401
 from pvlib.iotools.sodapro import read_cams  # noqa: F401
 from pvlib.iotools.sodapro import parse_cams  # noqa: F401
-from pvlib.iotools.sodapro import get_era5  # noqa: F401
-from pvlib.iotools.sodapro import parse_era5  # noqa: F401
+from pvlib.iotools.era5 import get_era5  # noqa: F401
+from pvlib.iotools.era5 import parse_era5  # noqa: F401

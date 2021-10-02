a2b65a26e7bd5283e205d0c9706e82e2ec0ac2b2
ndclt <nicolas.duclert@estaca.eu>
2021-10-02 22:38:51 +0200

    test: latitude given as string should fail

--- pvlib/tests/test_location.py
+++ pvlib/tests/test_location.py
@@ -44,6 +44,11 @@ def test_location_invalid_tz_type():
         Location(32.2, -111, [5])
 
 
+def test_location_invalid_latitude_type():
+    with pytest.raises(TypeError):
+        Location('32.2', -111)
+
+
 def test_location_print_all():
     tus = Location(32.2, -111, 'US/Arizona', 700, 'Tucson')
     expected_str = '\n'.join([

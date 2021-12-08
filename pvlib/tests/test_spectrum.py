79454c4cc7c6669811b445ede5cf00010fd8b6b0
Kevin Anderson <57452607+kanderso-nrel@users.noreply.github.com>
2021-12-08 09:48:28 -0700

    Handle aoi>90 correctly in spectrum.spectrl2 (#1349)
    * implement fix and add docstring note
    
    * add test
    
    * whatsnew

--- pvlib/tests/test_spectrum.py
+++ pvlib/tests/test_spectrum.py
@@ -92,3 +92,17 @@ def test_dayofyear_missing(spectrl2_data):
     kwargs.pop('dayofyear')
     with pytest.raises(ValueError, match='dayofyear must be specified'):
         _ = spectrum.spectrl2(**kwargs)
+
+
+def test_aoi_gt_90(spectrl2_data):
+    # test that returned irradiance values are non-negative when aoi > 90
+    # see GH #1348
+    kwargs, _ = spectrl2_data
+    kwargs['apparent_zenith'] = 70
+    kwargs['aoi'] = 130
+    kwargs['surface_tilt'] = 60
+
+    spectra = spectrum.spectrl2(**kwargs)
+    for key in ['poa_direct', 'poa_global']:
+        message = f'{key} contains negative values for aoi>90'
+        assert np.all(spectra[key] >= 0), message

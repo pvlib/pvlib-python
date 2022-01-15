b45ab620ab93cf38df1b91f5825de95a5629aed8
Cliff Hansen <cwhanse@sandia.gov>
2022-01-15 13:57:28 -0700

    add test for mlfm_fit

--- pvlib/tests/test_mlfm.py
+++ pvlib/tests/test_mlfm.py
@@ -1,33 +1,7 @@
-'''
-written : 220111 Steve Ransome (SRCL)
-tests functions from mlfm.py
 
-TESTED
-def mlfm_meas_to_norm(dmeas, ref, qty_mlfm_vars):
-
-def mlfm_6(dmeas, c_1, c_2, c_3, c_4, c_5, c_6):
-
-def mlfm_norm_to_stack(dnorm, ref, qty_mlfm_vars):
-
-NOT TESTED YET
-def mlfm_fit(dmeas, dnorm, mlfm_sel):
-
-def plot_mlfm_scatter(dmeas, dnorm, mlfm_file_name, qty_mlfm_vars):
-
-def plot_mlfm_stack(dmeas, dnorm, dstack, ref,
-                    mlfm_file_name, qty_mlfm_vars,
-                    xaxis_labels=12, is_i_sc_self_ref=False,
-                    is_v_oc_temp_module_corr=True):
-'''
-
-'''
-Define tolerance for checking.
-Most values are normalised ~1
-0.000001 is probably good
-'''
-
+import numpy as np
 import pandas as pd
-from pvlib.mlfm import mlfm_meas_to_norm, mlfm_6, mlfm_norm_to_stack
+from pvlib.mlfm import mlfm_meas_to_norm, mlfm_6, mlfm_norm_to_stack, mlfm_fit
 from numpy.testing import assert_allclose  # assert_almost_equal,
 import pytest
 
@@ -143,6 +117,72 @@ def mlfm_6_coeffs():
     return c_1, c_2, c_3, c_4, c_5, c_6, expected
 
 
+@pytest.fixture
+def matrix_data():
+    # sample ghi, tmod, ws and pr_dc to fit
+    # this data selectable from mlfm.ipynb
+    # ---
+    # select one of the following meas files
+    # meas_file = 2  # <<< change from 0 to 2
+    # ---
+
+    return pd.DataFrame(np.array(
+       [[0.1, 15, 0, 0.935774123487434],
+        [0.2, 15, 0, 0.978281104560968],
+        [0.4, 15, 0, 1.00721377598511],
+        [0.6, 15, 0, 1.02254628193195],
+        [0.8, 15, 0, 1.02710983555693],
+        [1.0, 15, 0, 1.02655910642259],
+        [0.1, 25, 0, 0.907539559416693],
+        [0.2, 25, 0, 0.94849519081601],   # LIC
+        [0.4, 25, 0, 0.980840831523425],
+        [0.6, 25, 0, 0.994311717861206],
+        [0.8, 25, 0, 0.998914055228048],
+        [1.0, 25, 0, 1],                  # STC
+        [1.1, 25, 0, 0.998984571122331],
+        [0.1, 50, 0, 0.833074775054297],
+        [0.2, 50, 0, 0.879615265280794],
+        [0.4, 50, 0, 0.908004964318957],
+        [0.6, 50, 0, 0.920260626745268],
+        [0.8, 50, 0, 0.925496431895749],
+        [1.0, 50, 0, 0.927551970214086],
+        [1.1, 50, 0, 0.926324993653569],
+        [0.1, 75, 0, 0.746819733167856],
+        [0.2, 75, 0, 0.792739683524666],
+        [0.4, 75, 0, 0.826481538938877],
+        [0.6, 75, 0, 0.842744854690247],
+        [0.8, 75, 0, 0.847735029475644],
+        [1.0, 75, 0, 0.849053676698728],
+        [1.1, 75, 0, 0.849039573519871]]),
+        columns=[
+            'poa_global_kwm2', 'temp_module', 'wind_speed', 'pr_dc'])
+
+
+@pytest.fixture
+def mlfm_6_fit():
+    # fit matrix
+    '''
+    Excel fit GRG linear values
+    c_1 = +1.0573318761708000
+    c_2 = -0.0030251199627269
+    c_3 = +0.1228522267570000
+    c_4 = -0.0545505400372862
+    c_5 = 0
+    c_6 = -0.002394779219883
+    rmse = 0.280%
+    '''
+    c_1 = +1.0579328401731174
+    c_2 = -0.0030248261647759975
+    c_3 = +0.12378885001559799
+    c_4 = -0.05521716508715758
+    c_5 = +0.000
+    c_6 = -0.0023546463713093836
+    expected = 0.9845007615699125
+
+    cc_target = [c_1, c_2, c_3, c_4, c_5, c_6]
+    return c_1, c_2, c_3, c_4, c_5, c_6, expected, cc_target
+
+
 def test_mlfm_meas_to_norm(mlfm_6_coeffs, reference, measured, normalized):
     norm_calc = mlfm_meas_to_norm(measured, reference, 6)
     assert_allclose(norm_calc, normalized, atol=1e-6)
@@ -157,3 +197,12 @@ def test_mlfm_6(measured, mlfm_6_coeffs):
 def test_mlfm_norm_to_stack(normalized, reference, stacked):
     stack_calc = mlfm_norm_to_stack(normalized, reference, 6)
     assert_allclose(stack_calc, stacked, atol=1e-6)
+
+
+def test_mlfm_fit(matrix_data, mlfm_6_fit):
+    c_1, c_2, c_3, c_4, c_5, c_6, expected, cc_target = mlfm_6_fit
+    # choose which parameter to fit - usually pr_dc
+    mlfm_sel = 'pr_dc'
+    norm, cc_fit, ee,  coeffs, errs = mlfm_fit(
+        matrix_data, matrix_data, mlfm_sel)
+    assert_allclose(cc_fit, cc_target, atol=1e-3)

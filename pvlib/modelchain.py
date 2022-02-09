af31f395b3bc8f086e0e3b418c3bd3bb3f83df68
Kevin Anderson <kevin.anderson@nrel.gov>
2022-02-09 16:58:49 -0500

    deprecate basic_chain

--- pvlib/modelchain.py
+++ pvlib/modelchain.py
@@ -21,6 +21,8 @@ from pvlib.pvsystem import _DC_MODEL_PARAMS
 from pvlib._deprecation import pvlibDeprecationWarning
 from pvlib.tools import _build_kwargs
 
+from pvlib._deprecation import deprecated
+
 # keys that are used to detect input data and assign data to appropriate
 # ModelChain attribute
 # for ModelChain.weather
@@ -62,6 +64,10 @@ SAPM_CONFIG = dict(
 )
 
 
+@deprecated(since='0.9.1', name='pvlib.modelchain.basic_chain',
+    alternative='pvlib.modelchain.ModelChain.with_pvwatts',
+    addendum='Note that with_pvwatts takes different model parameters.'
+)
 def basic_chain(times, latitude, longitude,
                 surface_tilt, surface_azimuth,
                 module_parameters, temperature_model_parameters,

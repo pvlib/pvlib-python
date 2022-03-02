6f75c146c3c30982497bfdf787213aa87a5a51fb
Somasree Majumder <56045049+soma2000-lang@users.noreply.github.com>
2022-03-03 00:50:26 +0530

    Update sodapro.py

--- pvlib/iotools/sodapro.py
+++ pvlib/iotools/sodapro.py
@@ -361,7 +361,7 @@ def read_cams(filename, integrated=False, label=None, map_variables=True):
 
     See Also
     --------
-    pvlib.iotools.parse_cams,pvlib.iotools.get_cams
+    pvlib.iotools.parse_cams, pvlib.iotools.get_cams
 
     References
     ----------

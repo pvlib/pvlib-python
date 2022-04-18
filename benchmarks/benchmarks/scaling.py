3fe33a8da5aaf479db0bd9d0690c3bd191310388
pvlib-benchmarker <77211855+Naman-Priyadarshi@users.noreply.github.com>
2022-04-18 15:31:19 +0530

    Solved more Stickler CI issues

--- benchmarks/benchmarks/scaling.py
+++ benchmarks/benchmarks/scaling.py
@@ -13,13 +13,14 @@ class Scaling:
         self.n = 1000
         lat = np.array((9.99, 10, 10.01))
         lon = np.array((4.99, 5, 5.01))
-        self.coordinates = np.array([(lati, loni) for 
+        self.coordinates = np.array([(lati, loni) for
                                      (lati, loni) in zip(lat, lon)])
         self.times = pd.date_range('2019-01-01', freq='1T', periods=self.n)
         self.positions = np.array([[0, 0], [100, 0], [100, 100], [0, 100]])
-        self.clearsky_index = pd.Series(np.random.rand(self.n), index=self.times)
+        self.clearsky_index = pd.Series(np.random.rand(self.n),
+                                        index=self.times)
         self.cloud_speed = 5
-        self.tmscales = np.array((1, 2, 4, 8, 16, 32, 64, 
+        self.tmscales = np.array((1, 2, 4, 8, 16, 32, 64,
                                  128, 256, 512, 1024, 2048, 4096))
 
     def time_latlon_to_xy(self):

973e67e3e78b61c53685ae9eec17e0c73d5a0cc8
Prajwal Borkar <48290911+PrajwalBorkar@users.noreply.github.com>
2022-05-17 11:47:44 +0530

    Updated documentation links to https

--- pvlib/iotools/sodapro.py
+++ pvlib/iotools/sodapro.py
@@ -153,11 +153,11 @@ def get_cams(latitude, longitude, start, end, email, identifier='mcclear',
     References
     ----------
     .. [1] `CAMS Radiation Service Info
-       <http://www.soda-pro.com/web-services/radiation/cams-radiation-service/info>`_
+       <https://www.soda-pro.com/web-services/radiation/cams-radiation-service/info>`_
     .. [2] `CAMS McClear Service Info
-       <http://www.soda-pro.com/web-services/radiation/cams-mcclear/info>`_
+       <https://www.soda-pro.com/web-services/radiation/cams-mcclear/info>`_
     .. [3] `CAMS McClear Automatic Access
-       <http://www.soda-pro.com/help/cams-services/cams-mcclear-service/automatic-access>`_
+       <https://www.soda-pro.com/help/cams-services/cams-mcclear-service/automatic-access>`_
     """
     try:
         time_step_str = TIME_STEPS_MAP[time_step]
@@ -263,9 +263,9 @@ def parse_cams(fbuf, integrated=False, label=None, map_variables=True):
     References
     ----------
     .. [1] `CAMS Radiation Service Info
-       <http://www.soda-pro.com/web-services/radiation/cams-radiation-service/info>`_
+       <https://www.soda-pro.com/web-services/radiation/cams-radiation-service/info>`_
     .. [2] `CAMS McClear Service Info
-       <http://www.soda-pro.com/web-services/radiation/cams-mcclear/info>`_
+       <https://www.soda-pro.com/web-services/radiation/cams-mcclear/info>`_
     """
     metadata = {}
     # Initial lines starting with # contain metadata
@@ -366,9 +366,9 @@ def read_cams(filename, integrated=False, label=None, map_variables=True):
     References
     ----------
     .. [1] `CAMS Radiation Service Info
-       <http://www.soda-pro.com/web-services/radiation/cams-radiation-service/info>`_
+       <https://www.soda-pro.com/web-services/radiation/cams-radiation-service/info>`_
     .. [2] `CAMS McClear Service Info
-       <http://www.soda-pro.com/web-services/radiation/cams-mcclear/info>`_
+       <https://www.soda-pro.com/web-services/radiation/cams-mcclear/info>`_
     """
     with open(str(filename), 'r') as fbuf:
         content = parse_cams(fbuf, integrated, label, map_variables)

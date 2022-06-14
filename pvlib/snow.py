fixing capacity loss calculation units to keep correct C1 value

pvlib/snow.py
@@ -302,14 +302,16 @@ def loss_townsend(snow_total, snow_events, surface_tilt, relative_humidity,
 
     ground_interference_term = 1 - C2 * np.exp(-gamma)
     temp_air_kelvin = temp_air + 273.15
+    effective_snow_weighted_in = effective_snow_weighted / 2.54
+    poa_global_kWh = poa_global / 1000
     loss_percentage = (
         C1
-        * effective_snow_weighted
+        * effective_snow_weighted_in
         * cosd(surface_tilt)**2
         * ground_interference_term
         * relative_humidity
         / temp_air_kelvin**2
-        / poa_global**0.67
+        / poa_global_kWh**0.67
     )
 
     return loss_percentage / 100

pvlib/tests/test_snow.py
@@ -120,8 +120,8 @@ def test_loss_townsend():
     angle_of_repose = 40
     slant_height = 2.54
     lower_edge_drop_height = 0.254
-    expected = np.array([0.00191035, 0.00198382, 0.00154297, 0.00042579, 0, 0,
-                         0, 0, 0, 0, 0.00065624, 0.00150623])
+    expected = np.array([0.07696253, 0.07992262, 0.06216201, 0.01715392, 0, 0,
+                         0, 0, 0, 0, 0.02643821, 0.06068194])
     actual = snow.loss_townsend(snow_total, snow_events, surface_tilt,
                                 relative_humidity, temp_air,
                                 poa_global, slant_height,
29329ece21a84bd2df25ad17b266e73f361a3db8
Kurt Rhee <33131958+kurt-rhee@users.noreply.github.com>
2022-07-07 07:42:04 -0700

    Update pvlib/tracking.py
    Co-authored-by: Cliff Hansen <cwhanse@sandia.gov>

--- pvlib/tracking.py
+++ pvlib/tracking.py
@@ -295,8 +295,8 @@ def singleaxis(apparent_zenith, apparent_azimuth,
 
     axis_tilt : float, default 0
         The tilt of the axis of rotation (i.e, the y-axis defined by
-        axis_azimuth) with respect to horizontal, in decimal degrees.
-        axis_tilt must be >= 0 and <= 90.
+        ``axis_azimuth``) with respect to horizontal.
+        ``axis_tilt`` must be >= 0 and <= 90. [degree]
 
     axis_azimuth : float, default 0
         A value denoting the compass direction along which the axis of

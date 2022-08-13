949084ac25508473f5bdd15267df4bc89e3194a3
Anton Driesse <anton.driesse@pvperformancelabs.com>
2022-08-13 16:17:47 +0200

    Add utility function to produce a sample sr to work or play with.

--- pvlib/spectrum/mismatch.py
+++ pvlib/spectrum/mismatch.py
@@ -5,3 +5,46 @@ The ``mismatch`` module provides functions for spectral mismatch calculations.
 import pvlib
 import numpy as np
 import pandas as pd
+from scipy.interpolate import interp1d
+
+def get_sample_sr(wavelength=None):
+    '''
+    Generate a generic smooth c-si spectral response for tests and experiments.
+
+    Notes
+    -----
+    This sr is based on measurements taken on a reference cell.
+    The measured data points have been adjusted by PV Performance Labs so that
+    standard cubic spline interpolation produces a curve without oscillations.
+    '''
+    SR_DATA = np.array([[ 290, 0.00],
+                        [ 350, 0.27],
+                        [ 400, 0.37],
+                        [ 500, 0.52],
+                        [ 650, 0.71],
+                        [ 800, 0.88],
+                        [ 900, 0.97],
+                        [ 957, 1.00],
+                        [1000, 0.93],
+                        [1050, 0.58],
+                        [1100, 0.21],
+                        [1150, 0.05],
+                        [1190, 0.00]]).transpose()
+
+    if wavelength is None:
+        resolution = 5.0
+        wavelength = np.arange(280, 1200 + resolution, resolution)
+
+    interpolator = interp1d(SR_DATA[0], SR_DATA[1],
+                            kind='cubic',
+                            bounds_error=False,
+                            fill_value=0.0,
+                            copy=False,
+                            assume_sorted=True)
+
+    sr = pd.Series(data=interpolator(wavelength), index=wavelength)
+
+    sr.index.name = 'wavelength'
+    sr.name = 'sr'
+
+    return sr

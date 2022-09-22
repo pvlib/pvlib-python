2de5a3716272da5b4e8c7ae20dbb762767f2b093
Silvana Ovaitt <silvana.ayala@nrel.gov>
2022-09-22 12:33:23 -0600

    stickler

--- pvlib/tests/iotools/test_sam.py
+++ pvlib/tests/iotools/test_sam.py
@@ -17,12 +17,16 @@ testfile_radiation_csv = DATA_DIR / \
 testfile_radiation_csv = r'C:\Users\sayala\Documents\GitHub\pvlib-python\pvlib\data\pvgis_hourly_Timeseries_45.000_8.000_SA_30deg_0deg_2016_2016.csv'
 
 def test_saveSAM_WeatherFile():
-    data, inputs, metadata = read_pvgis_hourly(testfile_radiation_csv, map_variables=True)#, pvgis_format=pvgis_format)
-    metadata = {'latitude': inputs['latitude'],
-           'longitude': inputs['longitude'],
-           'elevation': inputs['elevation'],
+    data, months_selected, inputs, metadata = get_pvgis_tmy(latitude=33, longitude=-110, map_variables=True)
+    #  Reading local file doesn't work read_pvgis_hourly returns different 
+    # keys than get_pvgis_tmy when map=variables = True.  Opened issue for that
+#    data, inputs, metadata = read_pvgis_hourly(testfile_radiation_csv, 
+#                                               map_variables=True)
+    metadata = {'latitude': inputs['location']['latitude'],
+           'longitude': inputs['location']['longitude'],
+           'elevation': inputs['location']['elevation'],
            'source': 'User-generated'}
-    metadata['TZ'] = -7        
+    metadata['TZ'] = -7
     data = tz_convert(data, tz_convert_val=metadata['TZ'])
     coerce_year=2021  # read_pvgis_hourly does not coerce_year, so doing it here.
     data.index = data.index.map(lambda dt: dt.replace(year=coerce_year))

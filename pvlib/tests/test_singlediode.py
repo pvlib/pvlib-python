correcting Vth calculation

pvlib/tests/test_singlediode.py
@@ -70,39 +70,64 @@ def test_newton_fs_495(method, cec_module_fs_495):
 
 
 @pytest.mark.parametrize('method', ['lambertw', 'brentq', 'newton'])
-def test_precise_iv_curves1(method, precise_iv_curves1):
-    """test pvsystem.singlediode with different methods"""
-    pc = precise_iv_curves1
-    print(pc.columns)
+def test_precise_iv_curves(method, precise_iv_curves):
+    """
+    test pvsystem.singlediode with different methods
+    """
+    pc = precise_iv_curves
     il, io, rs, rsh, nnsvt = (
         pc['photocurrent'], pc['saturation_current'],
         pc['resistance_series'], pc['resistance_shunt'],
-        pc['n'] * pc['cells_in_series'] * pc['Temperature']
+        pc['n'] * pc['cells_in_series'] * pc['Vth']
     )
-    import pdb
-    pdb.set_trace()
-    ivcurve_pnts = len(pc['Currents'][0])
-    outs = pvsystem.singlediode(il, io, rs, rsh, nnsvt, method=method,
-                                ivcurve_pnts=ivcurve_pnts)
-    pc_i , pc_v = np.stack(pc['Currents']), np.stack(pc['Voltages'])
-    assert np.allclose(pc_i, outs['i'])
-    assert np.allclose(pc_v, outs['v'])
+    outs = pvsystem.singlediode(il, io, rs, rsh, nnsvt, method=method)
+
     assert np.allclose(pc['i_sc'], outs['i_sc'])
     assert np.allclose(pc['v_oc'], outs['v_oc'])
-    # the singlediode method doesn't actually get the MPP correct
-    pvs_imp = pvsystem.i_from_v(rsh, rs, nnsvt, vmp, io, il, method='lambertw')
-    pvs_vmp = pvsystem.v_from_i(rsh, rs, nnsvt, imp, io, il, method='lambertw')
-    assert np.allclose(pc['i_mp'], pvs_imp)
-    assert np.allclose(pc['v_mp'], pvs_vmp)
+    assert np.allclose(pc['i_mp'], outs['i_mp'])
+    assert np.allclose(pc['v_mp'], outs['v_mp'])
     assert np.allclose(pc['p_mp'], outs['p_mp'])
-    # assert np.isclose(pvs['i_x'], ix) # ???
+    # assert np.isclose(pvs['i_x'], ix) # not calculated in ivcurves
     # pvs_ixx = pvsystem.i_from_v(rsh, rs, nnsvt, (voc + vmp)/2, io, il,
-    #                             method='lambertw')
-    # assert np.isclose(pvs_ixx, ixx)
-    # ['Index', 'photocurrent', 'saturation_current', 'resistance_series',
-    #    'resistance_shunt', 'n', 'cells_in_series', 'Voltages', 'Currents',
-    #    'v_oc', 'i_sc', 'v_mp', 'i_mp', 'p_mp', 'Temperature', 'Irradiance',
-    #    'Sweep direction', 'Datetime']
+    #                             method='lambertw') # not calculated in ivcurves
+    # assert np.isclose(pvs_ixx, ixx) # not calculated in ivcurves
+
+
+@pytest.mark.parametrize('method', ['lambertw', 'brentq', 'newton'])
+def test_precise_iv_curves_ivcurve_pnts(method, precise_iv_curves):
+    """
+    test pvsystem.singlediode with different methods
+    tests only the points on the iv curve calcuated by passing ivcurve_pnts
+    interpolate between precise voltage/current values
+    """
+    pc = precise_iv_curves
+    x = (
+        pc['photocurrent'], pc['saturation_current'],
+        pc['resistance_series'], pc['resistance_shunt'],
+        pc['n'] * pc['cells_in_series'] * pc['Vth']
+    )
+    pc_i , pc_v = np.stack(pc['Currents']), np.stack(pc['Voltages'])
+    ivcurve_pnts = len(pc['Currents'][0])
+
+    if method == 'lambertw':
+        outs = pvsystem.singlediode(*x, method=method,
+                                    ivcurve_pnts=ivcurve_pnts)
+        assert np.allclose(pc_i, outs['i'])
+        assert np.allclose(pc_v, outs['v'])
+    else:
+        for idx, x_one_curve in enumerate(zip(*x)):
+            out = pvsystem.singlediode(*x_one_curve, method=method,
+                                        ivcurve_pnts=ivcurve_pnts) # create issue for this not vectorized
+            import pdb
+            pdb.set_trace()
+            assert np.allclose(
+                       np.interp(out['v'], pc_v[idx], pc_i[idx]), out['i'],
+                       rtol=1e-05, atol=1e-05
+                   )
+            assert np.allclose(
+                       np.interp(out['i'], pc_i[idx], pc_v[idx]), out['v'],
+                       rtol=1e-05, atol=1e-05
+                   )
 
 
 def get_pvsyst_fs_495():
e50def0f5dca8346d43fb21c5dd08ecde1689b98
Kirsten Perry <70228568+kperrynrel@users.noreply.github.com>
2022-11-01 06:16:32 -0700

    Implement irradiance.complete_irradiance with component sum equations (#1567)
    * added new irradiance function for component sum
    
    * added to docstrings
    
    * updated docstrings
    
    * updated methods with new component_sum_irradiance() function
    
    * added associated unit test for the new component sum function
    
    * added units tests for the component_sum_irradiance() function
    
    * added whatsnew file for v0.9.4
    
    * fixed modelchain error to stop unit test erroring
    
    * removed clearsky_dni arg for cases where it's not used
    
    * updated naming conventions for component_sum_irradiance function in modelchains
    
    * updated the routines to remove pep8 errors, added to clearsky_dni docstring
    
    * updated the routine to return dataframe after calculating component sum + other suggestions
    
    * updated the PR based on @kanderso-nrel review
    
    * made updates based on kanderso-nrel's suggestions
    
    * updated all the unit tests to pass
    
    * Update pvlib/irradiance.py
    
    Co-authored-by: Kevin Anderson <kevin.anderson@nrel.gov>
    
    * Update pvlib/irradiance.py
    
    Co-authored-by: Kevin Anderson <kevin.anderson@nrel.gov>
    
    * moved warning out of dni generation call (just ghi and dhi)
    
    * fix sticklerci formatting errors
    
    * Update pvlib/irradiance.py
    
    Co-authored-by: Kevin Anderson <kevin.anderson@nrel.gov>
    
    * updates to name-changed to 'complete_irradiance'
    
    * fixed over-indentation error
    
    * removed default none from function params
    
    * Update docs/sphinx/source/whatsnew/v0.9.4.rst
    
    Co-authored-by: Cliff Hansen <cwhanse@sandia.gov>
    
    * made updates based on @cwhanse's recommendations
    
    * Update pvlib/irradiance.py
    
    Co-authored-by: Will Holmgren <william.holmgren@gmail.com>
    
    * Update pvlib/irradiance.py
    
    Co-authored-by: Will Holmgren <william.holmgren@gmail.com>
    
    * changed to pytest error raise based on @wholmgren's recommendation
    
    * Made updates to code based on @wholmgren's recommendations
    
    * fix pep8 error 2
    
    * updated modelchains to use zenith instead of apparent zenith
    
    * check that unit tests run
    
    * undo ModelChain.complete_irradiance test changes
    
    Co-authored-by: Perry <kperry@nrel.gov>
    Co-authored-by: Kevin Anderson <kevin.anderson@nrel.gov>
    Co-authored-by: Cliff Hansen <cwhanse@sandia.gov>
    Co-authored-by: Will Holmgren <william.holmgren@gmail.com>

--- pvlib/irradiance.py
+++ pvlib/irradiance.py
@@ -12,6 +12,7 @@ import numpy as np
 import pandas as pd
 
 from pvlib import atmosphere, solarposition, tools
+import pvlib  # used to avoid dni name collision in complete_irradiance
 
 
 # see References section of get_ground_diffuse function
@@ -2948,3 +2949,67 @@ def dni(ghi, dhi, zenith, clearsky_dni=None, clearsky_tolerance=1.1,
             (zenith < zenith_threshold_for_zero_dni) &
             (dni > max_dni)] = max_dni
     return dni
+
+
+def complete_irradiance(solar_zenith,
+                        ghi=None,
+                        dhi=None,
+                        dni=None,
+                        dni_clear=None):
+    r"""
+    Use the component sum equations to calculate the missing series, using
+    the other available time series. One of the three parameters (ghi, dhi,
+    dni) is passed as None, and the other associated series passed are used to
+    calculate the missing series value.
+
+    The "component sum" or "closure" equation relates the three
+    primary irradiance components as follows:
+
+    .. math::
+
+       GHI = DHI + DNI \cos(\theta_z)
+
+    Parameters
+    ----------
+    solar_zenith : Series
+        Zenith angles in decimal degrees, with datetime index.
+        Angles must be >=0 and <=180. Must have the same datetime index
+        as ghi, dhi, and dni series, when available.
+    ghi : Series, optional
+        Pandas series of dni data, with datetime index. Must have the same
+        datetime index as dni, dhi, and zenith series, when available.
+    dhi : Series, optional
+        Pandas series of dni data, with datetime index. Must have the same
+        datetime index as ghi, dni, and zenith series, when available.
+    dni : Series, optional
+        Pandas series of dni data, with datetime index. Must have the same
+        datetime index as ghi, dhi, and zenith series, when available.
+    dni_clear : Series, optional
+        Pandas series of clearsky dni data. Must have the same datetime index
+        as ghi, dhi, dni, and zenith series, when available. See
+        :py:func:`dni` for details.
+
+    Returns
+    -------
+    component_sum_df : Dataframe
+        Pandas series of 'ghi', 'dhi', and 'dni' columns with datetime index
+    """
+    if ghi is not None and dhi is not None and dni is None:
+        dni = pvlib.irradiance.dni(ghi, dhi, solar_zenith,
+                                   clearsky_dni=dni_clear,
+                                   clearsky_tolerance=1.1)
+    elif dni is not None and dhi is not None and ghi is None:
+        ghi = (dhi + dni * tools.cosd(solar_zenith))
+    elif dni is not None and ghi is not None and dhi is None:
+        dhi = (ghi - dni * tools.cosd(solar_zenith))
+    else:
+        raise ValueError(
+            "Please check that exactly one of ghi, dhi and dni parameters "
+            "is set to None"
+        )
+    # Merge the outputs into a master dataframe containing 'ghi', 'dhi',
+    # and 'dni' columns
+    component_sum_df = pd.DataFrame({'ghi': ghi,
+                                     'dhi': dhi,
+                                     'dni': dni})
+    return component_sum_df

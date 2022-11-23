dd0ac02dd7574fe04e61cd42d92be6dcd46d7e7b
Anton Driesse <anton.driesse@pvperformancelabs.com>
2022-11-23 16:57:18 +0100

    Change rtol in tests.

--- pvlib/tests/test_pvefficiency.py
+++ pvlib/tests/test_pvefficiency.py
@@ -11,7 +11,7 @@ def test_adr():
     e = [1.0, 0.949147, 0.928114, 0.876456, 0.855693, 0.80323]
 
     result = pvefficiency.adr(g, t, *p)
-    assert_allclose(result, e, rtol=1e-5)
+    assert_allclose(result, e, rtol=1e-4)
 
 
 def test_fit_pvefficiency_adr():
@@ -22,7 +22,7 @@ def test_fit_pvefficiency_adr():
     p = [1.0, -6.602189, 0.018582, 0.071889, 0.054049]
 
     result = pvefficiency.fit_pvefficiency_adr(g, t, e, dict_output=False)
-    assert_allclose(result, p, rtol=1e-5)
+    assert_allclose(result, p, rtol=1e-4)
 
     result = pvefficiency.fit_pvefficiency_adr(g, t, e, dict_output=True)
     assert 'k_a' in result
@@ -35,4 +35,4 @@ def test_adr_round_trip():
 
     p = pvefficiency.fit_pvefficiency_adr(g, t, e, dict_output=False)
     result = pvefficiency.adr(g, t, *p)
-    assert_allclose(result, e, rtol=1e-5)
+    assert_allclose(result, e, rtol=1e-4)

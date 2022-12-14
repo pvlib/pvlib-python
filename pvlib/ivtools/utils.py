3f306cc17d48f71c71441c536584d89c3c51700f
Michael Deceglie <Michael.Deceglie@nrel.gov>
2022-12-14 10:49:10 -0700

    move copyright statement to a comment

--- pvlib/ivtools/utils.py
+++ pvlib/ivtools/utils.py
@@ -468,12 +468,11 @@ def astm_e1036(v, i, imax_limits=(0.75, 1.15), vmax_limits=(0.75, 1.15),
     .. [1] Standard Test Methods for Electrical Performance of Nonconcentrator
        Terrestrial Photovoltaic Modules and Arrays Using Reference Cells,
        ASTM E1036-15(2019), :doi:`10.1520/E1036-15R19`
-
-
-    Adapted from https://github.com/NREL/iv_params
-    Copyright (c) 2022, Alliance for Sustainable Energy, LLC
-    All rights reserved.
     '''
+
+    # Adapted from https://github.com/NREL/iv_params
+    # Copyright (c) 2022, Alliance for Sustainable Energy, LLC
+    # All rights reserved.
 
     df = pd.DataFrame()
     df['v'] = v

5a0c80ba385f614316c6ea9da26de7ca7d0812d9
Cliff Hansen <cwhanse@sandia.gov>
2023-02-14 10:48:39 -0700

    revert to numpy 1.16

--- benchmarks/benchmarks/singlediode.py
+++ benchmarks/benchmarks/singlediode.py
@@ -1,12 +1,16 @@
 """
 ASV benchmarks for singlediode.py
 """
-from numpy.random import Generator, MT19937
+# slower MT implementation in numpy<=1.16
+from numpy.random import RandomState
+# replace with below when ASV migrates to numpy>=1.17
+# and replace 'rng.rang()' with 'rng()'
+# from numpy.random import Generator, MT19937
 from pvlib import singlediode as _singlediode
 
 seed = 11471
 
-rng = Generator(MT19937(seed))
+rng = RandomState(seed)
 base_params = (1., 5.e-9, 0.5, 2000., 72 * 1.1 * 0.025)
 nsamples = 10000
 
@@ -24,11 +28,11 @@ def b88(params):
 class SingleDiode:
 
     def setup(self, base_params, nsamples):
-        self.il = 9 * rng(nsamples) + 1.  # 1.- 10. A
-        self.io = 10**(-9 + 3. * rng(nsamples))  # 1e-9 to 1e-6 A
-        self.rs = 5 * rng(nsamples) + 0.05  # 0.05 to 5.05 Ohm
-        self.rsh = 10**(2 + 2 * rng(nsamples))  # 100 to 10000 Ohm
-        self.n = 1 + 0.7 * rng(nsamples)  # 1.0 to 1.7
+        self.il = 9. * rng.rand(nsamples) + 1.  # 1.- 10. A
+        self.io = 10**(-9 + 3. * rng.rand(nsamples))  # 1e-9 to 1e-6 A
+        self.rs = 5. * rng.rand(nsamples) + 0.05  # 0.05 to 5.05 Ohm
+        self.rsh = 10**(2 + 2 * rng.rand(nsamples))  # 100 to 10000 Ohm
+        self.n = 1 + 0.7 * rng.rand(nsamples)  # 1.0 to 1.7
         # 72 cells in series, roughly 25C Tcell
         self.nNsVth = 72 * self.n * 0.025
         self.params = (self.il, self.io, self.rs, self.rsh, self.nNsVth)

108def0ca1a2f7988c29771ab958f025e12526f2
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2023-02-24 23:30:25 +0100

    example: add comparison with other models
    0. We've found that MR model does not match other spectral mismatch models as expected - this commit shows that, in case some insight can be given
    1. Uses atmosphere.first_solar_spectral_correction--> to be changed before merging
    2. Compares with:
     * pvsystem.sapm_spectral_loss
     * atmosphere.first_solar_spectral_correction
    3. Docs are already up-to-date with the first_solar renaming

--- docs/examples/spectrum/plot_martin_ruiz_mismatch.py
+++ docs/examples/spectrum/plot_martin_ruiz_mismatch.py
@@ -32,7 +32,9 @@ How to use this correction factor to adjust the POA global irradiance.
 # compute sky diffuse, ground reflected and direct irradiance.
 
 import matplotlib.pyplot as plt
-from pvlib import spectrum, irradiance, iotools, location
+from pvlib import spectrum, irradiance, iotools, location, pvsystem, atmosphere
+import pandas as pd
+from scipy import stats
 
 surface_tilt = 40
 surface_azimuth = 180  # Pointing South
@@ -109,9 +111,96 @@ poa_irrad_modified['poa_global'] = (poa_irrad_modified['poa_direct']
 
 poa_irrad_global_diff = (poa_irrad['poa_global']
                          - poa_irrad_modified['poa_global'])
-poa_irrad['poa_global'].plot()
-poa_irrad_modified['poa_global'].plot()
-poa_irrad_global_diff.plot()
+plt.figure()
+datetimes = poa_irrad.index  # common to poa_irrad_*
+plt.plot(datetimes, poa_irrad['poa_global'].to_numpy())
+plt.plot(datetimes, poa_irrad_modified['poa_global'].to_numpy())
+plt.plot(datetimes, poa_irrad_global_diff.to_numpy())
 plt.legend(['Incident', 'Modified', 'Difference'])
 plt.ylabel('POA Global irradiance [W/m²]')
+plt.grid()
+plt.show()
+
+# %%
+# Comparison with other models
+# ----------------------------
+# During the addition of this model, a question arose about its trustworthiness
+# so, in order to check the integrity of the implementation, we will
+# compare it against :py:func:`pvlib.pvsystem.sapm_spectral_loss` and
+# :py:func:`pvlib.atmosphere.first_solar`.
+# Former model needs the parameters that characterise a module, but which one?
+# We will take the mean of Sandia parameters `'A0', 'A1', 'A2', 'A3', 'A4'` for
+# the same material type.
+# On the other hand, :py:func:`~pvlib.atmosphere.first_solar` needs the
+# precipitable water. We assume the standard spectrum, `1.42 cm`.
+
+# Retrieve modules and select the subset we want to work with the SAPM model
+module_type = 'mc-Si'  # Equivalent to monosi
+sandia_modules = pvsystem.retrieve_sam(name='SandiaMod')
+modules_subset = \
+    sandia_modules.loc[:, sandia_modules.loc['Material'] == module_type]
+
+# Define typical module and get the means of the A0 to A4 parameters
+modules_aggregated = pd.DataFrame(index=('mean', 'std'))
+for param in ('A0', 'A1', 'A2', 'A3', 'A4'):
+    result, _, _ = stats.mvsdist(modules_subset.loc[param])
+    modules_aggregated[param] = result.mean(), result.std()
+
+# Check if 'mean' is a representative value with help of 'std' just in case
+print(modules_aggregated)
+
+# Then apply the SAPM model and calculate introduced difference
+modifier_sapm_f1 = pvsystem.sapm_spectral_loss(airmass['airmass_absolute'],
+                                               modules_aggregated.loc['mean'])
+poa_irrad_sapm_modified = poa_irrad['poa_global'] * modifier_sapm_f1
+poa_irrad_sapm_difference = (poa_irrad['poa_global']
+                             - poa_irrad_sapm_modified)
+
+# atmosphere.first_solar model
+first_solar_pw = 1.42  # Default for AM1.5 spectrum
+modifier_first_solar = \
+    atmosphere.first_solar_spectral_correction(first_solar_pw,
+                                               airmass['airmass_absolute'],
+                                               module_type='monosi')
+poa_irrad_first_solar_mod = poa_irrad['poa_global'] * modifier_first_solar
+poa_irrad_first_solar_diff = (poa_irrad['poa_global']
+                              - poa_irrad_first_solar_mod)
+
+# %%
+# Plot global irradiance difference over time
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+datetimes = poa_irrad_global_diff.index  # common to poa_irrad_*_diff*
+plt.figure()
+plt.plot(datetimes, poa_irrad_global_diff.to_numpy(),
+         label='spectrum.martin_ruiz')
+plt.plot(datetimes, poa_irrad_sapm_difference.to_numpy(),
+         label='atmosphere.first_solar')
+plt.plot(datetimes, poa_irrad_first_solar_diff.to_numpy(),
+         label='pvsystem.sapm_spectral_loss')
+plt.legend()
+plt.title('Introduced difference comparison of different models')
+plt.ylabel('POA Global Irradiance Difference [W/m²]')
+plt.grid()
+plt.show()
+
+# %%
+# Plot modifier vs absolute airmass
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ama = airmass['airmass_absolute'].to_numpy()
+# spectrum.martin_ruiz has 3 modifiers, so we only calculate one as
+# M = S_eff / S_incident that takes into account the global effect
+martin_ruiz_agg_modifier = (poa_irrad_modified['poa_global']
+                            / poa_irrad['poa_global'])
+plt.figure()
+plt.scatter(ama, martin_ruiz_agg_modifier.to_numpy(),
+            label='spectrum.martin_ruiz')
+plt.scatter(ama, modifier_sapm_f1.to_numpy(),
+            label='pvsystem.sapm_spectral_loss')
+plt.scatter(ama, modifier_first_solar.to_numpy(),
+            label='atmosphere.first_solar')
+plt.legend()
+plt.title('Introduced difference comparison of different models')
+plt.xlabel('Absolute airmass')
+plt.ylabel(r'Modifier $M = \frac{S_{effective}}{S_{incident}}$')
+plt.grid()
 plt.show()

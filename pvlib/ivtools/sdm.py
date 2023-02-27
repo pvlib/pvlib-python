f041355e825717b4bf2c22eb180c7a342cc59835
Kevin Anderson <kevin.anderson@nrel.gov>
2023-02-27 16:14:07 -0500

    Use explicit finite difference instead of scipy.misc.derivative in pvsyst_temperature_coeff (#1674)
    * use explicit finite difference instead of scipy.misc.derivative
    
    * whatsnew
    
    * private function in tools.py
    
    Co-Authored-By: Mark Mikofski <bwana.marko@yahoo.com>
    
    ---------
    
    Co-authored-by: Mark Mikofski <bwana.marko@yahoo.com>

--- pvlib/ivtools/sdm.py
+++ pvlib/ivtools/sdm.py
@@ -11,13 +11,14 @@ import numpy as np
 from scipy import constants
 from scipy import optimize
 from scipy.special import lambertw
-from scipy.misc import derivative
 
 from pvlib.pvsystem import calcparams_pvsyst, singlediode, v_from_i
 from pvlib.singlediode import bishop88_mpp
 
 from pvlib.ivtools.utils import rectify_iv_curve, _numdiff
 from pvlib.ivtools.sde import _fit_sandia_cocontent
+
+from pvlib.tools import _first_order_centered_difference
 
 
 CONSTANTS = {'E0': 1000.0, 'T0': 25.0, 'k': constants.k, 'q': constants.e}
@@ -1344,5 +1345,6 @@ def pvsyst_temperature_coeff(alpha_sc, gamma_ref, mu_gamma, I_L_ref, I_o_ref,
             I_o_ref, R_sh_ref, R_sh_0, R_s, cells_in_series, R_sh_exp, EgRef,
             temp_ref)
     pmp = maxp(temp_ref, *args)
-    gamma_pdc = derivative(maxp, temp_ref, args=args)
+    gamma_pdc = _first_order_centered_difference(maxp, x0=temp_ref, args=args)
+
     return gamma_pdc / pmp

7132f5d85a51904e28836123a036bc0b49b1611c
Jacc0027 <88579674+Jacc0027@users.noreply.github.com>
2023-05-19 00:47:01 +0200

    Update test_atmosphere.py

--- pvlib/tests/test_atmosphere.py
+++ pvlib/tests/test_atmosphere.py
@@ -221,15 +221,17 @@ def test_caballero_spectral_correction_supplied():
 
 
 def test_caballero_spectral_correction_supplied_ambiguous():
-    dummy_coeffs = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1)
+    dummy_coeffs = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
     with pytest.raises(ValueError):
         atmosphere.caballero_spectral_correction(1, 1, 1,
                                                  module_type='cdte',
-                                                 coefficients=dummy_coeffs)
+                                                 coefficients=dummy_coeffs,
+                                                 aod500_ref=0.084, pw_ref=1.42)
 
 
 def test_caballero_spectral_correction_supplied_ambiguous_1():
     with pytest.raises(ValueError):
         atmosphere.caballero_spectral_correction(1, 1, 1,
                                                  module_type=None,
-                                                 coefficients=None)
+                                                 coefficients=None,
+                                                 aod500_ref=0.084, pw_ref=1.42)

167d49b364e963bb8ad2c43c340ea7c2d6d357f6
Kevin Anderson <kevin.anderso@gmail.com>
2023-06-09 20:51:30 -0400

    fix all these broken things

--- pvlib/tests/iotools/test_acis.py
+++ pvlib/tests/iotools/test_acis.py
@@ -10,10 +10,10 @@ from requests import HTTPError
 
 
 @pytest.mark.parametrize('dataset,expected,lat,lon', [
-    (1, [0.76, 1.78, 1.52, 0.76, 0.0], 40.0, -80.0)
-    (2, [0.05, 2.74, 1.43, 0.92, 0.0], 40.0083, -79.9653)
-    (3, [0.0, 2.79, 1.52, 1.02, 0.0], 40.0, -80.0)
-    (21, [0.6, 1.8, 1.9, 1.2, 0.0], 40.0, -80.9)
+    (1, [0.76, 1.78, 1.52, 0.76, 0.0], 40.0, -80.0),
+    (2, [0.05, 2.74, 1.43, 0.92, 0.0], 40.0083, -79.9653),
+    (3, [0.0, 2.79, 1.52, 1.02, 0.0], 40.0, -80.0),
+    (21, [0.6, 1.8, 1.9, 1.2, 0.0], 40.0, -80.0),
 ])
 @pytest.mark.remote_data
 @pytest.mark.flaky(reruns=RERUNS, reruns_delay=RERUNS_DELAY)
@@ -22,6 +22,8 @@ def test_get_acis_precipitation(dataset, expected, lat, lon):
     ed = '2012-01-05'
     precipitation, meta = get_acis_precipitation(40, -80, st, ed, dataset)
     idx = pd.date_range(st, ed, freq='d')
+    idx.name = 'date'
+    idx.freq = None
     expected = pd.Series(expected, index=idx, name='precipitation')
     assert_series_equal(precipitation, expected)
     assert meta == {'latitude': lat, 'longitude': lon}

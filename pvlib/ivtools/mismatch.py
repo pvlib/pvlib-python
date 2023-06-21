76efbacd20234b1ac7a8e918ca074b1c59518233
Abigail Jones <arjones@sandia.gov>
2023-06-21 14:13:36 -0400

    Move clipping to prepare_curves

--- pvlib/ivtools/mismatch.py
+++ pvlib/ivtools/mismatch.py
@@ -2,12 +2,13 @@ import pvlib
 import numpy as np
 
 
-def prepare_curves(params, num_pts):
+def prepare_curves(params, num_pts, breakdown_voltage=-0.5):
     # params is an n x 5 array where each row corresponds to one of
     # the n curves' five defining parameters (photocurrent, saturation
     # current, series resistance, shunt resistance, nNsVth)
     # num_pts is the number of points you want calculated for each curve
     # (this will also be the number of points in the aggregate curve)
+    # breakdown_voltage is the asymptote we use left of the y-axis
 
     # in case params is a list containing scalars, add a dimension
     if len(np.shape(params)) == 1:
@@ -30,24 +31,23 @@ def prepare_curves(params, num_pts):
     voltages = pvlib.singlediode.bishop88_v_from_i(bishop_inputs, *params.T,
                method='newton').T
 
-    return currents, voltages
+    # any voltages in array that are smaller than breakdown_voltage are
+    # clipped to be breakdown_voltage
+    clipped_voltages = np.clip(voltages, a_min=breakdown_voltage, a_max=None)
+
+    return currents, clipped_voltages
 
 
-def combine_curves(currents, voltages, breakdown_voltage=-0.5):
+def combine_curves(currents, voltages):
     # currents is a 1D array that contains a range from 0 to max_isc
     # voltages is a 2D array where each row corresponds to the
     # associated voltages of a single curve (should be n by
     # len(currents), where n is the number of curves we're summing over)
-    # breakdown_voltage is the asymptote we use left of the y-axis
 
     currents = np.asarray(currents)
     voltages = np.asarray(voltages)
     assert currents.ndim == 1
     assert voltages.ndim == 2
-
-    # any voltages in array that are smaller than breakdown_voltage are
-    # clipped to be breakdown_voltage
-    clipped_voltages = np.clip(voltages, a_min=breakdown_voltage, a_max=None)
 
     # for each current, add the associated voltages of all the curves
     # in our setup, this means summing each column of the voltage array

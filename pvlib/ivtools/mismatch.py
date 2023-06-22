3038f8697c332933dc974db727b91188f4613632
Abigail Jones <arjones@sandia.gov>
2023-06-22 13:25:15 -0400

    Added docstrings

--- pvlib/ivtools/mismatch.py
+++ pvlib/ivtools/mismatch.py
@@ -1,8 +1,71 @@
+"""
+The `mismatch` module contains functions for combining curves in
+series using the single diode model.
+"""
+
 import pvlib
 import numpy as np
 
 
 def prepare_curves(params, num_pts, breakdown_voltage=-0.5):
+    """
+    Calculates currents and voltages on IV curves with the given
+    parameters, using the single diode equation.
+
+    Parameters
+    ----------
+    params : array-like
+        An array of parameters representing a set of :math:`n` IV
+        curves. The array should contain :math:`n` rows and five
+        columns. Each row contains the five parameters needed for a
+        single curve. The parameters should be in the following order:
+
+            photocurrent : numeric
+                photo-generated current :math:`I_{L}` [A]
+
+            saturation_current : numeric
+                diode reverse saturation current :math:`I_{0}` [A]
+
+            resistance_series : numeric
+                series resistance :math:`R_{s}` [ohms]
+
+            resistance_shunt : numeric
+                shunt resistance :math:`R_{sh}` [ohms]
+
+            nNsVth : numeric
+                product of thermal voltage :math:`V_{th}` [V], diode
+                ideality factor :math:`n`, and number of series cells
+                :math:`N_{s}` [V]
+
+    num_pts : int
+        Number of points to compute for each IV curve.
+
+    breakdown_voltage : float
+        Vertical asymptote to use left of the y-axis. Any voltages that
+        are smaller than `breakdown_voltage` will be overwritten by it.
+
+    Returns
+    -------
+    tuple
+        currents : np.ndarray
+            A 1D array of current values. Has shape (`num_pts`,).
+
+        voltages : np.ndarray
+            A 2D array of voltage values, where each row corresponds to
+            a single IV curve. Has shape (:math:`n`, `num_pts`), where
+            :math:`n` is the number of IV curves passed in.
+
+    Notes
+    -----
+    This function assumes a simplified reverse bias model. When using
+    :func:`pvlib.pvsystem.bishop88_v_from_i`, `breakdown_factor` is left
+    at the default value, which excludes the reverse bias term from the
+    model. Instead, any returned voltages that are less than
+    `breakdown_voltage` are replaced by `breakdown_voltage`, yielding a
+    vertical asymptote at `breakdown_voltage`.
+
+
+    """
     # params is an n x 5 array where each row corresponds to one of
     # the n curves' five defining parameters (photocurrent, saturation
     # current, series resistance, shunt resistance, nNsVth)
@@ -33,12 +96,62 @@ def prepare_curves(params, num_pts, breakdown_voltage=-0.5):
 
     # any voltages in array that are smaller than breakdown_voltage are
     # clipped to be breakdown_voltage
-    clipped_voltages = np.clip(voltages, a_min=breakdown_voltage, a_max=None)
+    voltages = np.clip(voltages, a_min=breakdown_voltage, a_max=None)
 
-    return currents, clipped_voltages
+    return currents, voltages
 
 
 def combine_curves(currents, voltages):
+    """
+    Combines IV curves in series.
+
+    Parameters
+    ----------
+    currents : array-like
+        A 1D array-like object. Its first element must be zero, and it
+        should be increasing.
+
+    voltages : array-like
+        A 2D array-like object. Each row corresponds to a single IV
+        curve and contains the voltages for that curve that are
+        associated to elements of `currents`. Each row must be
+        decreasing.
+
+    Returns
+    -------
+    dict
+        Contains the following keys:
+            i_sc : scalar
+                short circuit current of combined curve [A]
+
+            v_oc : scalar
+                open circuit voltage of combined curve [V]
+
+            i_mp : scalar
+                current at maximum power point of combined curve [A]
+
+            v_mp : scalar
+                voltage at maximum power point of combined curve [V]
+
+            p_mp : scalar
+                power at maximum power point of combined curve [W]
+
+            i : np.ndarray
+                currents of combined curve [A]
+
+            v : np.ndarray
+                voltages of combined curve [V]
+
+    Notes
+    -----
+    If the combined curve does not cross the y-axis, then the last (and
+    hence largest) current is returned for short circuit current.
+
+    The maximum power point that is returned is the maximum power point
+    of the dataset. Its accuracy will improve as more points are passed
+    in.
+
+    """
     # currents is a 1D array that contains a range from 0 to max_isc
     # voltages is a 2D array where each row corresponds to the
     # associated voltages of a single curve (should be n by
@@ -66,13 +179,16 @@ def combine_curves(currents, voltages):
     # we're assuming voltages are decreasing, so combined_voltages
     # should also be decreasing
     if not np.all(np.diff(combined_voltages) < 0):
-        raise ValueError("Each row of voltages must be decreasing.")
+        raise ValueError("Each row of voltages array must be decreasing.")
 
     # get isc
     # np.interp requires second argument is increasing, so flip
     # combined_voltages and currents
     isc = np.interp(0., np.flip(combined_voltages), np.flip(currents))
 
+    # the first element of currents must be zero
+    if currents[0] != 0:
+        raise ValueError("First element of currents array must be zero.")
     # get voc
     voc = combined_voltages[0]
 

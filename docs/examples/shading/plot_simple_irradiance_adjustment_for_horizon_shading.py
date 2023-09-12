b767cd23bc8a576a9b5e607936ba64a75c246496
Saurabh Aneja <63314623+spaneja@users.noreply.github.com>
2023-09-12 12:14:38 -0400

    Update 2
    Adjusted to the broader 'simple irradiance...shading' rebrand, use hard-coded horizon profile data, and give example of how to get such data from PVGIS function in IO Tools.

--- docs/examples/shading/plot_simple_irradiance_adjustment_for_horizon_shading.py
+++ docs/examples/shading/plot_simple_irradiance_adjustment_for_horizon_shading.py
@@ -1,25 +1,24 @@
 """
-Far-Shading with PVGIS Horizon Data
-===================================
+Simple Irradiance Adjustment For Horizon Shading
+================================================
 
-Example of getting PVGIS horizon data, interpolating it to time-series
-solar-position data, and adjusting DNI and POA-global irradiance.
+Example for adjusting dni and global poa from horizon shading. Using
+hard-coded horizon profile data, this example interpolates it to time-series
+solar-position data, and adjust DNI and POA-global irradiance.
 """
 
 # %%
-# This example shows how to use retrived horizon elevation angles with
-# corresponding horizon azimuth angles from the
-# :py:func:`pvlib.iotools.get_pvgis_horizon` function.
+# This example shows how to use horizon elevation angles with
+# corresponding horizon azimuth angles for simple horizon shading adjustments.
 
 # After location information and a date range is established, solar position
 # data is queried from :py:meth:`pvlib.solar_position.get_solar_position`.
-# Horizon data is then retreived, and interpolated to the solar azimuth time
+# Horizon data is assigned, and interpolated to the solar azimuth time
 # series data. Finally, in times when solar elevation is greater than the
 # interpolated horizon elevation angle, DNI is set to 0.
 
 import numpy as np
 import pandas as pd
-from pvlib.iotools import get_pvgis_horizon
 from pvlib.location import Location
 from pvlib.irradiance import get_total_irradiance
 
@@ -37,10 +36,7 @@ location = Location(lat, lon, tz)
 solar_position = location.get_solarposition(times)
 clearsky = location.get_clearsky(times)
 
-# Get horizon file meta-data and data.
-horizon_data, horizon_metadata = get_pvgis_horizon(lat, lon)
-
-# Set variable names for easier reading.
+# Assign variable names for easier reading.
 surface_tilt = 30
 surface_azimuth = 180
 solar_azimuth = solar_position.azimuth
@@ -50,10 +46,21 @@ dni = clearsky.dni
 ghi = clearsky.ghi
 dhi = clearsky.dhi
 
+# Use hard-coded horizon profile data from location object above.
+horizon_profile = pd.Series([
+    10.7, 11.8, 11.5, 10.3, 8.0, 6.5, 3.8, 2.3, 2.3, 2.3, 4.6, 8.0, 10.3, 11.1,
+    10.7, 10.3, 9.2, 6.1, 5.3, 2.3, 3.1, 1.9, 1.9, 2.7, 3.8, 5.3, 6.5, 8.4,
+    8.8, 8.4, 8.4, 8.4, 6.5, 6.1, 6.5, 6.1, 7.3, 9.2, 8.4, 8.0, 5.7, 5.3, 5.3,
+    4.2, 4.2, 4.2, 7.3, 9.5
+], index=np.arange(0, 360, 7.5))
+
+# Ex - To get the above horizon data from PVGIS (assuming pvlib is imported):
+# horizon_profile, horizon_metadata = pvlib.iotools.get_pvgis_horizon(lat, lon)
+
 # Interpolate the horizon elevation data to the solar azimuth, and keep as a
 # numpy array.
 horizon_elevation_data = np.interp(
-    solar_azimuth, horizon_data.index, horizon_data
+    solar_azimuth, horizon_profile.index, horizon_profile
 )
 
 # Convert to Pandas Series for easier usage.

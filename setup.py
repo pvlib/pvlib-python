c78ec3bf9baacee9bceb8c579792cc1b5a545944
Kevin Anderson <kevin.anderso@gmail.com>
2023-11-29 11:57:51 -0500

    Edit project description in README, RTD, PyPI (#1913)
    * edit project description in README, RTD, PyPI
    
    * Apply suggestions from code review
    
    Co-authored-by: Anton Driesse <anton.driesse@pvperformancelabs.com>
    
    * adopt reviewer suggestions
    
    Co-Authored-By: Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
    Co-Authored-By: Will Holmgren <william.holmgren@gmail.com>
    
    * remove history and support statement from setup.py
    
    * add "university research projects"
    
    * README: move history/funding to separate section
    
    ---------
    
    Co-authored-by: Anton Driesse <anton.driesse@pvperformancelabs.com>
    Co-authored-by: Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
    Co-authored-by: Will Holmgren <william.holmgren@gmail.com>

--- setup.py
+++ setup.py
@@ -12,14 +12,11 @@ except ImportError:
 DESCRIPTION = ('A set of functions and classes for simulating the ' +
                'performance of photovoltaic energy systems.')
 LONG_DESCRIPTION = """
-PVLIB Python is a community supported tool that provides a set of
+pvlib python is a community developed toolbox that provides a set of
 functions and classes for simulating the performance of photovoltaic
-energy systems. PVLIB Python was originally ported from the PVLIB MATLAB
-toolbox developed at Sandia National Laboratories and it implements many
-of the models and methods developed at the Labs. More information on
-Sandia Labs PV performance modeling programs can be found at
-https://pvpmc.sandia.gov/. We collaborate with the PVLIB MATLAB project,
-but operate independently of it.
+energy systems and accomplishing related tasks.  The core mission of pvlib
+python is to provide open, reliable, interoperable, and benchmark
+implementations of PV system models.
 
 We need your help to make pvlib-python a great tool!
 

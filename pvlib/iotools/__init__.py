36d56c322b7501f93b296cab6ed55e5dbf02112b
Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
2023-12-19 19:36:10 +0100

    Update __init__.py

--- pvlib/iotools/__init__.py
+++ pvlib/iotools/__init__.py
@@ -1,7 +1,6 @@
 from pvlib.iotools.tmy import read_tmy2, read_tmy3  # noqa: F401
 from pvlib.iotools.epw import read_epw, parse_epw  # noqa: F401
 from pvlib.iotools.srml import read_srml  # noqa: F401
-from pvlib.iotools.srml import read_srml_month_from_solardat  # noqa: F401
 from pvlib.iotools.srml import get_srml  # noqa: F401
 from pvlib.iotools.surfrad import read_surfrad  # noqa: F401
 from pvlib.iotools.midc import read_midc  # noqa: F401

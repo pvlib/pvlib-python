da4110f1ba6d2e35aca43a2efd9f3c7ab37c7c4e
lorenzo-solcast <142359869+lorenzo-solcast@users.noreply.github.com>
2023-12-20 12:04:59 +1100

    add functionality to load Solcast API data to `iotools` (#1875)
    * prototype (#1)
    
    * first iteration
    
    * dynamic period
    
    * docstring
    
    * feedback
    
    * linting
    
    * Update pvlib/iotools/solcast.py
    
    Co-authored-by: Cliff Hansen <cwhanse@sandia.gov>
    
    * Update pvlib/iotools/solcast.py
    
    Co-authored-by: Cliff Hansen <cwhanse@sandia.gov>
    
    * Update pvlib/iotools/solcast.py
    
    Co-authored-by: Cliff Hansen <cwhanse@sandia.gov>
    
    * midpoint docstring
    
    * flak8 formatting
    
    * PR 1875 (#2)
    
    * kandersolar feedback (#3)
    
    * addressing feedback from Kandersolar
    
    * Review (#4)
    
    added hack for ISO periods in Pandas 0.25 and clearsky parameter maps
    
    * comment on pandas version
    
    * Update pvlib/iotools/solcast.py
    
    Co-authored-by: Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
    
    * Update pvlib/iotools/solcast.py
    
    Co-authored-by: Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
    
    * Update pvlib/iotools/solcast.py
    
    Co-authored-by: Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
    
    * Update pvlib/iotools/solcast.py
    
    Co-authored-by: Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
    
    * Update pvlib/iotools/solcast.py
    
    Co-authored-by: Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
    
    * Update pvlib/iotools/solcast.py
    
    Co-authored-by: Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
    
    * Update pvlib/iotools/solcast.py
    
    Co-authored-by: Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
    
    * Update pvlib/iotools/solcast.py
    
    Co-authored-by: Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
    
    * Update pvlib/iotools/solcast.py
    
    Co-authored-by: Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
    
    * Adams's feedback
    
    * Update pvlib/iotools/solcast.py
    
    Co-authored-by: Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
    
    * Adams's feedback
    
    * Last minor changes
    
    * added test for _get_solcast
    
    * feat: add additional test coverage (#5)
    
    Co-authored-by: Hugh Cutcher <hugh@solcast.com.au>
    
    * linting
    
    ---------
    
    Co-authored-by: Cliff Hansen <cwhanse@sandia.gov>
    Co-authored-by: Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
    Co-authored-by: hugh-solcast <143680553+hugh-solcast@users.noreply.github.com>
    Co-authored-by: Hugh Cutcher <hugh@solcast.com.au>

--- pvlib/iotools/__init__.py
+++ pvlib/iotools/__init__.py
@@ -27,3 +27,7 @@ from pvlib.iotools.acis import get_acis_nrcc  # noqa: F401
 from pvlib.iotools.acis import get_acis_mpe  # noqa: F401
 from pvlib.iotools.acis import get_acis_station_data  # noqa: F401
 from pvlib.iotools.acis import get_acis_available_stations  # noqa: F401
+from pvlib.iotools.solcast import get_solcast_forecast  # noqa: F401
+from pvlib.iotools.solcast import get_solcast_live  # noqa: F401
+from pvlib.iotools.solcast import get_solcast_historic  # noqa: F401
+from pvlib.iotools.solcast import get_solcast_tmy  # noqa: F401

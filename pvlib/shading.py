379ca9593a00a439f579e82e55c3a7bcae7bb881
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2024-01-27 12:49:45 +0100

    Figures have captions, images do not
    https://pandemic-overview.readthedocs.io/en/latest/myGuides/reStructuredText-Images-and-Figures-Examples.html#id18

--- pvlib/shading.py
+++ pvlib/shading.py
@@ -244,7 +244,7 @@ def projected_solar_zenith_angle(axis_tilt, axis_azimuth,
     direction of ``axis_azimuth`` (clockwise from north) and tilted from
     horizontal by ``axis_tilt``. See Figure 5 in [1]_:
 
-    .. image:: ../../_images/Anderson_Mikofski_2020_Fig5.jpg
+    .. figure:: ../../_images/Anderson_Mikofski_2020_Fig5.jpg
        :alt: Wire diagram of coordinates systems to obtain the projected angle.
        :align: center
        :scale: 75 %

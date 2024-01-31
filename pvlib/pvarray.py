d86de4503639c378f4c9adcb9380a96319edd13f
Cliff Hansen <cwhanse@sandia.gov>
2024-01-31 12:37:22 -0700

    tweak the note

--- pvlib/pvarray.py
+++ pvlib/pvarray.py
@@ -297,17 +297,17 @@ def huld(effective_irradiance, temp_mod, pdc0, k=None, cell_type=None):
                  k'_4 T' \log(G') + k'_5 T' \log^2 (G') + k'_6 T'^2)
 
 
-    With default values for ``k``, at very low irradiance, i.e.,
-    :math:`G' < 20 W/m^2`, :math:`P_{dc}` may be negative
-    due to the terms involving :math:`\log(G')`.
-
     PVGIS documentation shows a table of default parameters :math:`k'` for
     different cell types. The parameters :math:`k'` differ from the parameters
-    :math:`k` expected by :py:func:`huld` by the factor ``pdc0``, that is,
+    :math:`k` for :py:func:`huld` by the factor ``pdc0``, that is,
 
     .. math::
 
         k = P_{dc0} k'
+
+    With default values for :math:`k`, at very low irradiance, i.e.,
+    :math:`G' < 20 W/m^2`, :math:`P_{dc}` may be negative
+    due to the terms involving :math:`\log(G')`.
 
     :py:func:`huld` is a component of the PV performance model implemented in
     PVGIS. Among other components, the full PVGIS model includes:

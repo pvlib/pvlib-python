df450c7e9654854f78f9032cc3836315cb57742c
Kevin Anderson <kevin.anderso@gmail.com>
2024-02-23 12:16:01 -0500

    fix numba version check in `conftest.py`

--- pvlib/tests/conftest.py
+++ pvlib/tests/conftest.py
@@ -139,20 +139,14 @@ def has_spa_c():
 requires_spa_c = pytest.mark.skipif(not has_spa_c(), reason="requires spa_c")
 
 
-def has_numba():
-    try:
-        import numba
-    except ImportError:
-        return False
-    else:
-        vers = numba.__version__.split('.')
-        if int(vers[0] + vers[1]) < 17:
-            return False
-        else:
-            return True
+try:
+    import numba   # noqa: F401
+    has_numba = True
+except ImportError:
+    has_numba = False
 
 
-requires_numba = pytest.mark.skipif(not has_numba(), reason="requires numba")
+requires_numba = pytest.mark.skipif(not has_numba, reason="requires numba")
 
 
 try:

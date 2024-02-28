2be29f2d086d083c22788f5932767e681b9174e9
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2024-02-28 13:50:50 +0100

    Update shading.py

--- pvlib/shading.py
+++ pvlib/shading.py
@@ -276,8 +276,8 @@ def projected_solar_zenith_angle(solar_zenith, solar_azimuth,
       the axis of a single-axis tracker (i.e. the plane
       whose normal vector coincides with the tracker torque tube)
       yields the tracker rotation angle that maximizes direct irradiance
-      capture. This tracking strategy is called :ref:`true-tracking
-      <sphx_glr_gallery_solar-tracking_plot_single_axis_tracking.py:True-tracking>`.
+      capture. This tracking strategy is called *true-tracking*. Learn more
+      about tracking in :ref:`sphx_glr_gallery_solar-tracking_plot_single_axis_tracking.py`.
 
     - Self-shading in large PV arrays is often modeled by assuming
       a simplified 2-D array geometry where the sun's position is

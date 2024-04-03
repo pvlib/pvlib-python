f1cfaf8cf9d07d60b8c119b5884bf7f1c6a7f88d
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2024-04-03 18:36:39 +0200

    Update plot_oedi_9068.py

--- docs/examples/system-models/plot_oedi_9068.py
+++ docs/examples/system-models/plot_oedi_9068.py
@@ -5,8 +5,8 @@
 A basic model of a 4.7 MW single-axis tracking CdTe system located in
 Colorado, United States.
 """
-# sphinx_gallery_thumbnail_path = "_images/OEDI_9068_daily_timeseries.png"
 # %%
+# sphinx_gallery_thumbnail_path = "_images/OEDI_9068_daily_timeseries.png"
 # This example model uses satellite-based solar resource data from the
 # NSRDB PSM3. This approach is useful for pre-construction energy modeling
 # and in retrospective analyses where the system’s own irradiance

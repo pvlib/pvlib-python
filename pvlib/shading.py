255e678aa5f76f6d7c55280b7c2ace8e7fb6d775
Echedey Luis <80125792+echedey-ls@users.noreply.github.com>
2024-04-14 00:17:39 +0200

    Apply suggestions from code review (Adam)
    Co-authored-by: Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>

--- pvlib/shading.py
+++ pvlib/shading.py
@@ -356,10 +356,11 @@ def linear_shade_loss(shaded_fraction, diffuse_fraction):
     ----------
     shaded_fraction : numeric
         The fraction of the collector width shaded by an adjacent row. A
-        value of 1 is completely shaded and zero is no shade.
+        value of 1 corresponds to completely shaded and zero is no shading.
     diffuse_fraction : numeric
-        The ratio of diffuse plane of array (poa) irradiance to global poa.
-        A value of 1 is completely diffuse and zero is no diffuse.
+        The ratio of diffuse plane of array (POA) irradiance to global POA.
+        A value of 1 corresponds to all incident irradiance being diffuse,
+        and a value of zero corresponds to all irradiance being direct.
 
     Returns
     -------
@@ -370,13 +371,13 @@ def linear_shade_loss(shaded_fraction, diffuse_fraction):
     Notes
     -----
     The reasoning behind this loss model is described in sections 4.2 and 4.3
-    of [1]_, page 8 of the manual.
+    of [1]_.
 
     The loss is calculated as:
 
     .. math::
 
-        \text{loss} = \text{shaded\_fraction}\cdot(1-\text{diffuse\_fraction})
+        \text{loss} = \text{shaded_fraction}\cdot(1-\text{diffuse_fraction})
 
     See also
     --------
@@ -396,7 +397,6 @@ def linear_shade_loss(shaded_fraction, diffuse_fraction):
     References
     ----------
     .. [1] First Solar, “First Solar Series 4 Module.” [Online]. Available at: https://web.archive.org/web/20230512210902/https://www.firstsolar.com/-/media/First-Solar/Technical-Documents/User-Guides/Series-4-Module-User-Guide-North-America.ashx?la=en [accessed 2024-04-12]
-    .. [2] First Solar, “Diffuse Irradiance and Tracker Simulations,” May
-    01-02, 2013. http://web.archive.org/web/20170127080529/https://energy.sandia.gov/wp-content/gallery/uploads/26-Littmann-Tracking-and-Diffuse-Shading.pdf0_.pdf [accessed 2024-04-12]
+    .. [2] First Solar, “Diffuse Irradiance and Tracker Simulations,” May 01-02, 2013. http://web.archive.org/web/20170127080529/https://energy.sandia.gov/wp-content/gallery/uploads/26-Littmann-Tracking-and-Diffuse-Shading.pdf0_.pdf [accessed 2024-04-12]
     """
     return shaded_fraction * (1 - diffuse_fraction)

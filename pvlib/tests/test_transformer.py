f18ef779ec8fe0453e89b6f33bf3b3dc60ca210a
kurt-rhee <kurtrhee@gmail.com>
2024-05-17 16:08:15 -0700

    edited documentation to include latex, unsure if it will compile correctly in docs.

---
+++ pvlib/tests/test_transformer.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+
+from numpy.testing import assert_allclose
+
+from pvlib import transformer
+
+
+def test_simple_transformer():
+
+    # define test inputs
+    input_power = pd.Series([
+        -800.0,
+        436016.609823837,
+        1511820.16603752,
+        1580687.44677249,
+        1616441.79660171
+        ])
+    no_load_loss_fraction = 0.002
+    load_loss_fraction = 0.007
+    transformer_rating = 2750000
+
+    # define expected test results
+    expected_output_power = pd.Series([
+        -6300.10103234071,
+        430045.854892526,
+        1500588.39919874,
+        1568921.77089526,
+        1604389.62839879
+    ])
+
+    # run test function with test inputs
+    calculated_output_power = transformer.simple_efficiency(
+        input_power=input_power,
+        no_load_loss_fraction=no_load_loss_fraction,
+        load_loss_fraction=load_loss_fraction,
+        transformer_rating=transformer_rating
+        )
+    
+    # determine if expected results are obtained
+    assert_allclose(calculated_output_power, expected_output_power)
+
+if __name__ == '__main__':
+    test_simple_transformer()
--- pvlib/transformer.py
+++ pvlib/transformer.py
@@ -10,23 +10,63 @@ import numpy as np
 import pandas as pd
 
 
-def simple_efficiency(input_power, no_load_loss, load_loss, kva_rating):
+def simple_efficiency(input_power, no_load_loss_fraction, load_loss_fraction, transformer_rating):
     r'''
     Calculate the energy at the output terminal of the transformer
      after taking into account efficiency using a simple calculation.
+
+    The equation used in this function can be derived from the reference.
+
+    First, assume that the load loss is proportional to the square of output power.
+
+        .. math::
+        L_{load}(P_{out}) = L_{load}(P_{out}) * P^2_{out}
+        L_{load}(P_{out}) = L_{full, load} * P^2_{out}
+    
+    Total loss is the variable load loss, plus a constant no-load loss:
+
+        .. math::
+        L_{total}(P_{out}) = L_{no, load} + L_{load}(P_{out})
+        L_{total}(P_{out}) = L_{no, load} + L_{full, load} * P^2_{out}
+
+
+    Conservation of energy:
+
+        .. math::
+        P_{in} = P_{out} + L_{total}(P_{out})
+        P_{in} = P_{out} + L_{no, load} + L_{full, load} * P^2_{out}
+
+    Now use quadratic formula to solve for $P_{out}$ as a function of $P_in$.
+
+        ..math::
+        P_{out} = \frac{-b +- \sqrt{b^2 - 4ac}}{2a}
+        a = L_{full, load}
+        b = 1
+        c = L_{no, load} - P_{in}
+            
+    Therefore:
+        
+        ..math::
+        P_{out} = \frac{-1 +- \sqrt{1 - 4*L_{full, load}*L_{no, load} - P_{in}}}{2*L_{no, load} - P_{in}}
+
+    Note that the positive root must be the correct one if the output power in positive.
+
 
     Parameters
     ----------
     input_power : numeric
         The power that is input into the transformer. [W]
 
-    no_load_loss : numeric
+    no_load_loss_fraction : numeric
         The constant losses experienced by a transformer, even
         when the transformer is not under load. [% from 0 to 1]
 
-    load_loss:  numeric
+    load_loss_fraction:  numeric
         The load dependent losses experienced by the transformer.
         [% from 0 to 1]
+
+    transformer_rating: numeric
+        The nominal output power of the transformer. [VA]
 
     Returns
     -------
@@ -41,44 +81,63 @@ def simple_efficiency(input_power, no_load_loss, load_loss, kva_rating):
 
     '''
 
-    full_load_loss = (no_load_loss + load_loss) * kva_rating
-    no_load_loss_power = no_load_loss * kva_rating
-    load_loss_power = full_load_loss - no_load_loss_power
-    loss_power = (
-        1 / 
-        (2 * load_loss_power) *
+    # calculate the load loss in terms of VA instead of percent
+    loss_at_full_load = (no_load_loss_fraction + load_loss_fraction) * transformer_rating
+    no_load_loss = no_load_loss_fraction * transformer_rating
+    load_loss = loss_at_full_load - no_load_loss
+
+    # calculate how much power is lost
+    combined_loss = (
+        (1 / (2 * load_loss)) * 
         (
-            (kva_rating ** 2) +
-            (2 * load_loss_power * input_power) - 
-            (
-                kva_rating * np.sqrt(
-                    (kva_rating ** 2) +
-                    (4 * load_loss_power * (input_power - no_load_loss))
-                )
-            )
+            (transformer_rating ** 2) +
+            (2 * load_loss * input_power) - 
+            (transformer_rating * np.sqrt(
+                (transformer_rating ** 2) +
+                (4 * load_loss) * (input_power - no_load_loss)
+            ))
         )
     )
 
-    output_power = input_power - loss_power
+
+    # calculate final output power given calculated losses
+    output_power = input_power - combined_loss
 
     return output_power
 
 
 if __name__ == '__main__':
     
-    input_power = pd.Series([0, 100, 200, 300])
-    no_load_loss = 0.01
-    load_loss = 0.005
-    kva_rating = 100
+    # define test inputs
+    input_power = pd.Series([
+        -800.0,
+        436016.609823837,
+        1511820.16603752,
+        1580687.44677249,
+        1616441.79660171
+        ])
+    no_load_loss_fraction = 0.002
+    load_loss_fraction = 0.007
+    transformer_rating = 2750000
 
-    test = simple_efficiency(
+    # define expected test results
+    expected_output_power = pd.Series([
+        -6300.10103234071,
+        430045.854892526,
+        1500588.39919874,
+        1568921.77089526,
+        1604389.62839879
+    ])
+
+    # run test function with test inputs
+    calculated_output_power = simple_efficiency(
         input_power=input_power,
-        no_load_loss=no_load_loss,
-        load_loss=load_loss,
-        kva_rating=kva_rating
-    )
+        no_load_loss_fraction=no_load_loss_fraction,
+        load_loss_fraction=load_loss_fraction,
+        transformer_rating=transformer_rating
+        )
 
-    print(test)
+    # print(calculated_output_power)
 
     print('done')
 

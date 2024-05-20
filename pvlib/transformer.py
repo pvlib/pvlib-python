aeeb23b0889eff9baa2d168fa39352f2a3b8cdb6
Kurt Rhee <33131958+kurt-rhee@users.noreply.github.com>
2024-05-20 15:22:35 -0700

    Update pvlib/transformer.py
    Co-authored-by: Cliff Hansen <cwhanse@sandia.gov>

--- pvlib/transformer.py
+++ pvlib/transformer.py
@@ -42,10 +42,11 @@ def simple_efficiency(
     Now use quadratic formula to solve for $P_{out}$ as a function of $P_in$.
 
         ..math::
-        P_{out} = \frac{-b +- \sqrt{b^2 - 4ac}}{2a}
-        a = L_{full, load}
-        b = 1
-        c = L_{no, load} - P_{in}
+
+           P_{out} = \frac{-b +- \sqrt{b^2 - 4ac}}{2a}
+           a = L_{full, load}
+           b = 1
+           c = L_{no, load} - P_{in}
 
     Therefore:
 

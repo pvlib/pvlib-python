cb8a17a5bfa3e725e9dfd5bb92e0b3f9fa21e7f8
RDaxini <143435106+RDaxini@users.noreply.github.com>
2024-05-31 09:20:30 +0100

    Update mismatch.py
    add new spectral factor (to be tested)

--- pvlib/spectrum/mismatch.py
+++ pvlib/spectrum/mismatch.py
@@ -571,3 +571,127 @@ def spectral_factor_caballero(precipitable_water, airmass_absolute, aod500,
     )
     modifier = f_AM + f_AOD + f_PW  # Eq 5
     return modifier
+
+def test_spectral_factor_pelland(airmass_absolute, clearness_index,
+                            module_type = None, coefficients = None,
+                            min_airmass_absolute = 0.58,
+                            max_airmass_absolute = 10):
+    r"""
+    Estimate a technology-specific spectral mismatch modifier from
+    airmass and clearness index using the Pelland model, which takes the
+    following form:
+        
+        .. math::
+
+            M = a_1*kc**(a_2)*ama**(a_3),
+    
+    where M is the spectral mismatch factor, and a_1, a_2, a_3 are
+    module-specific coefficients.
+
+    The motivtion for this model is to include the effect of cloud cover on the
+    spectrum, and thus spectral mismatch. Another motivation is to develop a
+    simple parameterisation compared with existing models. Model coefficients
+    are derived using spectral irradiance and other meteorological data from
+    eight locations. These coefficients for seven modules, available here via
+    the ``module_type`` parameter, as well as more details on the model, can be
+    found in Ref. [1]_
+
+    Parameters
+    ----------
+    airmass_absolute : numeric
+        absolute (pressure-adjusted) airmass. [unitless]
+
+    clearness_index: numeric
+        clearness index. [unitless]
+
+    module_type : str, optional
+        One of the following PV technology strings from [1]_:
+
+        * ``'fs4-1'`` - First Solar series 4-1 and earlier CdTe module.
+        * ``'fs4-2'`` - First Solar 4-2 and earlier CdTe module.
+        * ``'monosi'``, - anonymous sc-si module.
+        * ``'multisi'``, - anonymous mc-si- module.
+        * ``'cigs'`` - anonymous copper indium gallium selenide module.
+        * ``'asi'`` - anonymous amorphous silicon module.
+
+    coefficients : array-like, optional
+        user-defined coefficients, if not using one of the default coefficient
+        sets via the ``module_type`` parameter.
+
+    Returns
+    -------
+    mismatch: numeric
+        spectral mismatch factor (unitless) which is multiplied
+        with broadband irradiance reaching a module's cells to estimate
+        effective irradiance, i.e., the irradiance that is converted to
+        electrical current.
+
+    References
+    ----------
+    .. [1] Pelland, S., Beswick, C., Thevenard, D., Côté, A., Pai, A. and 
+    Poissant, Y., 2020, June. Development and testing of the PVSPEC model of 
+    photovoltaic spectral mismatch factor. In 2020 47th IEEE Photovoltaic 
+    Specialists Conference (PVSC) (pp. 1258-1264). IEEE.
+        :doi:`https://doi.org/10.1109/PVSC45281.2020.9300932`
+    """
+# =============================================================================
+#      --- Screen Input Data ---
+# =============================================================================
+    #kc
+    kc = np.atleast_1d(clearness_index)
+    kc = kc.astype('float64')
+    if np.min(kc) < 0:
+        raise ValueError('Clearness index cannot be less than 0')
+    if np.max(kc) > 1:
+        raise ValueError('Clearness index cannot be grater than 1')
+    #ama
+    if np.max(airmass_absolute) > max_airmass_absolute:
+        warn('High air mass values greater than 'f'{max_airmass_absolute} '+
+             'in dataset')
+    # Warn user about exceptionally low ama data
+    if np.min(airmass_absolute) < min_airmass_absolute:
+        airmass_absolute = np.maximum(airmass_absolute, min_airmass_absolute )
+        warn('Exceptionally low air mass: ' +
+             'model not intended for extra-terrestrial use')
+# =============================================================================
+#      --- Default coefficients ---
+# =============================================================================
+    _coefficients = {}
+    _coefficients['multisi'] = (
+        0.9847, -0.05237, 0.03034)
+    _coefficients['monosi'] = (
+        0.9845, -0.05169, 0.03034)
+    _coefficients['fs-2'] = (
+        1.002, -0.07108, 0.02465)
+    _coefficients['fs-4'] = (
+        0.9981, -0.05776, 0.02336)
+    _coefficients['cigs'] = (
+        0.9791, -0.03904, 0.03096)
+    _coefficients['asi'] = (
+        1.051, -0.1033, 0.009838)
+    _coefficients['polysi'] = _coefficients['multisi']
+    _coefficients['xsi'] = _coefficients['monosi']
+# =============================================================================
+#      --- Check arguments ---
+# =============================================================================
+    if module_type is not None and coefficients is None:
+        coefficients = _coefficients[module_type.lower()]
+    elif module_type is None and coefficients is not None:
+        pass
+    elif module_type is None and coefficients is None:
+        raise TypeError('No valid input provided, both module_type and ' +
+                        'coefficients are None. module_type can be one of ' +
+                        'poly-Si, monosi, fs-2, fs-4, cigs, or asi')
+    else:
+        raise TypeError('Cannot resolve input, must supply only one of ' +
+                        'module_type and coefficients. module_type can be ' +
+                        'one of poly-Si, monosi, fs-2, fs-4, cigs, or asi')
+# =============================================================================
+#      --- Specral mismatch calculation ---
+# =============================================================================
+    coeff = coefficients
+    ama = airmass_absolute
+    kc = clearness_index
+    mismatch = coeff[0]*kc**(coeff[1])*ama**(coeff[2])
+
+    return mismatch

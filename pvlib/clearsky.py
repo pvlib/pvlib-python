3fed2829cb598c49ab4786a61ae4376a79fe6191
yhkee0404 <yhkee0404@gmail.com>
2024-06-01 23:53:52 +0900

    Add docstring Notes to lookup_linke_turbidity

--- pvlib/clearsky.py
+++ pvlib/clearsky.py
@@ -169,6 +169,14 @@ def lookup_linke_turbidity(time, latitude, longitude, filepath=None,
     Returns
     -------
     turbidity : Series
+
+    Notes
+    -----
+    Linke turbidity is obtained from a file of historical monthly averages.
+    The returned value for each time is either the monthly value or an
+    interpolated value to smooth the transition between months.
+    Interpolation is done on the day of year, and for this purpose time is
+    first converted to UTC.
     """
 
     # The .h5 file 'LinkeTurbidities.h5' contains a single 2160 x 4320 x 12

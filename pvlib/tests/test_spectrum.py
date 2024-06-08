b36116d837ec003dfdb1e6c1603fc02d37c1c8ce
RDaxini <143435106+RDaxini@users.noreply.github.com>
2024-06-08 18:53:52 +0100

    Update test_spectrum.py

--- pvlib/tests/test_spectrum.py
+++ pvlib/tests/test_spectrum.py
@@ -318,16 +318,16 @@ def test_spectral_factor_caballero_supplied_ambiguous():
 
 
 @pytest.mark.parametrize("module_type,expected", [
-    ('asi', np.array([1.15534029, 1.1123772, 1.08286684])),
-    ('fs-2', np.array([1.0694323, 1.04948777, 1.03556288])),
-    ('fs-4', np.array([1.05234725, 1.037771, 1.0275516])),
-    ('multisi', np.array([1.03310403, 1.02391703, 1.01744833])),
-    ('monosi', np.array([1.03225083, 1.02335353, 1.01708734])),
-    ('cigs', np.array([1.01475834, 1.01143927, 1.00909094])),
+    ('asi', np.array([1.15534029, 1.1123772, 1.08286684, 1.01915462])),
+    ('fs-2', np.array([1.0694323, 1.04948777, 1.03556288, 0.9881471])),
+    ('fs-4', np.array([1.05234725, 1.037771, 1.0275516, 0.98820533])),
+    ('multisi', np.array([1.03310403, 1.02391703, 1.01744833, 0.97947605])),
+    ('monosi', np.array([1.03225083, 1.02335353, 1.01708734, 0.97950110])),
+    ('cigs', np.array([1.01475834, 1.01143927, 1.00909094, 0.97852966])),
 ])
 def test_spectral_factor_pvspec(module_type, expected):
-    ams = np.array([1.0, 1.5, 2.0])
-    kcs = np.array([0.4, 0.6, 0.8])
+    ams = np.array([1.0, 1.5, 2.0, 1.5])
+    kcs = np.array([0.4, 0.6, 0.8, 1.4])
     out = spectrum.spectral_factor_pvspec(ams, kcs,
                                           module_type=module_type)
     assert np.allclose(expected, out, atol=1e-8)

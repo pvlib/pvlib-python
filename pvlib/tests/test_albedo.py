0e5b464bcc9b94a405247c86fff4fbde2919a7dd
Ioannis Sifnaios <john_sif@hotmail.com>
2024-06-10 22:04:03 +0200

    fixed linter

--- pvlib/tests/test_albedo.py
+++ pvlib/tests/test_albedo.py
@@ -8,21 +8,22 @@ from numpy.testing import assert_allclose
 
 def test_albedo_water_default():
     result = albedo.inland_water_dvoracek(solar_elevation=90,
-                                 color_coeff=0.13,
-                                 wave_roughness_coeff=0.29)
+                                          color_coeff=0.13,
+                                          wave_roughness_coeff=0.29)
     assert_allclose(result, 0.072, 0.001)
 
 
 def test_albedo_water_negative_elevation():
     result = albedo.inland_water_dvoracek(solar_elevation=-60,
-                                 color_coeff=0.13,
-                                 wave_roughness_coeff=0.29)
+                                          color_coeff=0.13,
+                                          wave_roughness_coeff=0.29)
     assert_allclose(result, 0.13, 0.01)
 
 
 def test_albedo_water_string_surface_condition():
     result = albedo.inland_water_dvoracek(solar_elevation=90,
-                                 surface_condition='clear_water_no_waves')
+                                          surface_condition=
+                                          'clear_water_no_waves')
     assert_allclose(result, 0.072, 0.001)
 
 
@@ -31,8 +32,9 @@ def test_albedo_water_ndarray():
     color_coeffs = np.array([0.1, 0.1, 0.2, 0.3, 0.4])
     roughness_coeffs = np.array([0.3, 0.3, 0.8, 1.5, 2])
     result = albedo.inland_water_dvoracek(solar_elevation=solar_elevs,
-                                 color_coeff=color_coeffs,
-                                 wave_roughness_coeff=roughness_coeffs)
+                                          color_coeff=color_coeffs,
+                                          wave_roughness_coeff=
+                                          roughness_coeffs)
     expected = np.array([0.1, 0.1, 0.12875, 0.06278, 0.064])
     assert_allclose(expected, result, atol=1e-5)
 
@@ -44,7 +46,8 @@ def test_albedo_water_series():
     color_coeffs = pd.Series([0.1, 0.1, 0.2, 0.3, 0.4], index=times)
     roughness_coeffs = pd.Series([0.1, 0.3, 0.8, 1.5, 2], index=times)
     result = albedo.inland_water_dvoracek(solar_elevation=solar_elevs,
-                                 color_coeff=color_coeffs,
-                                 wave_roughness_coeff=roughness_coeffs)
+                                          color_coeff=color_coeffs,
+                                          wave_roughness_coeff=
+                                          roughness_coeffs)
     expected = pd.Series([0.1, 0.1, 0.12875, 0.06278, 0.064], index=times)
     assert_series_equal(expected, result, atol=1e-5)

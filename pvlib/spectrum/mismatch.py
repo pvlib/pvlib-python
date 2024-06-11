d5f097995356eb50f8939b49094987b3e0de4f7d
RDaxini <143435106+RDaxini@users.noreply.github.com>
2024-06-11 16:14:41 +0200

    Update pvlib/spectrum/mismatch.py
    Co-authored-by: Echedey Luis <80125792+echedey-ls@users.noreply.github.com>

--- pvlib/spectrum/mismatch.py
+++ pvlib/spectrum/mismatch.py
@@ -649,9 +649,8 @@ def spectral_factor_pvspec(airmass_absolute, clearsky_index,
        method. Solar energy, 68(1), pp.33-48.
        :doi:`10.1016/S0038-092X(99)00055-9`
     .. [4] SoDa website monthly Linke turbidity values:
-       `http://www.sodapro.com/gl/web-services/atmosphere/linke-turbidity-
-       factor-ozone-watervapor-and-angstroembeta`_.
-    """
+       `http://www.sodapro.com/gl/web-services/atmosphere/linke-turbidity-factor-ozone-watervapor-and-angstroembeta`_.
+    """  # noqa: E501
 
     _coefficients = {}
     _coefficients['multisi'] = (0.9847, -0.05237, 0.03034)

0c992692bfc04e166b7190d1a494a714ab5ba4dd
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2024-06-12 06:52:15 +0200

    Revert unintended changes

--- pvlib/bifacial/__init__.py
+++ pvlib/bifacial/__init__.py
@@ -7,7 +7,7 @@ from pvlib.bifacial import pvfactors, infinite_sheds, utils  # noqa: F401
 from .loss_models import power_mismatch_deline  # noqa: F401
 
 pvfactors_timeseries = deprecated(
-    since="0.9.1",
-    name="pvlib.bifacial.pvfactors_timeseries",
-    alternative="pvlib.bifacial.pvfactors.pvfactors_timeseries",
+    since='0.9.1',
+    name='pvlib.bifacial.pvfactors_timeseries',
+    alternative='pvlib.bifacial.pvfactors.pvfactors_timeseries'
 )(pvfactors.pvfactors_timeseries)

56c7d2eb53d4945ed13ed6b925c5d655d8ab24a8
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2024-06-12 06:38:15 +0200

    yup it didnt save

--- pvlib/bifacial/__init__.py
+++ pvlib/bifacial/__init__.py
@@ -4,7 +4,7 @@ The ``bifacial`` submodule contains functions to model bifacial modules.
 
 from pvlib._deprecation import deprecated
 from pvlib.bifacial import pvfactors, infinite_sheds, utils  # noqa: F401
-from .losses_models import power_mismatch_deline  # noqa: F401
+from .loss_models import power_mismatch_deline  # noqa: F401
 
 pvfactors_timeseries = deprecated(
     since="0.9.1",

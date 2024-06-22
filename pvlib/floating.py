ff791e63775fb41c9572d4e5cecc2e3515500271
Ioannis Sifnaios <john_sif@hotmail.com>
2024-06-22 12:54:12 +0200

    format edits

--- pvlib/floating.py
+++ pvlib/floating.py
@@ -24,21 +24,22 @@ def daily_stream_temperature_stefan(temp_air):
     Notes
     -----
     The equation for calculating the daily average stream water temperature
-    :math:`T_w` using the ambient air temperature :math:`T_{air}` is given by:
+    :math:`T_w` using the ambient air temperature :math:`T_{air}` is provided
+    in [1]_ and given by:
 
     .. math::
        :label: stream
 
-        T_w = 5 + 0.75 * T_{air}
+        T_w = 5 + 0.75 \cdot T_{air}
 
     The predicted daily stream water temperatrues of this equation had a
-    standard deviation of 2.7 ^oC compared to measurements. Small, shallow
+    standard deviation of 2.7 $^o$C compared to measurements. Small, shallow
     streams had smaller deviations than large, deep rivers.
 
     It should be noted that this equation is limited to streams, i.e., water
     bodies that are well mixed in vertical and transverse direction of a cross
     section. Also, it is only tested on ice-free streams. Consequently, when
-    the mean ambient air temperature is lower than -6 ^oC, the surface stream
+    the mean ambient air temperature is lower than -6 $^o$C, the surface stream
     water temperature is assumed to be zero.
 
     References

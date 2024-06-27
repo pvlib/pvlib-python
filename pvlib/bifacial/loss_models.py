6fafcb4586612e57b0d92585e14a504a266ad743
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2024-06-28 00:33:47 +0200

    Kevin's review (II)
    Co-Authored-By: Kevin Anderson <57452607+kandersolar@users.noreply.github.com>

--- pvlib/bifacial/loss_models.py
+++ pvlib/bifacial/loss_models.py
@@ -1,15 +1,12 @@
 import numpy as np
 import pandas as pd
 
-from typing import Literal
-
 
 def power_mismatch_deline(
     rmad,
-    model: Literal[
-        "fixed-tilt", "single-axis-tracking"
-    ] = "single-axis-tracking",
-    fillfactor: float = None,
+    model=(0, 0.142, 0.032),
+    fill_factor: float = None,
+    fill_factor_reference: float = 0.79,
 ):
     r"""
     Estimate DC power loss due to irradiance non-uniformity.
@@ -17,13 +14,12 @@ def power_mismatch_deline(
     This model is described for bifacial modules in [1]_, where the backside
     irradiance is less uniform due to mounting and site conditions.
 
-    Depending on the mounting type, the power loss is estimated with either
-    equation (11) or (12) of [1]_. Passing a custom polynomial is also valid.
+    The power loss is estimated by a polynomial model of the Relative Mean
+    Absolute Difference (RMAD) of the cell-by-cell total irradiance.
 
-    Use ``fillfactor`` to account for different fill factors between the
-    trained model and the module of interest.
-    For example, if the fill factor of the module of interest is
-    0.65, then set ``fillfactor=0.65``.
+    Use ``fill_factor`` to account for different fill factors between the
+    trained model and the module of interest. Specify model's fill factor with
+    ``fill_factor_reference``.
 
     .. versionadded:: 0.11.1
 
@@ -35,52 +31,40 @@ def power_mismatch_deline(
         Check out the *Notes* section for the equation to calculate it from the
         bifaciality and the front and back irradiances.
 
-    model : str, numpy.polynomial.polynomial.Polynomial or list, default ``"single-axis-tracking"``
+    model : float collection or numpy.polynomial.polynomial.Polynomial, default ``(0, 0.142, 0.032)``
         The model coefficients to use.
-        If a string, it must be one of the following:
-
-        * ``"fixed-tilt"``: Eq. (11) of [1]_.
-        * ``"single-axis-tracking"``: Eq. (12) of [1]_.
 
         If a :external:class:`numpy.polynomial.polynomial.Polynomial`,
-        it is evaluated as is.
-
-        If neither a string nor a ``Polynomial``, it must be the coefficients
-        of a polynomial in ``rmad``, where the first element is the constant
-        term and the last element is the highest order term. A
+        it is evaluated as is. If not a ``Polynomial``, it must be the
+        coefficients of a polynomial in ``rmad``, where the first element is
+        the constant term and the last element is the highest order term. A
         :external:class:`~numpy.polynomial.polynomial.Polynomial`
         will be created internally.
 
-    fillfactor : float, optional
+    fill_factor : float, optional
         Fill factor at standard test condition (STC) of the module.
         Accounts for different fill factors between the trained model and the
         module under non-uniform irradiance.
-        Models from [1]_ were calculated for a ``fillfactor`` of 0.79.
-        This parameter will only be used if ``model`` is a string.
-        Raises a ``ValueError`` if the model is a custom polynomial.
-        Internally, this argument applies :ref:`Equation (7) <fillfactor_eq>`.
+        If not provided, the model is used as is.
+
+    fill_factor_reference : float, default 0.79
+        Fill factor at STC of the module used to train the model.
+        If ``fill_factor`` is provided, this value is used to scale the output
+        of the model.
 
     Returns
     -------
-    loss : numeric, pd.Series if ``rmad`` is a pd.Series
+    loss : numeric
         The fractional power loss. [Unitless]
-
-    Raises
-    ------
-    ValueError
-        If the model is not a string and ``fillfactor`` is not ``None``.
+        Output will be a ``pandas.Series`` if ``rmad`` is a ``pandas.Series``.
 
     Notes
     -----
-    The models implemented are equations (11) and (12) of [1]_:
+    The default model implemented is equation (11) [1]_:
 
     .. math::
 
-       \text{model="fixed-tilt"} & \Rightarrow M[\%] =
-       0.142 \Delta[\%] + 0.032 \Delta[\%]^2 \qquad & \text{(11)}
-
-       \text{model="single-axis-tracking"} & \Rightarrow M[\%] =
-       0.054 \Delta[\%] + 0.068 \Delta[\%]^2 \qquad & \text{(12)}
+       M[\%] = 0.142 \Delta[\%] + 0.032 \Delta[\%]^2 \qquad & \text{(11)}
 
     where :math:`\Delta[\%]` is the Relative Mean Absolute Difference of the
     global irradiance, Eq. (4) of [1]_ and [2]_.
@@ -93,14 +77,15 @@ def power_mismatch_deline(
        M[\%] = 1 - \frac{P_{Array}}{\sum P_{Cells}} \qquad \text{(1)}
 
     To account for a module with a fill factor distinct from the one used to
-    train the model (0.79), the output of the model can be modified by Eq. (7):
-
-    .. _fillfactor_eq:
+    train the model (``0.79`` by default), the output of the model can be
+    modified with Eq. (7):
 
     .. math::
 
        M[\%]_{FF_1} = M[\%]_{FF_0} \frac{FF_1}{FF_0} \qquad \text{(7)}
 
+    where parameter ``fill_factor`` is :math:`FF_1` and
+    ``fill_factor_reference`` is :math:`FF_0`.
     In the section *See Also*, you will find two packages that can be used to
     calculate the irradiance at different points of the module.
 
@@ -140,34 +125,14 @@ def power_mismatch_deline(
        https://en.wikipedia.org/wiki/Mean_absolute_difference#Relative_mean_absolute_difference
        (accessed 2024-04-14).
     """  # noqa: E501
-    if isinstance(model, str):
-        _MODEL_POLYNOMS = {
-            "fixed-tilt": [0, 0.142, 0.032],  # Eq. (11), [1]
-            "single-axis-tracking": [0, 0.054, 0.068],  # Eq. (12), [1]
-        }
-        try:
-            model_polynom = np.polynomial.Polynomial(_MODEL_POLYNOMS[model])
-        except KeyError:
-            raise ValueError(
-                f"Invalid model '{model}'. Available models are "
-                f"{list(_MODEL_POLYNOMS.keys())}."
-            )
-        else:
-            if fillfactor:
-                # Use fillfactor to modify output of a known trained model
-                # Eq. (7), [1]
-                model_polynom = model_polynom * fillfactor / 0.79
-    else:
-        if fillfactor:
-            raise ValueError(
-                "Fill factor can only be used with predefined models. "
-                "Modify polynomial or multiply output by "
-                "'module_fillfactor / training_fillfactor'."
-            )
-        if isinstance(model, np.polynomial.Polynomial):
-            model_polynom = model
-        else:  # expect an iterable
-            model_polynom = np.polynomial.Polynomial(coef=model)
+    if isinstance(model, np.polynomial.Polynomial):
+        model_polynom = model
+    else:  # expect an iterable
+        model_polynom = np.polynomial.Polynomial(coef=model)
+
+    if fill_factor:  # Eq. (7), [1]
+        # Modify output of a trained model with different fill factor
+        model_polynom = model_polynom * fill_factor / fill_factor_reference
 
     mismatch = model_polynom(rmad)
     if isinstance(rmad, pd.Series):
--- pvlib/tests/bifacial/test_losses_models.py
+++ pvlib/tests/bifacial/test_losses_models.py
@@ -4,33 +4,18 @@ import pandas as pd
 import numpy as np
 from numpy.testing import assert_allclose
 
-import pytest
-
 
 def test_power_mismatch_deline():
     """tests bifacial.power_mismatch_deline"""
     premise_rmads = np.array([0.0, 0.05, 0.1, 0.15, 0.2, 0.25])
-    # default model result values
-    expected_sat_mms = np.array(
-        [0.0, 0.00287, 0.00608, 0.00963, 0.01352, 0.01775]
-    )
-    result_def_mms = bifacial.power_mismatch_deline(premise_rmads)
-    assert_allclose(result_def_mms, expected_sat_mms, atol=1e-5)
-    assert np.all(np.diff(result_def_mms) > 0)  # higher RMADs => higher losses
-    # default model matches single-axis tracker
-    result_sat_mms = bifacial.power_mismatch_deline(
-        premise_rmads, model="single-axis-tracking"
-    )
-    assert_allclose(result_sat_mms, expected_sat_mms)
-    # fixed-tilt model result values
+    # test default model is for fixed tilt
     expected_ft_mms = np.array(
         [0.0, 0.00718, 0.01452, 0.02202, 0.02968, 0.0375]
     )
-    result_ft_mms = bifacial.power_mismatch_deline(
-        premise_rmads, model="fixed-tilt"
-    )
-    assert_allclose(result_ft_mms, expected_ft_mms)
-    assert np.all(np.diff(result_ft_mms) > 0)  # higher RMADs => higher losses
+    result_def_mms = bifacial.power_mismatch_deline(premise_rmads)
+    assert_allclose(result_def_mms, expected_ft_mms, atol=1e-5)
+    assert np.all(np.diff(result_def_mms) > 0)  # higher RMADs => higher losses
+
     # test custom coefficients, set model to 1+1*RMAD
     # as Polynomial class
     polynomial = np.polynomial.Polynomial([1, 1, 0])
@@ -48,18 +33,15 @@ def test_power_mismatch_deline():
     result_mms = bifacial.power_mismatch_deline(pd.Series(premise_rmads))
     assert isinstance(result_mms, pd.Series)
 
-    # test fillfactor
-    # with an internal model
+    # test fill_factor, fill_factor_reference
+    # default model + default fill_factor_reference
     result_mms = bifacial.power_mismatch_deline(
-        premise_rmads, fillfactor=0.65
+        premise_rmads, fill_factor=0.65
     )
-    assert_allclose(result_mms, expected_sat_mms * 0.65 / 0.79, atol=1e-5)
-    # fails for a custom polynomial
-    with pytest.raises(ValueError, match="Fill factor can only be used"):
-        bifacial.power_mismatch_deline(
-            premise_rmads, model=polynomial, fillfactor=0.24
-        )
-
-    # test raises error on inexistent model
-    with pytest.raises(ValueError, match="Invalid model 'foo'"):
-        bifacial.power_mismatch_deline(premise_rmads, model="foo")
+    assert_allclose(result_mms, expected_ft_mms * 0.65 / 0.79, atol=1e-5)
+    # default model + custom fill_factor_reference
+    ff_ref = 0.75
+    result_mms = bifacial.power_mismatch_deline(
+        premise_rmads, fill_factor=0.65, fill_factor_reference=ff_ref
+    )
+    assert_allclose(result_mms, expected_ft_mms * 0.65 / ff_ref, atol=1e-5)

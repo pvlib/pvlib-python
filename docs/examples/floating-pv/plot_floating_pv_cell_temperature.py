c68c899e9875cee270490998b85698dd03fd13a7
Ioannis Sifnaios <john_sif@hotmail.com>
2024-06-28 09:46:01 +0200

    Cliff's suggestions

--- docs/examples/floating-pv/plot_floating_pv_cell_temperature.py
+++ docs/examples/floating-pv/plot_floating_pv_cell_temperature.py
@@ -15,8 +15,9 @@ and convective heat transfers play roles in determining the module's
 temperature.
 
 A popular model for calculating the PV cell temperature is the
-empirical heat loss factor model suggested by Faiman. A modified version of this model is implemented in
-PVSyst (:py:func:`~pvlib.temperature.pvsyst_cell`). The PVSyst model for cell
+empirical heat loss factor model suggested by Faiman. A modified version of
+this model is implemented in PVSyst
+(:py:func:`~pvlib.temperature.pvsyst_cell`). The PVSyst model for cell
 temperature :math:`T_{C}` is given by
 
 .. math::

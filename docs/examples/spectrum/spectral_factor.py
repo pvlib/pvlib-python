37bdee0fd6ee62afa897eedc8eab9e98c903b81b
RDaxini <143435106+RDaxini@users.noreply.github.com>
2024-07-04 16:38:47 +0100

    Update spectral_factor.py

--- docs/examples/spectrum/spectral_factor.py
+++ docs/examples/spectrum/spectral_factor.py
@@ -9,21 +9,21 @@ mismatch factor, :math:`M`, from atmospheric variable inputs.
 # Introduction
 # ------------
 # This example demonstrates how to use different `spectrum.spectral_factor`
-# models in pvlib to calculate the spectral mismatch factor, :math:`M`. While
-# :math:`M` for a photovoltaic (PV) module can be calculated exactly using
-# spectral irradiance and module spectral response data, these data are not
-# always available. Pvlib provides several functions to estimate the spectral
-# mismatch factor, :math:`M`, using proxies of the prevailing spectral
+# models in pvlib-python to calculate the spectral mismatch factor, :math:`M`.
+# While :math:`M` for a photovoltaic (PV) module can be calculated exactly
+# using spectral irradiance and module spectral response data, these data are
+# not always available. pvlib-python provides several functions to estimate the
+# spectral mismatch factor, :math:`M`, using proxies of the prevailing spectral
 # irradiance conditions, such as air mass and clearsky index, which are easily
 # derived from common ground-based measurements such as broadband irradiance.
 # More information on a range of spectral factor models, as well as the
 # variables upon which they are based, can be found in [1]_.
 #
 # Let's import some data. This example uses a Typical Meteorological Year 3
-# (TMY3) file for the location of Greensboro, North Carolina, from the pvlib
-# data directory. This TMY3 file is constructed using the median month from
-# each year between 1980 and 2003, from which we extract the first week of
-# August 2001 to analyse.
+# (TMY3) file for the location of Greensboro, North Carolina, from the
+# pvlib-python data directory. This TMY3 file is constructed using the median
+# month from each year between 1980 and 2003, from which we extract the first
+# week of August 2001 to analyse.
 
 # %%
 import pathlib
@@ -38,10 +38,10 @@ meteo, metadata = pvlib.iotools.read_tmy3(DATA_DIR / '723170TYA.CSV',
 meteo = meteo.loc['2001-08-01':'2001-08-07']
 
 # %%
-# pvlib Spectral Factor Functions
-# -----------------------------
-# This example demonstrates the application of three pvlib spectral factor
-# functions:
+# Spectral Factor Functions
+# -------------------------
+# This example demonstrates the application of three pvlib-python
+# spectral factor functions:
 #
 # - :py:func:`~pvlib.spectrum.spectral_factor_sapm`, which requires only
 #   the absolute airmass :math:`AM_a`
@@ -91,9 +91,10 @@ w = meteo.precipitable_water
 # %%
 # Calculation of Spectral Mismatch
 # --------------------------------
-# Let's calculate the spectral mismatch factor using the three pvlib functions.
-# First, we need to import some model coefficients for the SAPM spectral factor
-# function, which, unlike the other two functions, lacks built-in coefficients.
+# Let's calculate the spectral mismatch factor using the three spectral factor
+# functions. First, we need to import some model coefficients for the SAPM
+# spectral factor function, which, unlike the other two functions, lacks
+# built-in coefficients.
 
 # Import some for a mc-Si module from the SAPM module database.
 module = pvlib.pvsystem.retrieve_sam('SandiaMod')['LG_LG290N1C_G3__2013_']
@@ -111,7 +112,7 @@ df_results.columns = ['SAPM', 'PVSPEC', 'FS']
 # not been modelled in each case, only the same module technology.
 
 # Plot M
-fig1, ax1 = plt.subplots(figsize=(9.6, 7.2))
+fig1, ax1 = plt.subplots()
 df_results.plot(ax=ax1)
 
 ax1.set_xlabel('Date (m-d H:M)')
@@ -121,7 +122,7 @@ ax1.set_ylim(0.85, 1.15)
 plt.show()
 
 # We can also zoom in one one day, for example August 1st.
-fig2, ax1 = plt.subplots(figsize=(9.6, 7.2))
+fig2, ax1 = plt.subplots()
 df_results.loc['2001-08-02'].plot(ax=ax1)
 ax1.set_ylim(0.85, 1.15)
 plt.show()

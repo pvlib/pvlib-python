849ac5687f4ecde3882f4cc08db87724206f919e
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2024-07-05 21:51:52 +0200

    Fix empty sidebar not collapsing (I hope)
    Co-Authored-By: Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -215,7 +215,9 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-# html_sidebars = {}
+html_sidebars = {
+    "**": ["sidebar-nav-bs"]
+}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.

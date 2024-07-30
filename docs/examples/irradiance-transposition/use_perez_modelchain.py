4196d54fc4df3be6465342ac4d4d472508597827
Ben Pierce <bgp12@case.edu>
2024-07-30 12:28:58 -0400

    linter

--- docs/examples/irradiance-transposition/use_perez_modelchain.py
+++ docs/examples/irradiance-transposition/use_perez_modelchain.py
@@ -2,17 +2,23 @@
 Use different Perez coefficients with the ModelChain
 =================
 
-This example demonstrates how to customize the ModelChain to use site-specifc Perez coefficients.
+This example demonstrates how to customize the ModelChain
+to use site-specifc Perez coefficients.
 """
 
 #%%
-# The :py:class:`pvlib.modelchain.ModelChain` object provides a useful interface for easily constructing a model with a simple, unified interface.
-# However, a user may want to customize their models in various ways. 
-# One such example is during the irradiance transposition step. 
-# The Perez models perform very well on field data, but they require a set of fitted coefficients from various sites. 
-# It has been noted that these coefficients can be specific to various climates, so users may see a boost in model performance when using the correct set of parameters. 
-# However, the base `pvlib.modelchain.ModelChain` only supports the default coefficients. 
-# This example shows how the `pvlib.modelchain.ModelChain` can be adjusted to use a different set of Perez coefficients. 
+# The :py:class:`pvlib.modelchain.ModelChain` object provides a useful interface
+# for easily constructing a model with a simple, unified interface.
+# However, a user may want to customize their models in various ways.
+# One such example is during the irradiance transposition step.
+# The Perez models perform very well on field data, but
+# they require a set of fitted coefficients from various sites.
+# It has been noted that these coefficients can be specific to
+# various climates, so users may see a boost in model performance
+# when using the correct set of parameters.
+# However, the base `pvlib.modelchain.ModelChain` only supports the default coefficients.
+# This example shows how the `pvlib.modelchain.ModelChain` can
+# be adjusted to use a different set of Perez coefficients.
 
 import pandas as pd
 from pvlib.pvsystem import PVSystem
@@ -58,7 +64,7 @@ system = PVSystem(arrays = None,
 
 mc = ModelChain(system, location=loc)
 
-# Cape Canaveral seems like the most likely match for climate 
+# Cape Canaveral seems like the most likely match for climate
 model_perez = 'capecanaveral1988'
 
 solar_position = loc.get_solarposition(times=weather_data.index)
@@ -94,7 +100,7 @@ mc.run_model_from_poa(POA_irradiance_new_perez)
 ac_power_new_perez = mc.results.ac
 
 start, stop = '1990-05-05 06:00:00', '1990-05-05 19:00:00'
-plt.plot(ac_power_default.loc[start:stop], 
+plt.plot(ac_power_default.loc[start:stop],
          label="Default Composite Perez Model")
 plt.plot(ac_power_new_perez.loc[start:stop],
          label="Cape Canaveral Perez Model")
@@ -102,4 +108,5 @@ plt.xticks(rotation=90)
 plt.ylabel("AC Power ($W$)")
 plt.legend()
 #%%
-# Note that there is a small, but noticeable difference from the default coefficients that may add up over longer periods of time. 
+# Note that there is a small, but noticeable difference from the default
+# coefficients that may add up over longer periods of time.

4ed2315749f3f27594f5267ad3f18b2b4ba7dee2
Scott Nelson <scott@scottnelson.co>
2024-08-14 06:11:45 -0400

    Update hour_angle docstring

--- pvlib/solarposition.py
+++ pvlib/solarposition.py
@@ -1360,6 +1360,11 @@ def hour_angle(times, longitude, equation_of_time):
     times : :class:`pandas.DatetimeIndex`
         Corresponding timestamps, must be localized to the timezone for the
         ``longitude``.
+        
+        A `pytz.exceptions.AmbiguousTimeError` could be raised if any of the
+        given times are on a day where the local daylight savings transition happened
+        at midnight.  If you're working with such a timezone, consider converting to
+        a non-DST timezone (i.e. GMT-4) before calling this function.
     longitude : numeric
         Longitude in degrees
     equation_of_time : numeric

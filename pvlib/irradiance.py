ef54fe9432d20fcc5c212fe3f2ca3b3133c2c8a6
RDaxini <143435106+RDaxini@users.noreply.github.com>
2024-09-17 12:28:02 +0100

    add last accessed to reference

--- pvlib/irradiance.py
+++ pvlib/irradiance.py
@@ -810,12 +810,13 @@ def haydavies(surface_tilt, surface_azimuth, dhi, dni, dni_extra,
 
     References
     -----------
-    .. [1] Hay, J. E., Davies, J.A., 1980. Calculations of the solar
+    .. [1] Hay, J. E., Davies, J. A., 1980. Calculations of the solar
        radiation incident on an inclined surface. In: Hay, J.E., Won, T.K.
        (Eds.), Proc. of First Canadian Solar Radiation Data Workshop, 59.
        Ministry of Supply and Services, Canada. Available from:
        https://archive.org/details/proceedingsfirst00cana/mode/2up
-    .. [2] Loutzenhiser P.G. et. al. "Empirical validation of models to
+       (Last accessed: 17/09/24)
+    .. [2] Loutzenhiser P. G. et. al. "Empirical validation of models to
        compute solar irradiance on inclined surfaces for building energy
        simulation" 2007, Solar Energy vol. 81. pp. 254-267
        :doi:`10.1016/j.solener.2006.03.009`

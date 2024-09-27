9c06acddd737d423c4716fdc29984445c2c4cf32
Cliff Hansen <cwhanse@sandia.gov>
2024-09-27 11:34:11 -0700

    play nice now

--- pvlib/pvsystem.py
+++ pvlib/pvsystem.py
@@ -1133,9 +1133,9 @@ class Array:
             albedo = self.albedo
 
         # dni_extra is not needed for all models, but this is easier
-        if (dni_extra is None and hasattr(solar_zenith, 'index') and
-            isinstance(solar_zenith.index, pd.DatetimeIndex)):
-                dni_extra = irradiance.get_extra_radiation(solar_zenith.index)
+        if (dni_extra is None and hasattr(solar_zenith, 'index')
+                and isinstance(solar_zenith.index, pd.DatetimeIndex)):
+            dni_extra = irradiance.get_extra_radiation(solar_zenith.index)
         else:
             # use the solar constant
             dni_extra = 1367

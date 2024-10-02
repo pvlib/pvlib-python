fd0974f7b23183d906e59139c5bcbf39827f59cc
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2024-10-02 21:44:03 +0100

    test-driven-development for the win: unchanged remain properties
    Co-Authored-By: Kevin Anderson <57452607+kandersolar@users.noreply.github.com>

--- pvlib/tests/test__deprecation.py
+++ pvlib/tests/test__deprecation.py
@@ -11,18 +11,25 @@ import warnings
 
 @pytest.fixture
 def renamed_kwarg_func():
-    """Returns a function decorated by renamed_kwarg_warning."""
+    """Returns a function decorated by renamed_kwarg_warning.
+    This function is called 'func' and has a docstring equal to 'docstring'.
+    """
 
     @_deprecation.renamed_kwarg_warning(
         "0.1.0", "old_kwarg", "new_kwarg", "0.2.0"
     )
     def func(new_kwarg):
+        """docstring"""
         return new_kwarg
 
     return func
 
 
 def test_renamed_kwarg_warning(renamed_kwarg_func):
+    # assert decorated function name and docstring are unchanged
+    assert renamed_kwarg_func.__name__ == "func"
+    assert renamed_kwarg_func.__doc__ == "docstring"
+
     # assert no warning is raised when using the new kwarg
     with warnings.catch_warnings():
         warnings.simplefilter("error")

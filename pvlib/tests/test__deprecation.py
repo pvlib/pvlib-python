1fdecb2472477791f8fbd1385f560af5f5c67252
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2024-10-02 21:37:55 +0100

    Fix no warning test
    Co-Authored-By: Kevin Anderson <57452607+kandersolar@users.noreply.github.com>

--- pvlib/tests/test__deprecation.py
+++ pvlib/tests/test__deprecation.py
@@ -6,10 +6,13 @@ import pytest
 
 from pvlib import _deprecation
 
+import warnings
+
 
 @pytest.fixture
 def renamed_kwarg_func():
     """Returns a function decorated by renamed_kwarg_warning."""
+
     @_deprecation.renamed_kwarg_warning(
         "0.1.0", "old_kwarg", "new_kwarg", "0.2.0"
     )
@@ -21,8 +24,10 @@ def renamed_kwarg_func():
 
 def test_renamed_kwarg_warning(renamed_kwarg_func):
     # assert no warning is raised when using the new kwarg
-    assert renamed_kwarg_func(new_kwarg=1) == 1  # as keyword argument
-    assert renamed_kwarg_func(1) == 1  # as positional argument
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        assert renamed_kwarg_func(new_kwarg=1) == 1  # as keyword argument
+        assert renamed_kwarg_func(1) == 1  # as positional argument
 
     # assert a warning is raised when using the old kwarg
     with pytest.warns(Warning, match="Parameter 'old_kwarg' has been renamed"):

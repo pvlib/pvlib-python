fce529c62bf7b8cdac76246f2d15f87d2bf26cc3
RDaxini <143435106+RDaxini@users.noreply.github.com>
2024-10-03 16:13:21 +0100

    extraterrestrial -> direct extraterrestrial
    as per the paper (variable I_on)

--- pvlib/irradiance.py
+++ pvlib/irradiance.py
@@ -932,10 +932,10 @@ def reindl(surface_tilt, surface_azimuth, dhi, dni, ghi, dni_extra,
     :math:`DHI`, :math:`BHI`, and :math:`GHI` are the diffuse, beam (direct)
     and global horizontal irradiances, respectively. :math:`A` is the
     anisotropy index, which is the ratio of the direct normal irradiance to the
-    extraterrestrial irradiation, :math:`R_b` is the projection ratio, which is
-    defined as the ratio of the cosine of the angle of incidence (AOI) to the
-    cosine of the zenith angle, and :math:`\theta_T` is the tilt angle of the
-    array.
+    direct extraterrestrial irradiation, :math:`R_b` is the projection ratio,
+    which is defined as the ratio of the cosine of the angle of incidence (AOI)
+    to the cosine of the zenith angle, and :math:`\theta_T` is the tilt angle
+    of the array.
 
     The ``poa_sky_diffuse`` calculation is generated from Loutzenhiser et al.
     (2007) [3]_, Equation 8. The beam and ground reflectance portion of the

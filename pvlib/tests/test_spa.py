c85731f69e02eea145cc925a545b7b90c73e67db
Kevin Anderson <kevin.anderso@gmail.com>
2024-10-25 14:33:56 -0400

    one last linter fix

--- pvlib/tests/test_spa.py
+++ pvlib/tests/test_spa.py
@@ -452,4 +452,3 @@ test_cases_issue_2207 = [
 def test_julian_day_issue_2207(inputs, expected):
     result = pvlib.spa.julian_day_dt(*inputs, microsecond=0)
     assert result == expected, f"Failed for inputs {inputs}"
-

4ef201b3ffeb960f4a5b4d56d0297a42b012642d
Cliff Hansen <cwhanse@sandia.gov>
2024-11-07 08:19:29 -0800

    back to ANY

--- pvlib/tests/ivtools/test_sdm.py
+++ pvlib/tests/ivtools/test_sdm.py
@@ -85,15 +85,15 @@ def test_fit_desoto_init_guess(mocker):
     init_guess = {k: v for k, v in zip(
         ['IL_0', 'Io_0', 'Rs_0', 'Rsh_0', 'a_0'], init_guess_array)}
     mocker.spy(optimize, 'root')
-    specs = (9.43, 38.3, 8.71, 31.0, -0.13788, 0.005658, 1.121, -0.0002677,
-             298.15, constants.value('Boltzmann constant in eV/K'))  # for test
+    #specs = (9.43, 38.3, 8.71, 31.0, -0.13788, 0.005658, 1.121, -0.0002677,
+    #         298.15, constants.value('Boltzmann constant in eV/K'))  # for test
     root_args = {}  # to fill out call for test
     result, _ = sdm.fit_desoto(v_mp=31.0, i_mp=8.71, v_oc=38.3, i_sc=9.43,
                                alpha_sc=0.005658, beta_voc=-0.13788,
                                cells_in_series=60, init_guess=init_guess)
     optimize.root.assert_called_once_with(mocker.ANY,
                                           x0=pytest.approx(init_guess_array),
-                                          args=(pytest.approx(specs),),
+                                          args=(mocker.ANY,),
                                           **root_args)
 
 

285cc1ace745c8dac94f9beffb2fd6bce627d97f
Cliff Hansen <cwhanse@sandia.gov>
2024-12-11 09:02:32 -0700

    add link to pvsyst_cell

--- pvlib/pvsystem.py
+++ pvlib/pvsystem.py
@@ -171,8 +171,8 @@ class PVSystem:
         Valid strings are ``'open_rack'``, ``'close_mount'``,
         ``'freestanding'``, and ``'insulated_back'`. Used to identify a
         parameter set for the SAPM cell temperature model.
-        See :py:func:`~pvlib.temperature.sapm_module`
-        for definitions.
+        See :py:func:`~pvlib.temperature.sapm_module` and
+        :py:func:`~pvlib.temperature.pvsyst_cell` for definitions.
 
     losses_parameters : dict or Series, optional
         Losses parameters as defined by PVWatts or other.
@@ -1398,8 +1398,8 @@ class FixedMount(AbstractMount):
         Valid strings are ``'open_rack'``, ``'close_mount'``,
         ``'freestanding'``, and ``'insulated_back'`. Used to identify a
         parameter set for the SAPM cell temperature model.
-        See :py:func:`~pvlib.temperature.sapm_module`
-        for definitions.
+        See :py:func:`~pvlib.temperature.sapm_module`  and
+        :py:func:`~pvlib.temperature.pvsyst_cell` for definitions.
 
     module_height : float, optional
        The height above ground of the center of the module [m]. Used for
@@ -1479,7 +1479,8 @@ class SingleAxisTrackerMount(AbstractMount):
         ``'freestanding'``, and ``'insulated_back'`. Used to identify a
         parameter set for the SAPM cell temperature model.
         ``'open_rack'`` should be used for systems with single-axis trackers.
-        See :py:func:`~pvlib.temperature.sapm_module` for definitions.
+        See :py:func:`~pvlib.temperature.sapm_module` and
+        :py:func:`~pvlib.temperature.pvsyst_cell` for definitions.
 
     module_height : float, optional
        The height above ground of the center of the module [m]. Used for

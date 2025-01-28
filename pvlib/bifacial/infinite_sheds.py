dae732612294fd0defd424907abd0d96bedc61eb
jason-rpkt <jason.lun.leung@dnv.com>
2025-01-28 12:01:12 +0800

    Update from solar_zenith to ghi Update docs\sphinx\source\whatsnew\v0.11.3.rst

--- pvlib/bifacial/infinite_sheds.py
+++ pvlib/bifacial/infinite_sheds.py
@@ -260,11 +260,10 @@ def get_irradiance_poa(surface_tilt, surface_azimuth, solar_zenith,
     Returns
     -------
     output : dict or DataFrame
-        Output is a DataFrame when solar_zenith is a Series.
-        solar_zenith is the one that sets the output type.
+        Output is a "pandas.DataFrame" when "ghi" is a Series.
+        Otherwise it is a dict of "numpy.ndarray"
         See Issue #2245
-        See Notes for
-        descriptions of content.
+        See Notes for descriptions of content.
 
     Notes
     -----
@@ -375,7 +374,7 @@ def get_irradiance_poa(surface_tilt, surface_azimuth, solar_zenith,
         'poa_global': poa_global, 'poa_direct': poa_direct,
         'poa_diffuse': poa_diffuse, 'poa_ground_diffuse': poa_gnd_pv,
         'poa_sky_diffuse': poa_sky_pv, 'shaded_fraction': f_x}
-    if isinstance(solar_zenith, pd.Series):
+    if isinstance(ghi, pd.Series):
         output = pd.DataFrame(output)
     return output
 

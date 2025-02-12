c23ce24e683fc406123780b39ff89a75e14049b5
Will Holmgren <william.holmgren@gmail.com>
2025-02-12 09:32:58 -0700

    init file goes4 --> psm4

--- pvlib/iotools/__init__.py
+++ pvlib/iotools/__init__.py
@@ -11,6 +11,9 @@ from pvlib.iotools.solrad import get_solrad  # noqa: F401
 from pvlib.iotools.psm3 import get_psm3  # noqa: F401
 from pvlib.iotools.psm3 import read_psm3  # noqa: F401
 from pvlib.iotools.psm3 import parse_psm3  # noqa: F401
+from pvlib.iotools.psm4 import get_psm4  # noqa: F401
+from pvlib.iotools.psm4 import read_psm4  # noqa: F401
+from pvlib.iotools.psm4 import parse_psm4  # noqa: F401
 from pvlib.iotools.pvgis import get_pvgis_tmy, read_pvgis_tmy  # noqa: F401
 from pvlib.iotools.pvgis import read_pvgis_hourly  # noqa: F401
 from pvlib.iotools.pvgis import get_pvgis_hourly  # noqa: F401
@@ -34,6 +37,3 @@ from pvlib.iotools.solcast import get_solcast_live  # noqa: F401
 from pvlib.iotools.solcast import get_solcast_historic  # noqa: F401
 from pvlib.iotools.solcast import get_solcast_tmy  # noqa: F401
 from pvlib.iotools.solargis import get_solargis  # noqa: F401
-from pvlib.iotools.goes4 import get_goes4  # noqa: F401
-from pvlib.iotools.goes4 import read_goes4  # noqa: F401
-from pvlib.iotools.goes4 import parse_goes4  # noqa: F401

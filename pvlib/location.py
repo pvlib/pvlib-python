5d5e5530faa290af091315a26aaeee5ea9c21eb0
Mark Campanelli <mark.campanelli@gmail.com>
2025-03-08 18:20:05 -0700

    Implement review comments on documentation

--- pvlib/location.py
+++ pvlib/location.py
@@ -43,14 +43,14 @@ class Location:
         Use decimal degrees notation.
 
     tz : time zone as str, int, float, or datetime.tzinfo, default 'UTC'.
-        This value represents as a valid IANA time zone name string. See
-        http://en.wikipedia.org/wiki/List_of_tz_database_time_zones for a
-        list of valid name strings, any of which may be passed directly here.
-        ints and floats must be whole-number hour offsets from UTC, which
-        are converted to the IANA-suppored 'Etc/GMT-N' format. (Note the
-        limited range of the offset N and its sign-change convention.)
-        Time zones from the pytz and zoneinfo packages may also be passed
-        directly here, as they are subclasses of datetime.tzinfo.
+        See http://en.wikipedia.org/wiki/List_of_tz_database_time_zones for a
+        list of valid name strings. An `int` or `float` must be a whole-number hour
+        offsets from UTC, which can be converted to the IANA-supported
+        'Etc/GMT-N' format. (Note the limited range of the offset N and its
+        sign-change convention.) Time zones from the pytz and zoneinfo packages
+        may also be passed here, as they are subclasses of datetime.tzinfo.
+
+        The `tz` attribute is represented as a valid IANA time zone name string.
 
     altitude : float, optional
         Altitude from sea level in meters.

6db356bd053c2b4d35c7d0d2bb6274a8fb50de15
RDaxini <rajivdaxini@msn.com>
2025-04-14 14:34:14 -0600

    long url

--- pvlib/irradiance.py
+++ pvlib/irradiance.py
@@ -77,8 +77,7 @@ def get_extra_radiation(datetime_or_doy, solar_constant=1366.1,
        Thermal Processes, 2nd edn. J. Wiley and Sons, New York.
 
     .. [3] "Solar Radiation Basics". University of Oregon. Accessed: 04 April,
-       2025. [Online.] Available <https://web.archive.org/web/20240424224453/ \
-           http://solardata.uoregon.edu/SolarRadiationBasics.html>
+       2025. [Online.] Available https://web.archive.org/web/20240424224453/http://solardata.uoregon.edu/SolarRadiationBasics.html #noqa: E501
 
     .. [4] J. W. Spencer, "Fourier series representation of the sun," Search,
        vol. 2, p. 172, 1971.

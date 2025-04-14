96ce1024d1370b18563bd3ca143884eaa5517382
RDaxini <rajivdaxini@msn.com>
2025-04-14 14:26:54 -0600

    blank space

--- pvlib/irradiance.py
+++ pvlib/irradiance.py
@@ -75,7 +75,7 @@ def get_extra_radiation(datetime_or_doy, solar_constant=1366.1,
 
     .. [2] Duffie, J. A. and Beckman, W. A. 1991. Solar Engineering of
        Thermal Processes, 2nd edn. J. Wiley and Sons, New York.
-    
+
     .. [3] "Solar Radiation Basics". University of Oregon. Accessed: 04 April,
        2025. [Online.] Available <https://web.archive.org/web/20240424224453/ \
            http://solardata.uoregon.edu/SolarRadiationBasics.html

a27b90013bfd01d97ab5d17155c6dfe00f96ac7a
Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
2025-05-22 18:49:38 -0600

    Update pvlib/iotools/sodapro.py

--- pvlib/iotools/sodapro.py
+++ pvlib/iotools/sodapro.py
@@ -146,8 +146,8 @@ def get_cams(latitude, longitude, start, end, email, identifier='mcclear',
     For large geospatial areas, CAMS offers a pre-calculated
     gridded dataset [4]_over land and coastal areas. This dataset
     may not include the most recent data coverage and may not be
-    based on the most recent version. This dataset is not available through
-    pvlib.
+    based on the most recent CAMS version. This dataset is not available
+    through pvlib.
 
     See Also
     --------

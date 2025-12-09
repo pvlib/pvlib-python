be59498432546101fb5f5bda58684dad0a175433
Kevin Anderson <kevin.anderso@gmail.com>
2025-12-09 12:20:51 -0500

    Add function for accessing MERRA-2 (#2572)
    * function, tests, whatsnew, docs
    
    * lint
    
    * simplify tz handling
    
    * make new earthdata secrets accessible to tests
    
    * add test for tz-aware inputs
    
    * add tests for HTTPError on bad inputs
    
    * add a few more variables to docstring
    
    * Apply suggestions from code review
    
    Co-authored-by: Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
    
    * add link to datasets; add table with variable description
    
    * tweak tests
    
    * add LWGNT, LWGEM to variable map and docstring
    
    * change pvlib names to "longwave_net", "longwave_up"
    
    * add LWGAB
    
    ---------
    
    Co-authored-by: Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>

--- pvlib/iotools/__init__.py
+++ pvlib/iotools/__init__.py
@@ -43,3 +43,4 @@ from pvlib.iotools.meteonorm import get_meteonorm_observation_training  # noqa: 
 from pvlib.iotools.meteonorm import get_meteonorm_tmy  # noqa: F401
 from pvlib.iotools.nasa_power import get_nasa_power  # noqa: F401
 from pvlib.iotools.era5 import get_era5  # noqa: F401
+from pvlib.iotools.merra2 import get_merra2  # noqa: F401

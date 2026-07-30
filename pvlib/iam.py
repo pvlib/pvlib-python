6debdcae605c371599883becca830225e3b4d0c7
jason-rpkt <156782515+jason-rpkt@users.noreply.github.com>
2026-07-30 12:01:37 +0800

    Update pvlib/iam.py with warn_deprecated
    Co-authored-by: Cliff Hansen <cwhanse@sandia.gov>

--- pvlib/iam.py
+++ pvlib/iam.py
@@ -485,6 +485,12 @@ def interp(aoi, theta_ref, iam_ref, method='linear', normalize=True):
     if method in kvals:
         interpolator = make_interp_spline(theta_ref, iam_ref, k=kvals[method])
 
+    elif method in {'nearest', 'nearest-up', 'zero', 'slinear', 'previous', 'next'}:
+        msg = (
+            f"Interpolation method {method} is deprecated in pvlib"
+        )
+        warn_deprecated(since="0.15.3", removal="0.16.0", addendum=msg)
+        interpolator = interp1d(theta_ref, iam_ref, kind=method, fill_value='extrapolate')
     else:
         raise ValueError(
             f"Interpolation method '{method}' is not supported"

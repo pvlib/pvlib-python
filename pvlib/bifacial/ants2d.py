6e39273afab445d09ad1ecf8d77fbfde4332d501
Kevin Anderson <kevin.anderso@gmail.com>
2026-08-05 11:53:46 +0200

    Update pvlib/bifacial/ants2d.py
    Co-authored-by: Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>

--- pvlib/bifacial/ants2d.py
+++ pvlib/bifacial/ants2d.py
@@ -305,7 +305,7 @@ def get_irradiance(tracker_rotation, axis_azimuth, solar_zenith, solar_azimuth,
     """
     Get front and rear irradiance using the ANTS-2D bifacial irradiance model.
 
-    The ANTS-2D model [1] assumes the PV system comprises parallel,
+    The ANTS-2D model [1]_ assumes the PV system comprises parallel,
     evenly spaced rows on flat or uniformly sloped ground. Rows can be on fixed
     racking or single axis trackers. The model calculates irradiance at a
     location far from the ends of any rows, in effect, assuming that the
